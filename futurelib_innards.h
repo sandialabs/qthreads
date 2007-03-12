d18d84d326f77d39ceac3758e9f335e095a8a262
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2007-03-12 19:15:17 +0000

    oops
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@167 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ futurelib_innards.h
@@ -0,0 +1,14 @@
+#ifndef FUTURELIB_INNARDS_H
+#define FUTURELIB_INNARDS_H
+
+typedef struct location_s location_t;
+
+struct location_s
+{
+    aligned_t vp_count;
+    pthread_mutex_t vp_count_lock;
+    unsigned int vp_max;
+    unsigned int id;
+};
+
+#endif
---
+++ qthread_innards.h
@@ -0,0 +1,12 @@
+#ifndef QTHREAD_INNARDS_H
+#define QTHREAD_INNARDS_H
+
+/* These are the internal functions that users should be allowed to get at */
+unsigned int qthread_isfuture(const qthread_t * t);
+void qthread_assertfuture(qthread_t * t);
+void qthread_addertnotfuture(qthread_t * t);
+void qthread_fork_future_to(const qthread_f f, const void *arg,
+			    aligned_t * ret,
+			    const qthread_shepherd_id_t shepherd);
+
+#endif
