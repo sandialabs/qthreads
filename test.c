bf7302d91e1ee0edafb49971abc416d9a408d40f
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2007-03-23 18:52:25 +0000

    fuller test of qutil
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@178 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test.c
+++ test.c
@@ -1,17 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include <qthread/qthread.h>
 #include <qthread/qutil.h>
 
+/*
+ * This file tests the qutil functions
+ *
+ */
+
 int main(int argc, char *argv[])
 {
-    double array[3] = { 1.0, 2.0, 3.0 };
-    volatile double sum;
+    unsigned int *ui_array;
+    unsigned int ui_sum, ui_sum_authoritative = 0;
+    unsigned int ui_len = 10000;
+    double * d_array;
+    double d_sum, d_sum_authoritative = 0.0;
+    unsigned int d_len = 10000;
+    unsigned int i;
 
     qthread_init(3);
-    sum = qutil_double_FF_sum(NULL, array, 3);
-    printf("sum: %lf\n", sum);
+
+    ui_array = calloc(ui_len, sizeof(unsigned int));
+    for (i=0;i<ui_len; i++) {
+	ui_array[i] = i+1;
+	ui_sum_authoritative += i+1;
+    }
+    ui_sum = qutil_uint_sum(NULL, ui_array, ui_len);
+    assert(ui_sum == ui_sum_authoritative);
+    free(ui_array);
+
+    d_array = calloc(d_len, sizeof(double));
+    for (i=0;i<d_len; i++) {
+	d_array[i] = i+1;
+	d_sum_authoritative += i+1;
+    }
+    d_sum = qutil_double_sum(NULL, d_array, d_len);
+    assert(d_sum == d_sum_authoritative);
+    d_sum = qutil_double_FF_sum(NULL, d_array, d_len);
+    assert(d_sum == d_sum_authoritative);
+    free(d_array);
+
     qthread_finalize();
     return 0;
 }
