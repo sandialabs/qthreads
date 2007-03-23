270cde3f12142c45aa32a0761e948103dd7ba82b
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2007-03-23 18:52:37 +0000

    better comments
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@179 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- qthread/qutil.h
+++ qthread/qutil.h
@@ -6,13 +6,25 @@
 #include <qthread/qthread.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
+/* This computes the sum of all the doubles in an array */
+double qutil_double_sum(qthread_t * me, double *array, size_t length);
+/* This computes the sum of all the unsigned ints in an array */
+unsigned int qutil_uint_sum(qthread_t * me, unsigned int *array,
+			    size_t length);
+/* This computes the sum of all the doubles in an array that may not all be
+ * filled in yet */
+double qutil_double_FF_sum(qthread_t * me, double *array, size_t length);
 
-double qutil_double_sum(qthread_t *me, double * array, size_t length);
-unsigned int qutil_uint_sum(qthread_t *me, unsigned int * array, size_t length);
-double qutil_double_FF_sum(qthread_t *me, double *array, size_t length);
-double qutil_runloop_sum_double(qthread_t *me, double (*func)(qthread_t*, const int, void *), void * argstruct, const int loopstart, const int loopend, const int step);
+/* and this will run a bunch of threads, each of which returns a double. The
+ * return values will be summed up as they are computed. */
+double qutil_runloop_sum_double(qthread_t * me,
+				double (*func) (qthread_t *, const int,
+						void *), void *argstruct,
+				const int loopstart, const int loopend,
+				const int step);
 
 #ifdef __cplusplus
 }
