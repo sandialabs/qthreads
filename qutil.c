da1461f6f96a48df4cd41fd7f031ef8cd4160911
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2007-03-29 18:53:27 +0000

    fully fleshed out qutil arithmetic functions
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@194 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- qutil.c
+++ qutil.c
@@ -158,3 +158,43 @@ INNER_LOOP   (qutil_int_min_inner,    qutil_is_args, MIN_MACRO)
 INNER_LOOP_FF(qutil_int_FF_min_inner, qutil_is_args, MIN_MACRO)
 OUTER_LOOP(qutil_int_min, qutil_is_args, MIN_MACRO, int, qutil_int_min_inner, qutil_int_FF_min_inner)
 
+struct qutil_oetsort_args
+{
+    double *array;
+    size_t start, stop;
+};
+
+aligned_t qutil_oetsort_inner(qthread_t *me, struct qutil_oetsort_args *args)
+{
+    printf("inner: %i to %i\n", args->start, args->stop);
+    return 0;
+}
+
+void qutil_oetsort(qthread_t *me, double *array, size_t length, int checkfeb)
+{
+    /* first, decide how much of the array each thread gets */
+    size_t chunksize = 10; // could also use MT_LOOP_CHUNK
+    /* first, decide how many threads to use... */
+    size_t numthreads = length/chunksize; /* could also use numshepherds*2 or something like that */
+    aligned_t *rets;
+    size_t i;
+    struct qutil_oetsort_args *args;
+
+    /* spawn numthreads threads, and wait for them to complete */
+    rets = malloc(numthreads * sizeof(aligned_t));
+    args = malloc(numthreads * sizeof(struct qutil_oetsort_args));
+    for (i=0;i<numthreads; i++) {
+	args[i].array = array;
+	args[i].start = chunksize * i;
+	if ((chunksize+2)*i > length) {
+	    /* the last thread's chunk might have to be bigger than the others */
+	    args[i].end = length;
+	} else {
+	    args[i].end = (chunksize+1) * i;
+	}
+	qthread_fork(qutil_oetsort_inner, args+i, rets+i);
+    }
+    for (i=0;i<numthreads;i++) {
+	qthread_readFF(me, NULL, rets+i);
+    }
+}
