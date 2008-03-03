a0654a4930c333682813a752b968f3961a1b7a24
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2008-03-03 22:54:46 +0000

    enough of cross-compiling warnings
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@378 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@
 
 echo "Generating configure files..."
 
-autoreconf --install --symlink --warnings=all,no-obsolete && \
+autoreconf --install --symlink --warnings=gnu,obsolete,override,portability,no-obsolete && \
   echo "Preparing was successful if there was no error messages above." && \
   exit 0
 
