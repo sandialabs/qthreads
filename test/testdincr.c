fa327731170e8a2b72f7a065ba9c114a11c32c12
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2008-06-12 20:21:31 +0000

    test the qthread_dincr function
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@421 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/testdincr.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <assert.h>
+#include <qthread/qthread.h>
+
+double master = 0.0;
+
+aligned_t incr(qthread_t * me, void *arg)
+{
+    qthread_dincr(&master, 1.0);
+    return 0;
+}
+
+aligned_t incr5(qthread_t * me, void *arg)
+{
+    qthread_dincr(&master, 5.0);
+    return 0;
+}
+
+int main()
+{
+    int i;
+    aligned_t rets[30];
+    double ret_test;
+
+    qthread_init(7);
+
+    ret_test = qthread_dincr(&master, 1);
+    assert(master == 1.0);
+    assert(ret_test == 1.0);
+    master = 0;
+    for (i = 0; i < 30; i++) {
+	qthread_fork(incr, NULL, &(rets[i]));
+    }
+    for (i = 0; i < 30; i++) {
+	qthread_readFF(NULL, NULL, rets + i);
+    }
+    if (master != 30.0) {
+	printf("master is %f rather than 30\n", master);
+    }
+    assert(master == 30.0);
+    master = 0.0;
+    for (i = 0; i < 30; i++) {
+	qthread_fork(incr5, NULL, &(rets[i]));
+    }
+    for (i = 0; i < 30; i++) {
+	qthread_readFF(NULL, NULL, rets + i);
+    }
+    if (master != 150.0) {
+	printf("master is %f rather than 150\n", master);
+    }
+    assert(master == 150.0);
+
+    qthread_finalize();
+
+    return 0;
+}
