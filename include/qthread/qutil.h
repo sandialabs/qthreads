b8b8da6ec1d8a465f0751a878fe0f023c033145c
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2008-06-13 18:23:31 +0000

    qutil function args should be declared const
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@423 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread/qutil.h
+++ include/qthread/qutil.h
@@ -12,28 +12,28 @@ extern "C"
 #endif
 /* This computes the sum/product of all the doubles in an array. If checkfeb is
  * non-zero, then it will wait for each array entry to be marked FEB-full */
-double qutil_double_sum(qthread_t * me, double *array, size_t length,
+double qutil_double_sum(qthread_t * me, const double *array, size_t length,
 			int checkfeb);
-double qutil_double_mult(qthread_t * me, double *array, size_t length,
+double qutil_double_mult(qthread_t * me, const double *array, size_t length,
 			 int checkfeb);
-double qutil_double_max(qthread_t * me, double *array, size_t length,
+double qutil_double_max(qthread_t * me, const double *array, size_t length,
 			int checkfeb);
-double qutil_double_min(qthread_t * me, double *array, size_t length,
+double qutil_double_min(qthread_t * me, const double *array, size_t length,
 			int checkfeb);
 /* This computes the sum/product of all the unsigned ints in an array */
-unsigned int qutil_uint_sum(qthread_t * me, unsigned int *array,
+unsigned int qutil_uint_sum(qthread_t * me, const unsigned int *array,
 			    size_t length, int checkfeb);
-unsigned int qutil_uint_mult(qthread_t * me, unsigned int *array,
+unsigned int qutil_uint_mult(qthread_t * me, const unsigned int *array,
 			     size_t length, int checkfeb);
-unsigned int qutil_uint_max(qthread_t * me, unsigned int *array,
+unsigned int qutil_uint_max(qthread_t * me, const unsigned int *array,
 			    size_t length, int checkfeb);
-unsigned int qutil_uint_min(qthread_t * me, unsigned int *array,
+unsigned int qutil_uint_min(qthread_t * me, const unsigned int *array,
 			    size_t length, int checkfeb);
 /* This computes the sum/product of all the ints in an array */
-int qutil_int_sum(qthread_t * me, int *array, size_t length, int checkfeb);
-int qutil_int_mult(qthread_t * me, int *array, size_t length, int checkfeb);
-int qutil_int_max(qthread_t * me, int *array, size_t length, int checkfeb);
-int qutil_int_min(qthread_t * me, int *array, size_t length, int checkfeb);
+int qutil_int_sum(qthread_t * me, const int *array, size_t length, int checkfeb);
+int qutil_int_mult(qthread_t * me, const int *array, size_t length, int checkfeb);
+int qutil_int_max(qthread_t * me, const int *array, size_t length, int checkfeb);
+int qutil_int_min(qthread_t * me, const int *array, size_t length, int checkfeb);
 
 void qutil_mergesort(qthread_t *me, double *array, size_t length);
 void qutil_qsort(qthread_t *me, double *array, size_t length);
