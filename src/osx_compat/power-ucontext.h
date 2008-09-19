1328e8ccf74828777e057d745a7e34d95d39d943
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2008-09-19 22:40:22 +0000

    handle floating point registers correctly on ppc
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@483 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/osx_compat/power-ucontext.h
+++ src/osx_compat/power-ucontext.h
@@ -12,11 +12,13 @@ struct mcontext
 	unsigned long	toc;		/* callee saved: r2 */
 	unsigned long	r3;		/* first arg to function, return register: r3 */
 	unsigned long	gpr[19];	/* callee saved: r13-r31 */
+	/* double-aligned for performance */
+	unsigned long pad;
+	double fpregs[18];		/* callee saved: r14-r31 */
 /*
 // XXX: currently do not save vector registers or floating-point state
-//	unsigned long	pad;
-//	unsigned long long	fpr[18];	/ * callee saved: f14-f31 * /
 //	unsigned long	vr[4*12];	/ * callee saved: v20-v31, 256-bits each * /
+//      unsigned long   vrsave;         / * which v regs should be saved? * /
 */
 };
 
