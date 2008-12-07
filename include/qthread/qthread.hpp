bc4532ca00da28743432cf720203a09e02c57d9e
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2008-12-07 06:43:46 +0000

    tidy up a bit
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@555 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread/qthread.hpp
+++ include/qthread/qthread.hpp
@@ -11,153 +11,155 @@
 template <bool> class OnlyTrue;
 template <> class OnlyTrue<true> {};
 #define QTHREAD_CHECKSIZE(X) sizeof(OnlyTrue<(bool)(sizeof(X) == sizeof(aligned_t))>)
+#define QTHREAD_STATIC_ASSERT(X) sizeof(OnlyTrue<(bool)(X)>)
 
 template <typename T>
-inline int qthraed_feb_status(const T* addr) 
+static QINLINE int qthraed_feb_status(const T* addr) 
 {
     return qthread_feb_status((aligned_t*) addr);
 }
 
 template <typename T>
-inline int qthread_empty(const T* dest)
+static QINLINE int qthread_empty(const T* dest)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_empty(qthread_self(), (aligned_t*) dest);
 }
 template <typename T>
-inline int qthread_empty(qthread_t *me, const T* dest)
+static QINLINE int qthread_empty(qthread_t *me, const T* dest)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_empty(me, (aligned_t*) dest);
 }
 
 template <typename T>
-inline int qthread_fill(const T* dest)
+static QINLINE int qthread_fill(const T* dest)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_fill(qthread_self(), (aligned_t*) dest);
 }
 template <typename T>
-inline int qthread_fill(qthread_t *me, const T* dest)
+static QINLINE int qthread_fill(qthread_t *me, const T* dest)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_fill(me, (aligned_t*) dest);
 }
 
 template <typename T>
-inline int qthread_writeEF(T * const dest, const T * const src)
+static QINLINE int qthread_writeEF(T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeEF(qthread_self(), (aligned_t*) dest, (aligned_t*) src);
 }
 template <typename T>
-inline int qthread_writeEF(qthread_t *me, T * const dest, const T * const src)
+static QINLINE int qthread_writeEF(qthread_t *me, T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeEF(me, (aligned_t*) dest, (aligned_t*) src);
 }
 template <typename T, typename T2>
-inline int qthread_writeEF_const(T * const dest, const T2 src)
+static QINLINE int qthread_writeEF_const(T * const dest, const T2 src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeEF_const(qthread_self(), (aligned_t*) dest, (aligned_t) src);
 }
 template <typename T, typename T2>
-inline int qthread_writeEF_const(qthread_t *me, T * const dest, const T2 src)
+static QINLINE int qthread_writeEF_const(qthread_t *me, T * const dest, const T2 src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeEF_const(me, (aligned_t*) dest, (aligned_t) src);
 }
 
 template <typename T>
-inline int qthread_writeF(T * const dest, const T * const src)
+static QINLINE int qthread_writeF(T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeF(qthread_self(), (aligned_t*) dest, (aligned_t*) src);
 }
 template <typename T>
-inline int qthread_writeF(qthread_t *me, T * const dest, const T * const src)
+static QINLINE int qthread_writeF(qthread_t *me, T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeF(me, (aligned_t*) dest, (aligned_t*) src);
 }
 template <typename T, typename T2>
-inline int qthread_writeF_const(T * const dest, const T2 src)
+static QINLINE int qthread_writeF_const(T * const dest, const T2 src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeF_const(qthread_self(), (aligned_t*) dest, (aligned_t) src);
 }
 template <typename T, typename T2>
-inline int qthread_writeF_const(qthread_t *me, T * const dest, const T2 src)
+static QINLINE int qthread_writeF_const(qthread_t *me, T * const dest, const T2 src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_writeF_const(me, (aligned_t*) dest, (aligned_t) src);
 }
 
 template <typename T>
-inline int qthread_readFF(T * const dest, const T * const src)
+static QINLINE int qthread_readFF(T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_readFF(qthread_self(), (aligned_t*) dest, (aligned_t*) src);
 }
 template <typename T>
-inline int qthread_readFF(qthread_t *me, T * const dest, const T * const src)
+static QINLINE int qthread_readFF(qthread_t *me, T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_readFF(me, (aligned_t*) dest, (aligned_t*) src);
 }
 
 template <typename T>
-inline int qthread_readFE(T * const dest, const T * const src)
+static QINLINE int qthread_readFE(T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_readFE(qthread_self(), (aligned_t*) dest, (aligned_t*) src);
 }
 template <typename T>
-inline int qthread_readFE(qthread_t *me, T * const dest, const T * const src)
+static QINLINE int qthread_readFE(qthread_t *me, T * const dest, const T * const src)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_readFE(me, (aligned_t*) dest, (aligned_t*) src);
 }
 
 template <typename T>
-inline int qthread_lock(const T* a)
+static QINLINE int qthread_lock(const T* a)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_lock(qthread_self(), (aligned_t*) a);
 }
 template <typename T>
-inline int qthread_lock(qthread_t *me, const T* a)
+static QINLINE int qthread_lock(qthread_t *me, const T* a)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_lock(me, (aligned_t*) a);
 }
 
 template <typename T>
-inline int qthread_unlock(const T* a)
+static QINLINE int qthread_unlock(const T* a)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_unlock(qthread_self(), (aligned_t*) a);
 }
 template <typename T>
-inline int qthread_unlock(qthread_t *me, const T* a)
+static QINLINE int qthread_unlock(qthread_t *me, const T* a)
 {
     QTHREAD_CHECKSIZE(T);
     return qthread_unlock(me, (aligned_t*) a);
 }
 
-inline float qthread_incr(volatile float *operand, const float incr)
+static QINLINE float qthread_incr(volatile float *operand, const float incr)
 {
     return qthread_fincr(operand, incr);
 }
-inline double qthread_incr(volatile double *operand, const double incr)
+static QINLINE double qthread_incr(volatile double *operand, const double incr)
 {
     return qthread_dincr(operand, incr);
 }
 // BWB: FIX ME: need to restrict T to integer values...
 template <typename T, typename T2>
-inline T qthread_incr(volatile T *operand, const T2 incr)
+static QINLINE T qthread_incr(volatile T *operand, const T2 incr)
 {
+    QTHREAD_STATIC_ASSERT(sizeof(T) == 4 || sizeof(T) == 8);
     switch (sizeof(T)) {
     case 4:
         return qthread_incr32((volatile uint32_t*) operand, incr);
