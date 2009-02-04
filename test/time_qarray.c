ba63473285c587da8cb1a1f50bd81116f9c00406
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-02-04 17:43:39 +0000

    typo
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@645 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/time_qarray.c
+++ test/time_qarray.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 	}
     }
     if (argc >= 3) {
-	ELEMENT_COUNT = strtol(argv[1], NULL, 0);
+	ELEMENT_COUNT = strtol(argv[2], NULL, 0);
     }
 
     qthread_init(threads);
