4233c8ba2c58bf846c3ecf7324a64987ed690836
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-03-22 01:03:28 +0000

    qlfqueue timer
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@749 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/time_qlfqueue.c
+++ test/time_qlfqueue.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <assert.h>
 #include <qthread/qthread.h>
+#include <qthread/qloop.h>
 #include <qthread/qlfqueue.h>
+#include "qtimer.h"
 
 #define ELEMENT_COUNT 10000
 #define THREAD_COUNT 128
@@ -35,6 +37,32 @@ aligned_t dequeuer (qthread_t *me, void *arg)
     return 0;
 }
 
+void loop_queuer (qthread_t *me, const size_t startat, const size_t stopat, void *arg)
+{
+    size_t i;
+    qlfqueue_t *q = (qlfqueue_t *)arg;
+
+    for (i=startat; i<stopat; i++) {
+	if (qlfqueue_enqueue(q, (void*)me) != QTHREAD_SUCCESS) {
+	    fprintf(stderr, "qlfqueue_enqueue(q, %p) failed!\n", me);
+	    exit(-2);
+	}
+    }
+}
+
+void loop_dequeuer (qthread_t *me, const size_t startat, const size_t stopat, void *arg)
+{
+    size_t i;
+    qlfqueue_t *q = (qlfqueue_t *)arg;
+
+    for (i=startat; i<stopat; i++) {
+	if (qlfqueue_dequeue(q) == NULL) {
+	    fprintf(stderr, "qlfqueue_dequeue(q, %p) failed!\n", me);
+	    exit(-2);
+	}
+    }
+}
+
 int main(int argc, char *argv[])
 {
     qlfqueue_t *q;
@@ -42,6 +70,7 @@ int main(int argc, char *argv[])
     qthread_t *me;
     size_t i;
     aligned_t *rets;
+    qtimer_t timer = qtimer_new();
 
     if (argc == 2) {
 	threads = strtol(argv[1], NULL, 0);
@@ -61,38 +90,30 @@ int main(int argc, char *argv[])
 	exit(-1);
     }
 
-    if (qlfqueue_enqueue(q, (void*)me) != 0) {
-	fprintf(stderr, "qlfqueue_enqueue() failed!\n");
-	exit(-1);
-    }
-
-    if (qlfqueue_dequeue(q) != me) {
-	fprintf(stderr, "qlfqueue_dequeue() failed!\n");
-	exit(-1);
-    }
-
-    for (i = 0; i < ELEMENT_COUNT; i++) {
-	if (qlfqueue_enqueue(q, (void*)(intptr_t)(i+1)) != 0) {
-	    fprintf(stderr, "qlfqueue_enqueue(q,%i) failed!\n", (int)i);
-	    exit(-1);
-	}
-    }
-    for (i = 0; i < ELEMENT_COUNT; i++) {
-	if (qlfqueue_dequeue(q) != (void*)(intptr_t)(i+1)) {
-	    fprintf(stderr, "qlfqueue_dequeue() failed, didn't equal %i!\n", (int)i);
-	    exit(-1);
-	}
-    }
+    /* prime the pump */
+    qt_loop_balance(0, THREAD_COUNT * ELEMENT_COUNT, loop_queuer, q);
+    qt_loop_balance(0, THREAD_COUNT * ELEMENT_COUNT, loop_dequeuer, q);
     if (!qlfqueue_empty(q)) {
-	fprintf(stderr, "qlfqueue not empty after ordering test!\n");
-	exit(-1);
+	fprintf(stderr, "qlfqueue not empty after priming!\n");
+	exit(-2);
     }
-    if (interactive) {
-	printf("ordering test succeeded\n");
+
+    qtimer_start(timer);
+    qt_loop_balance(0, THREAD_COUNT * ELEMENT_COUNT, loop_queuer, q);
+    qtimer_stop(timer);
+    printf("loop balance enqueue: %f secs\n", qtimer_secs(timer));
+    qtimer_start(timer);
+    qt_loop_balance(0, THREAD_COUNT * ELEMENT_COUNT, loop_dequeuer, q);
+    qtimer_stop(timer);
+    printf("loop balance dequeue: %f secs\n", qtimer_secs(timer));
+    if (!qlfqueue_empty(q)) {
+	fprintf(stderr, "qlfqueue not empty after loop balance test!\n");
+	exit(-2);
     }
 
     rets = calloc(THREAD_COUNT, sizeof(aligned_t));
     assert(rets != NULL);
+    qtimer_start(timer);
     for (i = 0; i < THREAD_COUNT; i++) {
 	assert(qthread_fork(dequeuer, q, &(rets[i])) == QTHREAD_SUCCESS);
     }
@@ -102,14 +123,13 @@ int main(int argc, char *argv[])
     for (i = 0; i < THREAD_COUNT; i++) {
 	assert(qthread_readFF(me, NULL, &(rets[i])) == QTHREAD_SUCCESS);
     }
+    qtimer_stop(timer);
     free(rets);
     if (!qlfqueue_empty(q)) {
 	fprintf(stderr, "qlfqueue not empty after threaded test!\n");
 	exit(-2);
     }
-    if (interactive) {
-	printf("threaded test succeeded\n");
-    }
+    printf("threaded test: %f secs\n", qtimer_secs(timer));
 
     if (qlfqueue_free(q) != QTHREAD_SUCCESS) {
 	fprintf(stderr, "qlfqueue_free() failed!\n");
