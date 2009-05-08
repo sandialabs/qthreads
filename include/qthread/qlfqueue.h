bb20bc230130fd0d31b44ca18b7a3d6f641d9999
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-05-08 18:29:17 +0000

    using Q_STARTCXX/Q_ENDCXX, and reformatting with gnu indent
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@849 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread/qlfqueue.h
+++ include/qthread/qlfqueue.h
@@ -1,22 +1,27 @@
 #ifndef QTHREAD_QLFQUEUE_H
 #define QTHREAD_QLFQUEUE_H
 
+#include <qthread/qthread.h>
+
+Q_STARTCXX;
+
 typedef struct qlfqueue_s qlfqueue_t;
 
 /* Create a new qlfqueue */
-qlfqueue_t *qlfqueue_create(qthread_t *me);
+qlfqueue_t *qlfqueue_create(qthread_t * me);
 
 /* destroy that queue */
-int qlfqueue_destroy(qthread_t *me, qlfqueue_t * q);
+int qlfqueue_destroy(qthread_t * me, qlfqueue_t * q);
 
 /* enqueue something in the queue */
-int qlfqueue_enqueue(qthread_t *me, qlfqueue_t * q, void *elem);
+int qlfqueue_enqueue(qthread_t * me, qlfqueue_t * q, void *elem);
 
 /* dequeue something from the queue (returns NULL for an empty queue) */
-void *qlfqueue_dequeue(qthread_t *me, qlfqueue_t * q);
+void *qlfqueue_dequeue(qthread_t * me, qlfqueue_t * q);
 
 /* returns 1 if the queue is empty, 0 otherwise */
-int qlfqueue_empty(qlfqueue_t *q);
+int qlfqueue_empty(qlfqueue_t * q);
 
+Q_ENDCXX;
 
 #endif
