4ee472c8bdb7fae4bb7eb4cdfd7a59855cb77f59
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-05-22 16:55:01 +0000

    wavefront
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@896 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ src/wavefront.c
@@ -0,0 +1,206 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdlib.h>		       /* for malloc */
+
+#include <qthread_asserts.h>
+
+#include <qthread/qthread.h>
+#include <qthread/qdqueue.h>
+#include <qthread/wavefront.h>
+
+struct qt_wave_wargs
+{
+    qdqueue_t *const work_queue;
+    volatile aligned_t *restrict const no_more_work;
+    volatile aligned_t *restrict const donecount;
+    wave_f func;
+    qarray *restrict const *const R;
+    size_t maxcols;
+    int feb;
+};
+
+struct qt_wave_workunit
+{
+    size_t origrow, startrow, endrow, col;
+};
+
+static void qt_wave_worker(qthread_t * me, struct qt_wave_wargs *const arg)
+{
+    qarray *restrict const *const R = arg->R;
+
+    while (1) {
+	struct qt_wave_workunit *const wu =
+	    qdqueue_dequeue(me, arg->work_queue);
+	if (wu == NULL) {
+	    if (*(arg->no_more_work)) {
+		qthread_incr(arg->donecount, 1);
+		break;
+	    }
+	    qthread_yield(me);
+	} else {
+	    const size_t col = wu->col;
+	    size_t row;
+	    int requeued = 0;
+
+	    /* step 1: call the user function on the input */
+	    for (row = wu->startrow; row < wu->endrow; row++) {
+		void *left = qarray_elem_nomigrate(R[col - 1], row);
+
+		/* check to see if the left is ready */
+		if (col == 1 || qthread_feb_status(left)) {
+		    void *ptr = qarray_elem_nomigrate(R[col], row);
+
+		    /* we can assume that leftbelow is ready (because left is ready) */
+		    void *leftbelow =
+			qarray_elem_nomigrate(R[col - 1], row - 1);
+		    /* we can further assume that below is ready (either
+		     * because this work unit was queued or because we just
+		     * computed it) */
+		    void *below = qarray_elem_nomigrate(R[col], row - 1);
+
+		    arg->func(left, leftbelow, below, ptr);
+		    qthread_fill(me, ptr);
+		} else {
+		    /* re-queue the work unit */
+		    wu->startrow = row;
+		    requeued = 1;
+		    qdqueue_enqueue(me, arg->work_queue, wu);
+		    break;
+		}
+		if (row == 1 && arg->feb && col + 1 != arg->maxcols) {
+		    /* queue work for the right (since they have at least a
+		     * LITTLE data) */
+		    struct qt_wave_workunit *wu2 =
+			malloc(sizeof(struct qt_wave_workunit));
+		    qarray *const right = R[col + 1];
+
+		    wu2->startrow = 1;
+		    wu2->origrow = 1;
+		    wu2->endrow =
+			(right->count >
+			 right->segment_size) ? (right->
+						 segment_size) : (right->
+								  count);
+		    wu2->col = col + 1;
+		    qdqueue_enqueue_there(me, arg->work_queue, wu2,
+					  qarray_shepof(right, 1));
+		}
+	    }
+	    if (requeued == 0 && row == wu->endrow) {
+		/* we're done with this array segment! Huzzah! */
+		/* if we aren't using FEBs, we may need to queue the guy to the right */
+		if (wu->origrow == 1 && !arg->feb && col + 1 != arg->maxcols) {
+		    /* queue work for the right (since they have at least a
+		     * LITTLE data) */
+		    struct qt_wave_workunit *wu2 =
+			malloc(sizeof(struct qt_wave_workunit));
+		    qarray *const right = R[col + 1];
+
+		    wu2->startrow = 1;
+		    wu2->origrow = 1;
+		    wu2->endrow =
+			(right->count >
+			 right->segment_size) ? (right->
+						 segment_size) : (right->
+								  count);
+		    wu2->col = col + 1;
+		    qdqueue_enqueue_there(me, arg->work_queue, wu2,
+					  qarray_shepof(right, 1));
+		}
+		/* now, we may need to queue the next guy up the chain... */
+		if (wu->endrow < R[col]->count) {
+		    /* queue next (reuse the work unit structure) */
+		    wu->startrow = wu->endrow;
+		    wu->origrow = wu->endrow;
+		    wu->endrow = wu->startrow + R[col]->segment_size;
+		    if (wu->endrow > R[col]->count) {
+			wu->endrow = R[col]->count;
+		    }
+		    qdqueue_enqueue_there(me, arg->work_queue, wu,
+					  qarray_shepof(R[col],
+							wu->startrow));
+		} else {
+		    /* freeeeedooooooommmmmm! */
+		    if (col != 1) {
+			free(wu);
+		    }
+		    if (col + 1 == arg->maxcols) {
+			/* ALL DONE */
+			*(arg->no_more_work) = 1;
+		    }
+		}
+	    }
+	}
+    }
+}
+
+/* we must assume that R[0] is full, and that R[*][0] is full as well:
+ *
+ *  R[0][5] R[1][5] R[2][5] R[3][5] R[4][5] R[5][5]
+ *  R[0][4] R[1][4] R[2][4] R[3][4] R[4][4] R[5][4]
+ *  R[0][3] R[1][3] R[2][3] R[3][3] R[4][3] R[5][3]
+ *  R[0][2] R[1][2] R[2][2] R[3][2] R[4][2] R[5][2]
+ *  R[0][1] R[1][1] R[2][1] R[3][1] R[4][1] R[5][1]
+ *  R[0][0] R[1][0] R[2][0] R[3][0] R[4][0] R[5][0]
+ *
+ *  We also require that all qarrays in R have the same internal dimensions
+ *  (segment size, unit size, etc.)
+ *
+ */
+void qt_wavefront_config(qarray * restrict const *const R, size_t cols,
+			 wave_f func, int feb)
+{
+    const qthread_shepherd_id_t maxsheps = qthread_num_shepherds();
+    qthread_shepherd_id_t shep;
+    volatile aligned_t no_more_work = 0;
+    volatile aligned_t donecount = 0;
+    struct qt_wave_wargs wargs =
+	{ qdqueue_create(), &no_more_work, &donecount, func, R, cols, feb };
+    qarray *completed;
+    qthread_t *const me = qthread_self();
+    struct qt_wave_workunit wu = { 0 };
+
+    assert(cols > 1);
+    if (cols <= 1) {
+	return;
+    }
+    /* step 1: create an array to record the right-most data completed in each row */
+    completed = qarray_create(cols, sizeof(aligned_t));
+
+    /* step 2: set up a qdqueue of work */
+    /* -- work queue set up as part of initialization stuff, above */
+
+    /* step 3: spawn workers */
+    for (shep = 0; shep < maxsheps; shep++) {
+	qthread_fork_to((qthread_f) qt_wave_worker, &wargs, NULL, shep);
+    }
+
+    /* step 4: queue a job for the lower-left corner */
+    wu.startrow = 1;
+    wu.origrow = 1;
+    wu.endrow =
+	(R[1]->count >
+	 R[1]->segment_size) ? (R[1]->segment_size) : (R[1]->count);
+    wu.col = 1;
+    qdqueue_enqueue_there(me, wargs.work_queue, &wu, qarray_shepof(R[1], 1));
+
+    /* step 5: wait for the workers to get done */
+    while (donecount < maxsheps) {
+	qthread_yield(me);
+    }
+    qdqueue_destroy(me, wargs.work_queue);
+}
+
+void qt_basic_wavefront(int *restrict const *const R, size_t cols,
+			size_t rows, wave_f func)
+{
+    /* assuming R is properly initialized. */
+    for (size_t col = 1; col < cols; col++) {
+	for (size_t row = 1; row < rows; row++) {
+	    func(&(R[col - 1][row]), &(R[col - 1][row - 1]),
+		 &(R[col][row - 1]), &(R[col][row]));
+	}
+    }
+}
---
+++ test/test_wavefront.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <qthread/qthread.h>
+#include <qthread/wavefront.h>
+
+static size_t ASIZE = 10;
+
+void sum(const void *restrict left, const void *restrict leftdown,
+	 const void *restrict down, void *restrict out)
+{
+    *(int *)out = *(int *)left + *(int *)leftdown + *(int *)down;
+}
+
+void suma(const void *restrict left, const void *restrict leftdown,
+	  const void *restrict down, void *restrict out)
+{
+    *(aligned_t *) out =
+	*(aligned_t *) left + *(aligned_t *) leftdown + *(aligned_t *) down;
+}
+
+void assign1(qthread_t * me, const size_t startat, const size_t stopat,
+	     qarray * a, void *arg)
+{
+    aligned_t *ptr = qarray_elem_nomigrate(a, startat);
+    const size_t max = stopat - startat;
+
+    for (size_t i = 0; i < max; i++) {
+	ptr[i] = 1;
+    }
+}
+
+void emptyarray(qthread_t * me, const size_t startat, const size_t stopat,
+		qarray * a, void *arg)
+{
+    aligned_t *ptr = qarray_elem_nomigrate(a, startat);
+    const size_t max = stopat - startat;
+
+    for (size_t i = 0; i < max; i++) {
+	qthread_empty(me, ptr + i);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    {
+	int **R = calloc(ASIZE, sizeof(int *));
+	int i;
+
+	for (i = 0; i < ASIZE; i++) {
+	    R[i] = calloc(ASIZE, sizeof(int));
+	    R[i][0] = 1;
+	    if (i == 0) {
+		for (i = 0; i < ASIZE; i++) {
+		    R[0][i] = 1;
+		}
+		i = 0;
+	    }
+	}
+	/* do stuff */
+	qt_basic_wavefront(R, ASIZE, ASIZE, sum);
+
+	/* prove it */
+	for (int row = (ASIZE - 1); row >= 0; row--) {
+	    for (int col = 0; col < ASIZE; col++) {
+		printf("%7i ", R[col][row]);
+	    }
+	    printf("\n");
+	}
+	printf("\n");
+
+	/* free it */
+	for (i = 0; i < ASIZE; i++) {
+	    free(R[i]);
+	}
+	free(R);
+    }
+    qthread_init(0);
+    ASIZE = 1026;
+    {
+	qthread_t *me = qthread_self();
+	qarray **R = calloc(ASIZE, sizeof(qarray *));
+
+	R[0] = qarray_create_tight(ASIZE, sizeof(aligned_t));
+	qarray_iter_loop(me, R[0], 0, ASIZE, assign1, NULL);
+	for (int col = 1; col < ASIZE; col++) {
+	    R[col] = qarray_create_tight(ASIZE, sizeof(aligned_t));
+	    {
+		aligned_t *ptr = qarray_elem_nomigrate(R[col], 0);
+
+		*ptr = 1;
+	    }
+	    qarray_iter_loop(me, R[col], 0, ASIZE, emptyarray, NULL);
+	}
+	/* do stuff */
+	qt_wavefront(R, ASIZE, sum);
+
+	/* prove it */
+	for (int row = (ASIZE - 1); row >= 0; row--) {
+	    for (int col = 0; col < ASIZE; col++) {
+		aligned_t *tmpint = qarray_elem_nomigrate(R[col], row);
+
+		printf("%10u ", (unsigned int)*tmpint);
+	    }
+	    printf("\n");
+	}
+	/* free it */
+	for (int col = 0; col < ASIZE; col++) {
+	    qarray_destroy(R[col]);
+	}
+	free(R);
+    }
+}
