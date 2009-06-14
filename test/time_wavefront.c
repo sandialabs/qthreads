ae1f9cf3ece9143585b69258a308165f35c1acef
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-06-14 06:00:18 +0000

    quick timer (update to new wavefront)
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@999 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/time_wavefront.c
+++ test/time_wavefront.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <qthread/qthread.h>
@@ -7,80 +6,70 @@
 
 static size_t ASIZE = 10;
 
-void sum(const void *restrict left, const void *restrict leftdown,
+void avg(const void *restrict left, const void *restrict leftdown,
 	 const void *restrict down, void *restrict out)
 {
-    *(int *)out = *(int *)left + *(int *)leftdown + *(int *)down;
+    *(double*)out = (*(double*)left + *(double*)leftdown + *(double*)down) / 3.0;
 }
 
-void suma(const void *restrict left, const void *restrict leftdown,
-	  const void *restrict down, void *restrict out)
-{
-    *(aligned_t *) out =
-	*(aligned_t *) left + *(aligned_t *) leftdown + *(aligned_t *) down;
-}
-
-void assign1(qthread_t * me, const size_t startat, const size_t stopat,
+void assignrand(qthread_t * me, const size_t startat, const size_t stopat,
 	     qarray * a, void *arg)
 {
-    aligned_t *ptr = qarray_elem_nomigrate(a, startat);
+    double *ptr = qarray_elem_nomigrate(a, startat);
     const size_t max = stopat - startat;
 
     for (size_t i = 0; i < max; i++) {
-	ptr[i] = 1;
+	/*long r = random();
+	memcpy(&(ptr[i]), &r, sizeof(long));*/
+	ptr[i] = (double)(i+startat);
     }
 }
 
 int main(int argc, char *argv[])
 {
     int threads = 0;
+    int interactive = 0;
+    qthread_t *me;
+    qarray *v, *h;
+    qt_wavefront_lattice *L;
+    qtimer_t timer = qtimer_new();
 
     if (argc >= 2) {
 	threads = strtol(argv[1], NULL, 0);
 	if (threads < 0) {
 	    threads = 0;
 	} else {
+	    interactive = 1;
 	    printf("%i threads\n", threads);
 	}
     }
     if (argc >= 3) {
-	ASIZE = strtoul(argv[2], NULL, 0);
+	ASIZE = strtol(argv[2], NULL, 0);
 	printf("ASIZE: %i\n", (int)ASIZE);
     }
     qthread_init(threads);
-    {
-	qthread_t *me = qthread_self();
-	qarray **R = calloc(ASIZE, sizeof(qarray *));
-	qtimer_t timer = qtimer_new();
 
-	assert(R);
-	assert(timer);
-	R[0] = qarray_create_configured(ASIZE, sizeof(aligned_t), FIXED_HASH, 1, 1);
-	qarray_iter_loop(me, R[0], 0, ASIZE, assign1, NULL);
-	for (unsigned int col = 1; col < ASIZE; col++) {
-	    R[col] = qarray_create_configured(ASIZE, sizeof(aligned_t), FIXED_HASH, 1, 1);
-	    {
-		aligned_t *ptr = qarray_elem_nomigrate(R[col], 0);
+    me = qthread_self();
+    v=qarray_create_configured(ASIZE, sizeof(double), FIXED_HASH, 1, 1);
+    h=qarray_create_configured(ASIZE+1, sizeof(double), FIXED_HASH, 1, 1);
 
-		*ptr = 1;
-	    }
-	}
-	printf("initialized (%i-unit segments)\n", (int)(R[0]->segment_size));
-	/* do stuff */
-	qtimer_start(timer);
-	for (int i = 0; i < 10; i++) {
-	    qt_wavefront(R, ASIZE, sum);
-	    printf("i=%i\n", i);
-	}
-	qtimer_stop(timer);
+    qarray_iter_loop(me, h, 1, ASIZE+1, assignrand, NULL);
+    qarray_iter_loop(me, v, 0, ASIZE, assignrand, NULL);
+    printf("v items per seg: %i\n", (int)v->segment_size);
 
-	/* prove it */
-	printf("wavefront secs: %f\n", qtimer_secs(timer) / 10.0);
-	/* free it */
-	for (unsigned int col = 0; col < ASIZE; col++) {
-	    qarray_destroy(R[col]);
-	}
-	free(R);
-	qtimer_free(timer);
+    /* do stuff */
+    qtimer_start(timer);
+    L = qt_wavefront(v, h, avg);
+    qtimer_stop(timer);
+
+    if (L) {
+	printf("wavefront secs: %f\n", qtimer_secs(timer));
+	//qt_wavefront_print_lattice(L);
+    } else {
+	printf("wavefront returned NULL!\n");
     }
+    qarray_destroy(v);
+    qarray_destroy(h);
+    qt_wavefront_destroy_lattice(L);
+    return 0;
 }
