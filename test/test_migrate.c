3b83f1fa048c695cab799429e1125fd1dd34ae04
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-06-23 00:32:05 +0000

    check that shepherds identify themselves properly, and that NULL shepherd identification provides the same results as shepherd identification based on a qthread_t
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1026 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_migrate.c
+++ test/test_migrate.c
@@ -4,6 +4,17 @@
 #include <unistd.h>
 #include <qthread/qthread.h>
 
+static aligned_t checkres(qthread_t *me, void *arg)
+{
+    int myshep = qthread_shep(me);
+
+    assert(myshep == 1 || myshep == 0);
+
+    assert(myshep == (int)arg);
+
+    return 0;
+}
+
 static aligned_t migrant(qthread_t * me, void *arg)
 {
     int myshep = qthread_shep(me);
@@ -13,9 +24,11 @@ static aligned_t migrant(qthread_t * me, void *arg)
     if (myshep == 1) {
 	qthread_migrate_to(me, 0);
 	assert(qthread_shep(me) == 0);
+	assert(qthread_shep(NULL) == 0);
     } else {
 	qthread_migrate_to(me, 1);
 	assert(qthread_shep(me) == 1);
+	assert(qthread_shep(NULL) == 1);
     }
 
     return 0;
@@ -24,11 +37,18 @@ static aligned_t migrant(qthread_t * me, void *arg)
 int main(int argc, char *argv[])
 {
     aligned_t ret;
+    qthread_t *me;
 
     qthread_init(2);
+    me = qthread_self();
+
+    qthread_fork_to(checkres, (void*)0, &ret, 0);
+    qthread_readFF(me, &ret, &ret);
+    qthread_fork_to(checkres, (void*)1, &ret, 1);
+    qthread_readFF(me, &ret, &ret);
 
     qthread_fork(migrant, NULL, &ret);
-    qthread_readFF(qthread_self(), &ret, &ret);
+    qthread_readFF(me, &ret, &ret);
 
     qthread_finalize();
 
