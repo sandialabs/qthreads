0aef2e0bf08bab9c2f3f8411f7aea7fec3188ad9
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-06-30 21:12:54 +0000

    this test is critical for porting to new platforms, so lets make it a bit more verbose (in interactive mode)
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1049 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_migrate.c
+++ test/test_migrate.c
@@ -38,17 +38,43 @@ int main(int argc, char *argv[])
 {
     aligned_t ret;
     qthread_t *me;
+    int interactive = 0;
 
     qthread_init(2);
     me = qthread_self();
 
+    if (argc >= 2) {
+	interactive = 1;
+    }
+
+    assert(qthread_num_shepherds() >= 2);
+    if (interactive) {
+	printf("now to fork to shepherd 0...\n");
+	fflush(stdout);
+    }
     qthread_fork_to(checkres, (void*)0, &ret, 0);
     qthread_readFF(me, &ret, &ret);
+    if (interactive) {
+	printf("success in forking to shepherd 0!\n");
+	printf("now to fork to shepherd 1...\n");
+	fflush(stdout);
+    }
     qthread_fork_to(checkres, (void*)1, &ret, 1);
     qthread_readFF(me, &ret, &ret);
-
+    if (interactive) {
+	printf("success in forking to shepherd 1!\n");
+	printf("now to fork the migrant...\n");
+	fflush(stdout);
+    }
     qthread_fork(migrant, NULL, &ret);
+    if (interactive) {
+	printf("success in forking migrant!\n");
+	fflush(stdout);
+    }
     qthread_readFF(me, &ret, &ret);
+    if (interactive) {
+	printf("migrant returned successfully!\n");
+    }
 
     qthread_finalize();
 
