d6d78992221c9b61ed78b7b084c2e0b48240ce33
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-08-11 18:48:35 +0000

    a bit more verbosity, and compar to libc qsort
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1056 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/time_qutil_qsort.c
+++ test/time_qutil_qsort.c
@@ -15,14 +15,21 @@
 #include <qthread/qutil.h>
 #include "qtimer.h"
 
+int cmp(const void *a, const void *b) {
+    return(*(double*)a - *(double*)b);
+}
+
 int main(int argc, char *argv[])
 {
     aligned_t *ui_array, *ui_array2;
+    double *d_array, *d_array2;
     int threads = 1;
     size_t len = 1000000, i;
     qtimer_t timer = qtimer_new();
     double cumulative_time=0.0;
     int interactive = 0;
+    int using_doubles = 0;
+    unsigned long iterations = 10;
 
     if (argc >= 2) {
 	threads = strtol(argv[1], NULL, 0);
@@ -35,30 +42,81 @@ int main(int argc, char *argv[])
 	len = strtoul(argv[2], NULL, 0);
 	printf("len = %lu\n", (unsigned long)len);
     }
+    if (argc >= 4) {
+	iterations = strtoul(argv[3], NULL, 0);
+	printf("%lu iterations\n", (unsigned long)iterations);
+    }
+    if (argc >= 5) {
+	using_doubles = strtoul(argv[4], NULL, 0);
+	printf("using %s\n", using_doubles?"doubles":"aligned_ts");
+    }
 
     qthread_init(threads);
 
-    ui_array = calloc(len, sizeof(aligned_t));
-    for (i = 0; i < len; i++) {
-	ui_array[i] = random();
+    if (using_doubles) {
+	d_array = calloc(len, sizeof(double));
+	for (i = 0; i < len; i++) {
+	    d_array[i] = ((double)random())/((double)RAND_MAX) + random();
+	}
+	d_array2 = calloc(len, sizeof(double));
+	if (interactive) {
+	    printf("double array generated...\n");
+	}
+	for (unsigned int i=0;i<iterations;i++) {
+	    memcpy(d_array2, d_array, len*sizeof(double));
+	    qtimer_start(timer);
+	    qutil_qsort(qthread_self(), d_array2, len);
+	    qtimer_stop(timer);
+	    cumulative_time += qtimer_secs(timer);
+	    if (interactive == 1) {
+		printf("\t%u: sorting %lu doubles in parallel took: %f seconds\n",
+			i, (unsigned long)len,
+			qtimer_secs(timer));
+	    }
+	}
+	if (interactive == 1) {
+	    printf("sorting %lu doubles in parallel took: %f seconds\n",
+		    (unsigned long)len,
+		    cumulative_time/(double)iterations);
+	}
+	for (unsigned int i=0;i<iterations;i++) {
+	    memcpy(d_array2, d_array, len*sizeof(double));
+	    qtimer_start(timer);
+	    qsort(d_array2, len, sizeof(double), cmp);
+	    qtimer_stop(timer);
+	    cumulative_time += qtimer_secs(timer);
+	}
+	if (interactive == 1) {
+	    printf("sorting %lu doubles took: %f seconds\n",
+		    (unsigned long)len,
+		    cumulative_time/(double)iterations);
+	}
+	free(d_array);
+	free(d_array2);
+    } else {
+	ui_array = calloc(len, sizeof(aligned_t));
+	for (i = 0; i < len; i++) {
+	    ui_array[i] = random();
+	}
+	ui_array2 = calloc(len, sizeof(aligned_t));
+	if (interactive) {
+	    printf("ui_array generated...\n");
+	}
+	for (int i=0;i<10;i++) {
+	    memcpy(ui_array2, ui_array, len*sizeof(aligned_t));
+	    qtimer_start(timer);
+	    qutil_aligned_qsort(qthread_self(), ui_array2, len);
+	    qtimer_stop(timer);
+	    cumulative_time += qtimer_secs(timer);
+	}
+	if (interactive == 1) {
+	    printf("sorting %lu aligned_ts took: %f seconds\n",
+		    (unsigned long)len,
+		    cumulative_time);
+	}
+	free(ui_array);
+	free(ui_array2);
     }
-    ui_array2 = calloc(len, sizeof(aligned_t));
-    if (interactive) {
-	printf("ui_array generated...\n");
-    }
-    for (int i=0;i<10;i++) {
-	memcpy(ui_array2, ui_array, len*sizeof(aligned_t));
-	qtimer_start(timer);
-	qutil_aligned_qsort(qthread_self(), ui_array2, len);
-	qtimer_stop(timer);
-	cumulative_time += qtimer_secs(timer);
-    }
-    if (interactive == 1) {
-	printf("sorting %lu aligned_ts took: %f seconds\n",
-	       (unsigned long)len,
-	       cumulative_time);
-    }
-    free(ui_array);
 
     qtimer_free(timer);
 
