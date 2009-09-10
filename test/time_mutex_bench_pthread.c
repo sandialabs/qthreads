99fa9294a84c1aaa22cfe4afb55539541359556c
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-09-10 16:05:24 +0000

    lul and mutex benchmarks
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1064 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/time_mutex_bench_pthread.c
@@ -0,0 +1,68 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h" /* for _GNU_SOURCE */
+#endif
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <qthread/qthread.h>
+#include <qtimer.h>
+
+#include <pthread.h>
+
+#define NUM_THREADS 10
+#define PER_THREAD_INCR 1000000
+
+pthread_mutex_t locks[PER_THREAD_INCR];
+
+void * qincr(void *arg)
+{
+    size_t incrs;
+
+    for (incrs = 0; incrs < PER_THREAD_INCR; incrs++) {
+	pthread_mutex_lock(&(locks[incrs]));
+	pthread_mutex_unlock(&(locks[incrs]));
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t rets[NUM_THREADS];
+    size_t i;
+    int threads = 0;
+    int interactive = 0;
+    qthread_t *me;
+    qtimer_t timer = qtimer_new();
+    double cumulative_time = 0.0;
+
+    if (argc >= 2) {
+	threads = strtol(argv[1], NULL, 0);
+	if (threads < 0) {
+	    threads = 0;
+	    interactive = 0;
+	} else {
+	    printf("threads: %i\n", threads);
+	    interactive = 1;
+	}
+    }
+
+    for (int i=0; i<PER_THREAD_INCR; i++) {
+	pthread_mutex_init(&(locks[i]), NULL);
+    }
+
+    for (int iteration = 0; iteration < 10; iteration++) {
+	qtimer_start(timer);
+	for (int i=0; i<NUM_THREADS; i++) {
+	    pthread_create(&(rets[i]), NULL, qincr, NULL);
+	}
+	for (int i=0; i<NUM_THREADS; i++) {
+	    pthread_join(rets[i], NULL);
+	}
+	qtimer_stop(timer);
+	cumulative_time += qtimer_secs(timer);
+    }
+    printf("pthread time: %f\n", cumulative_time/10.0);
+
+    return 0;
+}
