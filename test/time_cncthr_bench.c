deb9f9546d80dd106b53576b6c51848c69ebdd67
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-09-10 16:53:32 +0000

    cncthr and thrcrt
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1065 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/time_cncthr_bench.c
@@ -0,0 +1,62 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h" /* for _GNU_SOURCE */
+#endif
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <qthread/qthread.h>
+#include <qtimer.h>
+
+#include <pthread.h>
+
+#define NUM_THREADS 1000000
+
+aligned_t qincr(qthread_t *me, void *arg)
+{
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    aligned_t rets[NUM_THREADS];
+    size_t i;
+    int threads = 0;
+    int interactive = 0;
+    qthread_t *me;
+    qtimer_t timer = qtimer_new();
+    double cumulative_time = 0.0;
+    size_t counter = 0;
+
+    if (argc >= 2) {
+	threads = strtol(argv[1], NULL, 0);
+	if (threads < 0) {
+	    threads = 0;
+	    interactive = 0;
+	} else {
+	    printf("threads: %i\n", threads);
+	    interactive = 1;
+	}
+    }
+
+    if (qthread_init(threads) != QTHREAD_SUCCESS) {
+	fprintf(stderr, "qthread library could not be initialized!\n");
+	exit(EXIT_FAILURE);
+    }
+    me = qthread_self();
+
+    for (int iteration = 0; iteration < 10; iteration++) {
+	qtimer_start(timer);
+	for (int i=0; i<NUM_THREADS; i++) {
+	    qthread_fork(qincr, NULL, &(rets[i]));
+	}
+	for (int i=0; i<NUM_THREADS; i++) {
+	    qthread_readFF(me, NULL, &(rets[i]));
+	}
+	qtimer_stop(timer);
+	cumulative_time += qtimer_secs(timer);
+    }
+    printf("qthread time: %f\n", cumulative_time/10.0);
+
+    qthread_finalize();
+    return 0;
+}
---
+++ test/time_cncthr_bench_pthread.c
@@ -0,0 +1,55 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h" /* for _GNU_SOURCE */
+#endif
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <qthread/qthread.h>
+#include <qtimer.h>
+
+#include <pthread.h>
+
+#define NUM_THREADS 1000000
+
+void * qincr(void *arg)
+{
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t rets[NUM_THREADS];
+    size_t i;
+    int threads = 0;
+    int interactive = 0;
+    qthread_t *me;
+    qtimer_t timer = qtimer_new();
+    double cumulative_time = 0.0;
+    size_t counter;
+
+    if (argc >= 2) {
+	threads = strtol(argv[1], NULL, 0);
+	if (threads < 0) {
+	    threads = 0;
+	    interactive = 0;
+	} else {
+	    printf("threads: %i\n", threads);
+	    interactive = 1;
+	}
+    }
+
+    for (int iteration = 0; iteration < 10; iteration++) {
+	qtimer_start(timer);
+	for (int i=0; i<NUM_THREADS; i++) {
+	    pthread_create(&(rets[i]), NULL, qincr, &counter);
+	}
+	for (int i=0; i<NUM_THREADS; i++) {
+	    pthread_join(rets[i], NULL);
+	}
+	qtimer_stop(timer);
+	cumulative_time += qtimer_secs(timer);
+    }
+    printf("pthread time: %f\n", cumulative_time/10.0);
+
+    return 0;
+}
