5bee7a51565f9029269abb1710093fbcee56013f
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-11-10 01:13:58 +0000

    test cacheline
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1104 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/test_cacheline.c
@@ -0,0 +1,21 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <qthread/qthread.h>
+#include <qthread/cacheline.h>
+
+int main()
+{
+	int cacheline = 0;
+
+    qthread_init(0);
+	cacheline = qthread_cacheline();
+	printf("%i bytes\n", cacheline);
+	assert(cacheline > 0);
+    qthread_finalize();
+    return 0;
+}
