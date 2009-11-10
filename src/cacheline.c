6a6760dd90cd52cb02f46042a1070c6ab8323a59
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2009-11-10 00:39:10 +0000

    pay attention to extended attributes correctly
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1103 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/cacheline.c
+++ src/cacheline.c
@@ -2,79 +2,220 @@
 # include "config.h"
 #endif
 #include <qthread/cacheline.h>
+#include <qthread/common.h>
+#include <stdio.h>
 
 static int cacheline_bytes = 0;
 
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+static void cpuid(int op, int *eax_ptr, int *ebx_ptr, int *ecx_ptr,
+		  int *edx_ptr)
+{				       /*{{{ */
+    int eax, ebx, ecx, edx;
+
+#  if (QTHREAD_ASSEMBLY_ARCH == QTHREAD_IA32)
+#   ifdef __PIC__
+    __asm__ __volatile__("push %%ebx\n\t" "cpuid\n\t" "mov %%ebx, %1\n\t"
+			 "pop %%ebx":"=a"(eax), "=m"(ebx), "=c"(ecx),
+			 "=d"(edx)
+			 :"a"    (op));
+#   else
+    __asm__ __volatile__("cpuid":"=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+			 :"a"    (op));
+#   endif
+#  elif (QTHREAD_ASSEMBLY_ARCH == QTHREAD_AMD64)
+    __asm__ __volatile__("cpuid":"=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+			 :"a"    (op));
+#  endif
+    *eax_ptr = eax;
+    *ebx_ptr = ebx;
+    *ecx_ptr = ecx;
+    *edx_ptr = edx;
+}				       /*}}} */
+
+static void descriptor(int d)
+{				       /*{{{ */
+    switch (d) {
+	case 0x00:
+	    return;
+	case 0x0a:
+	case 0x0c:
+	case 0x41:
+	case 0x42:
+	case 0x43:
+	case 0x44:
+	case 0x45:
+	case 0x82:
+	case 0x83:
+	case 0x84:
+	case 0x85:
+	    cacheline_bytes = MAX(cacheline_bytes, 32);
+	    return;
+	case 0x0d:
+	case 0x21:
+	case 0x22:
+	case 0x23:
+	case 0x25:
+	case 0x29:
+	case 0x2c:
+	case 0x39:
+	case 0x3a:
+	case 0x3b:
+	case 0x3c:
+	case 0x3d:
+	case 0x3e:
+	case 0x46:
+	case 0x47:
+	case 0x48:
+	case 0x49:
+	case 0x4a:
+	case 0x4b:
+	case 0x4c:
+	case 0x4d:
+	case 0x4e:
+	case 0x60:
+	case 0x66:
+	case 0x67:
+	case 0x68:
+	case 0x78:
+	case 0x79:
+	case 0x7a:
+	case 0x7b:
+	case 0x7c:
+	case 0x7d:
+	case 0x7f:
+	case 0x86:
+	case 0x87:
+	case 0xd0:
+	case 0xd1:
+	case 0xd2:
+	case 0xd6:
+	case 0xd7:
+	case 0xd8:
+	case 0xdc:
+	case 0xdd:
+	case 0xde:
+	case 0xe2:
+	case 0xe3:
+	case 0xe4:
+	case 0xea:
+	case 0xeb:
+	case 0xec:
+	    cacheline_bytes = MAX(cacheline_bytes, 64);
+	    return;
+	case 0x01:
+	case 0x02:
+	case 0x03:
+	case 0x04:
+	case 0x06:
+	case 0x08:
+	case 0x09:
+	case 0x30:
+	case 0x40:
+	case 0x50:
+	case 0x51:
+	case 0x52:
+	case 0x55:
+	case 0x56:
+	case 0x57:
+	case 0x5a:
+	case 0x5b:
+	case 0x5c:
+	case 0x5d:
+	case 0x70:
+	case 0x71:
+	case 0x72:
+	case 0x73:
+	case 0xb0:
+	case 0xb1:
+	case 0xb2:
+	case 0xb3:
+	case 0xb4:
+	case 0xca:
+	case 0xf0:
+	case 0xf1:
+	    return;
+	default:		       /*printf("no idea: %02x\n", d); */
+	    return;
+    }
+}				       /*}}} */
+
+static void examine(int r, char *str)
+{				       /*{{{ */
+    if ((r & 0x40000000) == 0) {
+	descriptor((r >> 0) & 0xff);
+	descriptor((r >> 8) & 0xff);
+	descriptor((r >> 16) & 0xff);
+	descriptor((r >> 24) & 0xff);
+    }
+}				       /*}}} */
+
 static void figure_out_cacheline_size()
-{
+{				       /*{{{ */
 #if (QTHREAD_ASSEMBLY_ARCH == QTHREAD_POWERPC32) || \
     (QTHREAD_ASSEMBLY_ARCH == QTHREAD_POWERPC64)
     if (sizeof(long) == 4) {
-	cacheline_bytes = 32; // G4
+	cacheline_bytes = 32;	       // G4
     } else {
-	cacheline_bytes == 128; // G5
+	cacheline_bytes == 128;	       // G5
     }
 #elif (QTHREAD_ASSEMBLY_ARCH == QTHREAD_SPARCV9_32) || \
       (QTHREAD_ASSEMBLY_ARCH == QTHREAD_SPARCV9_64)
     cacheline_bytes = 128;
 #elif (QTHREAD_ASSEMBLY_ARCH == QTHREAD_IA64)
-    cacheline_bytes = 128; // Itanium L2/L3 are 128, L1 is 64
+    cacheline_bytes = 128;	       // Itanium L2/L3 are 128, L1 is 64
 #elif (QTHREAD_ASSEMBLY_ARCH == QTHREAD_IA32) || \
       (QTHREAD_ASSEMBLY_ARCH == QTHREAD_AMD64)
 # if !defined(HAVE_GCC_INLINE_ASSEMBLY)
     cacheline_bytes = 128;
 # else
-    int op = 1, eax, ebx, ecx, edx;
+    int eax, ebx, ecx, edx;
+
     int tmp = 0;
-#  if (QTHREAD_ASSEMBLY_ARCH == QTHREAD_IA32)
-#   ifdef __PIC__
-    __asm__ __volatile__ ("push %%ebx\n\t"
-			  "cpuid\n\t"
-			  "mov %%ebx, %1\n\t"
-			  "pop %%ebx"
-			  :"=a"(eax), "=m"(ebx), "=c"(ecx), "=d"(edx)
-			  :"a"(op));
-#   else
-    __asm__ __volatile__("cpuid"
-	    :"=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-	    :"a"(op));
-#   endif
-#  elif (QTHREAD_ASSEMBLY_ARCH == QTHREAD_AMD64)
-    __asm__ __volatile__("cpuid"
-	    :"=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-	    :"a"(op));
-#  endif
-    tmp = 8*((ebx>>8)&0xff);
-    if (tmp == 0) {
-	op = 2;
-#  if (QTHREAD_ASSEMBLY_ARCH == QTHREAD_IA32)
-	__asm__ __volatile__ ("push %%ebx\n\t"
-		"cpuid\n\t"
-		"mov %%ebx, %1\n\t"
-		"pop %ebx"
-		:"=a"(eax), "=m"(ebx), "=c"(ecx), "=d"(edx)
-		:"a"(op));
-#  elif (QTHREAD_ASSEMBLY_ARCH == QTHREAD_AMD64)
-	__asm__ __volatile__ ("cpuid"
-		:"=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-		:"a"(op));
-#  endif
-	tmp = 8*((ebx>>8)&0xff); /* XXX THIS IS PROBABLY WRONG */
+
+    cpuid(1, &eax, &ebx, &ecx, &edx);
+    tmp = 8 * ((ebx >> 8) & 0xff);     // The clflush width, according to AMD
+    cacheline_bytes = MAX(cacheline_bytes, tmp);
+    {
+	int i = 1;
+
+	int limit;
+
+	do {
+	    cpuid(2, &eax, &ebx, &ecx, &edx);
+	    limit = eax & 0xf;
+	    i++;
+	    examine(eax, "eax");
+	    examine(ebx, "ebx");
+	    examine(ecx, "ecx");
+	    examine(edx, "edx");
+	} while (i < limit);
     }
-    cacheline_bytes = tmp;
+    {
+	cpuid(0x80000005, &eax, &ebx, &ecx, &edx);
+	tmp = (ecx >> 8) & 0xff;
+	cacheline_bytes = MAX(cacheline_bytes, tmp);
+    }
+    {
+	cpuid(0x80000006, &eax, &ebx, &ecx, &edx);
+	tmp = (ecx >> 8) & 0xff;
+	cacheline_bytes = MAX(cacheline_bytes, tmp);
+    }
 # endif
 #else
-    cacheline_bytes = 128; // safe default, probably not accurate
+    cacheline_bytes = 128;	       // safe default, probably not accurate
 #endif
-}
+}				       /*}}} */
 
 /* returns the cache line size */
 int qthread_cacheline()
-{
+{				       /*{{{ */
     if (cacheline_bytes == 0) {
 	figure_out_cacheline_size();
-	if (cacheline_bytes == 0) /* to cache errors in cacheline detection */
+	if (cacheline_bytes == 0) {    /* to cache errors in cacheline detection */
 	    cacheline_bytes = 128;
+	}
     }
     return cacheline_bytes;
-}
+}				       /*}}} */
