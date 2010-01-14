bdc252e40234e312b12d039afe6ca76a29f9b432
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-01-14 17:31:37 +0000

    more configurable
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1219 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_qlfqueue.c
+++ test/test_qlfqueue.c
@@ -5,15 +5,15 @@
 #include <qthread/qlfqueue.h>
 #include "argparsing.h"
 
-#define ELEMENT_COUNT 10000
-#define THREAD_COUNT 128
+static int elementcount = 10000;
+static int threadcount = 128;
 
 static aligned_t queuer (qthread_t *me, void *arg)
 {
     qlfqueue_t *q = (qlfqueue_t*)arg;
     size_t i;
 
-    for (i = 0; i < ELEMENT_COUNT; i++) {
+    for (i = 0; i < elementcount; i++) {
 	if (qlfqueue_enqueue(me, q, (void*)me) != QTHREAD_SUCCESS) {
 	    fprintf(stderr, "qlfqueue_enqueue(q, %p) failed!\n", (void*)me);
 	    exit(-2);
@@ -27,11 +27,12 @@ static aligned_t dequeuer (qthread_t *me, void *arg)
     qlfqueue_t *q = (qlfqueue_t*)arg;
     size_t i;
 
-    for (i = 0; i < ELEMENT_COUNT; i++) {
+    for (i = 0; i < elementcount; i++) {
 	while (qlfqueue_dequeue(me, q) == NULL) {
 	    qthread_yield(me);
 	}
     }
+    iprintf("dequeuer %i exiting\n", qthread_id(me));
     return 0;
 }
 
@@ -44,7 +45,10 @@ int main(int argc, char *argv[])
 
     assert(qthread_initialize() == 0);
     me = qthread_self();
+    NUMARG(threadcount, "THREAD_COUNT");
+    NUMARG(elementcount, "ELEMENT_COUNT");
     CHECK_VERBOSE();
+    iprintf("%i shepherds\n", qthread_num_shepherds());
 
     if ((q = qlfqueue_create()) == NULL) {
 	fprintf(stderr, "qlfqueue_create() failed!\n");
@@ -61,13 +65,13 @@ int main(int argc, char *argv[])
 	exit(-1);
     }
 
-    for (i = 0; i < ELEMENT_COUNT; i++) {
+    for (i = 0; i < elementcount; i++) {
 	if (qlfqueue_enqueue(me, q, (void*)(intptr_t)(i+1)) != 0) {
 	    fprintf(stderr, "qlfqueue_enqueue(q,%i) failed!\n", (int)i);
 	    exit(-1);
 	}
     }
-    for (i = 0; i < ELEMENT_COUNT; i++) {
+    for (i = 0; i < elementcount; i++) {
 	if (qlfqueue_dequeue(me, q) != (void*)(intptr_t)(i+1)) {
 	    fprintf(stderr, "qlfqueue_dequeue() failed, didn't equal %i!\n", (int)i);
 	    exit(-1);
@@ -79,15 +83,18 @@ int main(int argc, char *argv[])
     }
     iprintf("ordering test succeeded\n");
 
-    rets = calloc(THREAD_COUNT, sizeof(aligned_t));
+    rets = calloc(threadcount, sizeof(aligned_t));
     assert(rets != NULL);
-    for (i = 0; i < THREAD_COUNT; i++) {
+    for (i = 0; i < threadcount; i++) {
 	assert(qthread_fork(dequeuer, q, &(rets[i])) == QTHREAD_SUCCESS);
     }
-    for (i = 0; i < THREAD_COUNT; i++) {
+    iprintf("dequeuers forked\n");
+    for (i = 0; i < threadcount; i++) {
 	assert(qthread_fork(queuer, q, NULL) == QTHREAD_SUCCESS);
     }
-    for (i = 0; i < THREAD_COUNT; i++) {
+    iprintf("queuers forked\n");
+    for (i = 0; i < threadcount; i++) {
+	iprintf("waiting for %i\n", i);
 	assert(qthread_readFF(me, NULL, &(rets[i])) == QTHREAD_SUCCESS);
     }
     free(rets);
