6fab279df3e51f69a510388815bbb68f9672f8cf
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-02-08 19:32:17 +0000

    export new qqloop functions
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1329 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread/qloop.h
+++ include/qthread/qloop.h
@@ -13,6 +13,8 @@ typedef void (*qt_loopr_f) (qthread_t * me, const size_t startat,
 			    void *restrict ret);
 typedef void (*qt_accum_f) (void *restrict a, void *restrict b);
 
+typedef struct qqloop_handle_s qqloop_handle_t;
+
 void qt_loop(const size_t start, const size_t stop, const size_t stride,
 	     const qthread_f func, void *argptr);
 void qt_loop_future(const size_t start, const size_t stop,
@@ -29,6 +31,12 @@ void qt_loopaccum_balance_future(const size_t start, const size_t stop,
 				 const size_t size, void *restrict out,
 				 const qt_loopr_f func, void *restrict argptr,
 				 const qt_accum_f acc);
+qqloop_handle_t *qt_loop_queue_create(const size_t start, const size_t stop,
+				      const qt_loop_f func,
+				      void *const argptr);
+void qt_loop_queue_run(qqloop_handle_t *loop);
+void qt_loop_queue_run_there(qqloop_handle_t *loop, qthread_shepherd_id_t shep);
+void qt_loop_queue_addworker(qqloop_handle_t *loop, const qthread_shepherd_id_t shep);
 
 double qt_double_sum(double *array, size_t length, int checkfeb);
 double qt_double_prod(double *array, size_t length, int checkfeb);
@@ -50,51 +58,62 @@ static Q_UNUSED void qt_dbl_add_acc(void *restrict a, void *restrict b)
 {
     *(double *)a += *(double *)b;
 }
+
 static Q_UNUSED void qt_int_add_acc(void *restrict a, void *restrict b)
 {
     *(saligned_t *) a += *(saligned_t *) b;
 }
+
 static Q_UNUSED void qt_uint_add_acc(void *restrict a, void *restrict b)
 {
     *(aligned_t *) a += *(aligned_t *) b;
 }
+
 static Q_UNUSED void qt_dbl_prod_acc(void *restrict a, void *restrict b)
 {
     *(double *)a *= *(double *)b;
 }
+
 static Q_UNUSED void qt_int_prod_acc(void *restrict a, void *restrict b)
 {
     *(saligned_t *) a *= *(saligned_t *) b;
 }
+
 static Q_UNUSED void qt_uint_prod_acc(void *restrict a, void *restrict b)
 {
     *(aligned_t *) a *= *(aligned_t *) b;
 }
+
 static Q_UNUSED void qt_dbl_max_acc(void *restrict a, void *restrict b)
 {
     if (*(double *)b > *(double *)a)
 	*(double *)a = *(double *)b;
 }
+
 static Q_UNUSED void qt_int_max_acc(void *restrict a, void *restrict b)
 {
     if (*(saligned_t *) b > *(saligned_t *) a)
 	*(saligned_t *) a = *(saligned_t *) b;
 }
+
 static Q_UNUSED void qt_uint_max_acc(void *restrict a, void *restrict b)
 {
     if (*(aligned_t *) b > *(aligned_t *) a)
 	*(aligned_t *) a = *(aligned_t *) b;
 }
+
 static Q_UNUSED void qt_dbl_min_acc(void *restrict a, void *restrict b)
 {
     if (*(double *)b < *(double *)a)
 	*(double *)a = *(double *)b;
 }
+
 static Q_UNUSED void qt_int_min_acc(void *restrict a, void *restrict b)
 {
     if (*(saligned_t *) b < *(saligned_t *) a)
 	*(saligned_t *) a = *(saligned_t *) b;
 }
+
 static Q_UNUSED void qt_uint_min_acc(void *restrict a, void *restrict b)
 {
     if (*(aligned_t *) b < *(aligned_t *) a)
