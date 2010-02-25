b13e0fe4fb8a33822e72145be8c358cc6926a243
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-02-25 21:13:12 +0000

    fix configure generation to handle dynamic version number
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1391 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,12 @@
 
 echo "Generating configure files..."
 
+# If this directory isn't removed, the configure script may not have the right
+# dynamically-generated version number
+if [ -d autom4te.cache ] ; then
+	rm -rf autom4te.cache
+fi
+
 autoreconf --install --symlink --warnings=gnu,obsolete,override,portability,no-obsolete && \
   echo "Preparing was successful if there was no error messages above." && \
   exit 0
