dc3c4edc96df4db80c297ec600f354d8ca2bbdff
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-03-01 18:38:38 +0000

    make qtimer always available
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1396 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/test_qtimer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include <qthread/qthread.h>
+#include <qthread/qtimer.h>
+#include "argparsing.h"
+
+int main()
+{
+    int i;
+    qthread_t *me;
+    qtimer_t t;
+
+    assert(qthread_initialize() == QTHREAD_SUCCESS);
+    me = qthread_self();
+
+    CHECK_VERBOSE();
+
+    t = qtimer_new();
+    assert(t);
+    qtimer_start(t);
+    me = qthread_self();
+    qtimer_stop(t);
+    assert(qtimer_secs(t) > 0);
+    iprintf("t = %f secs\n", qtimer_secs(t));
+    qtimer_free(t);
+
+    return 0;
+}
