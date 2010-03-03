f9eb2a744e6b6fb19f9646e3c60eace48fee0474
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-03-03 21:08:36 +0000

    QTHREAD_CASLOCK2 for initializing
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1418 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qt_atomics.h
+++ include/qt_atomics.h
@@ -36,6 +36,7 @@
 
 #ifdef QTHREAD_MUTEX_INCREMENT
 # define QTHREAD_CASLOCK(var)	var; QTHREAD_FASTLOCK_TYPE var##_caslock
+# define QTHREAD_CASLOCK2(var)	var = NULL; QTHREAD_FASTLOCK_TYPE var##_caslock
 # define QTHREAD_CASLOCK_INIT(var,i)   var = i; QTHREAD_FASTLOCK_INIT(var##_caslock)
 # define QTHREAD_CASLOCK_DESTROY(var)	QTHREAD_FASTLOCK_DESTROY(var##_caslock)
 # define QTHREAD_CASLOCK_READ(var)   (void*)qt_cas_read_ui((volatile uintptr_t*)&(var), &(var##_caslock))
@@ -62,6 +63,7 @@ static QINLINE uintptr_t qt_cas_read_ui(volatile uintptr_t * const ptr, QTHREAD_
 }
 #else
 # define QTHREAD_CASLOCK(var)	(var)
+# define QTHREAD_CASLOCK2(var)	(var = NULL)
 # define QTHREAD_CASLOCK_INIT(var,i) (var) = i
 # define QTHREAD_CASLOCK_DESTROY(var)
 # define QTHREAD_CASLOCK_READ(var) (var)
