d2b828f6c2715d0c5a18913c1d46d19f2ccd0d1f
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-03-24 22:03:25 +0000

    test loop queues
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1440 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/test_queue_loop.c
@@ -0,0 +1,71 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"		       /* for _GNU_SOURCE */
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>		       /* for INT_MIN & friends (according to C89) */
+#include <float.h>		       /* for DBL_MIN & friends (according to C89) */
+#include <sys/time.h>
+#include <time.h>
+#include <qthread/qloop.h>
+#include <qthread/qtimer.h>
+#include "argparsing.h"
+
+//#define BIGLEN 200000000U
+static unsigned int BIGLEN = 1000000U;
+aligned_t *uia = NULL;
+
+void sum(qthread_t *me, const size_t startat, const size_t stopat, void *arg_)
+{
+    size_t i;
+    aligned_t local_sum = 0;
+    //printf("S%i: summing %i numbers, from %i to %i\n", (int)qthread_shep(me), (int)(stopat-startat), (int)startat, (int)stopat);
+    for (i=startat; i<stopat; i++) {
+	local_sum += uia[i];
+    }
+    qthread_incr((aligned_t*)arg_, local_sum);
+}
+
+int main(int argc, char *argv[])
+{
+    size_t i;
+    struct timeval start, stop;
+
+    assert(qthread_initialize() == QTHREAD_SUCCESS);
+    CHECK_VERBOSE();
+    NUMARG(BIGLEN, "BIGLEN");
+    future_init(128);
+
+    {
+	aligned_t uitmp = 0, uisum = 0;
+	qqloop_handle_t *loophandle;
+	qtimer_t t = qtimer_create();
+
+	uia = (aligned_t *) malloc(sizeof(aligned_t) * BIGLEN);
+	assert(uia);
+	for (i = 0; i < BIGLEN; i++) {
+	    uia[i] = random();
+	}
+	qtimer_start(t);
+	for (i = 0; i < BIGLEN; i++)
+	    uisum += uia[i];
+	qtimer_stop(t);
+	iprintf("summing-serial   %u uints took %g seconds\n",
+		BIGLEN, qtimer_secs(t));
+	iprintf("\tsum was %lu\n", (unsigned long)uisum);
+	loophandle = qt_loop_queue_create(0, BIGLEN, sum, &uitmp);
+	qtimer_start(t);
+	qt_loop_queue_run(loophandle);
+	qtimer_stop(t);
+	iprintf("summing-parallel %u uints took %g seconds\n",
+		BIGLEN, qtimer_secs(t));
+	iprintf("\tsum was %lu\n", (unsigned long)uitmp);
+	assert(uitmp == uisum);
+
+	free(uia);
+	qtimer_destroy(t);
+    }
+
+    return 0;
+}
