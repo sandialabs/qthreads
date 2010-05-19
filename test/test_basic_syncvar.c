17e60fdd460914451d2c49832d4a81e7d9885405
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-05-19 20:11:23 +0000

    use writeF_const
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1537 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_basic_syncvar.c
+++ test/test_basic_syncvar.c
@@ -16,8 +16,10 @@ static aligned_t consumer(qthread_t * t, void *arg)
 
     iprintf("consumer(%p) locking id(%p)\n", t, &id);
     qthread_syncvar_readFE(t, &me, &id);
-    iprintf("consumer(%p) id's status became: %u\n", t, qthread_syncvar_status(&id));
-    iprintf("consumer(%p) unlocking id(%p), result is %lu\n", t, &id, (unsigned long)me);
+    iprintf("consumer(%p) id's status became: %u\n", t,
+	    qthread_syncvar_status(&id));
+    iprintf("consumer(%p) unlocking id(%p), result is %lu\n", t, &id,
+	    (unsigned long)me);
     me++;
     qthread_syncvar_writeEF(t, &id, &me);
 
@@ -34,11 +36,13 @@ static aligned_t producer(qthread_t * t, void *arg)
 
     iprintf("producer(%p) locking id(%p)\n", t, &id);
     qthread_syncvar_readFE(t, &me, &id);
-    iprintf("producer(%p) unlocking id(%p), result is %lu\n", t, &id, (unsigned long)me);
+    iprintf("producer(%p) unlocking id(%p), result is %lu\n", t, &id,
+	    (unsigned long)me);
     me++;
     qthread_syncvar_writeEF(t, &id, &me);
 
-    iprintf("producer(%p) x's status is: %u (expect empty)\n", t, qthread_syncvar_status(&x));
+    iprintf("producer(%p) x's status is: %u (expect empty)\n", t,
+	    qthread_syncvar_status(&x));
     iprintf("producer(%p) filling x(%p)\n", t, &x);
     qthread_syncvar_writeEF(t, &x, &res);
 
@@ -57,24 +61,25 @@ int main(int argc, char *argv[])
     iprintf("%i threads...\n", qthread_num_shepherds());
     iprintf("Initial value of x: %lu\n", (unsigned long)x.u.w);
 
-    {
-	uint64_t t = 1;
-	qthread_syncvar_writeF(qthread_self(), &id, &t);
-    }
+    qthread_syncvar_writeF_const(qthread_self(), &id, 1);
     iprintf("id = 0x%lx\n", (unsigned long)id.u.w);
-    { uint64_t t = 0;
-	qthread_syncvar_readFF(qthread_self(), &t, &id);
-	assert(t == 1);
+    {
+	uint64_t tmp = 0;
+	qthread_syncvar_readFF(qthread_self(), &tmp, &id);
+	assert(tmp == 1);
     }
-    iprintf("x's status is: %u (want 1; full nowait)\n", qthread_syncvar_status(&x));
+    iprintf("x's status is: %u (want 1; full nowait)\n",
+	    qthread_syncvar_status(&x));
     assert(qthread_syncvar_status(&x) == 1);
     qthread_syncvar_readFE(qthread_self(), NULL, &x);
-    iprintf("x's status became: %u (want 0; empty nowait)\n", qthread_syncvar_status(&x));
+    iprintf("x's status became: %u (want 0; empty nowait)\n",
+	    qthread_syncvar_status(&x));
     assert(qthread_syncvar_status(&x) == 0);
     qthread_fork(consumer, NULL, NULL);
     qthread_fork(producer, NULL, &t);
     qthread_readFF(qthread_self(), NULL, &t);
-    iprintf("blocking on x (current status: %u)\n", qthread_syncvar_status(&x));
+    iprintf("blocking on x (current status: %u)\n",
+	    qthread_syncvar_status(&x));
     qthread_syncvar_readFF(qthread_self(), &x_value, &x);
     assert(qthread_syncvar_status(&x) == 1);
 
