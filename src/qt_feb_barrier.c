5a3b0a83034f54b507ee604d667b31262c378bf5
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-07-23 15:24:14 +0000

    fix race condition in feb barrier
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1621 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/qt_feb_barrier.c
+++ src/qt_feb_barrier.c
@@ -15,7 +15,8 @@
 
 struct qt_feb_barrier_s
 {
-    aligned_t block_on_me;
+    syncvar_t in_gate;
+    syncvar_t out_gate;
     aligned_t blockers;
     size_t max_blockers;
 };
@@ -57,21 +58,35 @@ qt_feb_barrier_t *qt_feb_barrier_create(qthread_t *me, size_t max_threads)
 #endif
     b->blockers = 0;
     b->max_blockers = max_threads;
-    qthread_empty(me, &b->block_on_me);
+    b->in_gate.u.w = 0;
+    b->out_gate.u.w = 0;
+    qthread_syncvar_fill(me, &b->in_gate);
+    qthread_syncvar_empty(me, &b->out_gate);
     return b;
 }
 
 void qt_feb_barrier_enter(qthread_t *me, qt_feb_barrier_t *b)
 {
-    aligned_t waiters = qthread_incr(&b->blockers, 1) + 1;
+    aligned_t waiters;
     qassert_retvoid(b);
+    /* pass through the in_gate */
+    qthread_syncvar_readFF(me, NULL, &b->in_gate);
+    /* increment the blocker count */
+    waiters = qthread_incr(&b->blockers, 1) + 1;
     if (waiters == b->max_blockers) {
-	/* last guy into the barrier */
-	qthread_fill(me, &b->block_on_me);
-	b->blockers = 0;
-	qthread_empty(me, &b->block_on_me);
+	/* last guy into the barrier, close the in_gate, open the out_gate */
+	qthread_syncvar_empty(me, &b->in_gate);
+	qthread_syncvar_fill(me, &b->out_gate);
     } else {
-	qthread_readFF(me, NULL, &b->block_on_me);
+	/* pass through the out_gate */
+	qthread_syncvar_readFF(me, NULL, &b->out_gate);
+    }
+    /* I'm on the way out, so decrement the blocker count */
+    waiters = qthread_incr(&b->blockers, -1) - 1;
+    if (waiters == 0) {
+	/* last guy out of the barrier, close the out_gate, open the in_gate */
+	qthread_syncvar_empty(me, &b->out_gate);
+	qthread_syncvar_fill(me, &b->in_gate);
     }
 }
 
@@ -79,7 +94,8 @@ void qt_feb_barrier_destroy(qthread_t *me, qt_feb_barrier_t *b)
 {
     assert(feb_barrier_pool != NULL);
     assert(b->blockers == 0);
-    qthread_fill(me, &b->block_on_me);
+    qthread_syncvar_fill(me, &b->out_gate);
+    qthread_syncvar_fill(me, &b->in_gate);
 #ifndef UNPOOLED
     qt_mpool_free(feb_barrier_pool, b);
 #else
