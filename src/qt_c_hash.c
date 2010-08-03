540df04f7db432db562783598f409e68eff6b58f
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-08-03 22:20:15 +0000

    whitespace
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1629 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/qt_c_hash.c
+++ src/qt_c_hash.c
@@ -6,7 +6,7 @@
 #include "qt_atomics.h"
 #include "qthread/cacheline.h"
 #include <stdlib.h>
-#include <unistd.h> /* for getpagesize() */
+#include <unistd.h>		       /* for getpagesize() */
 
 #ifndef QT_HASH_CAST
 #define QT_HASH_CAST qt_key_t
@@ -23,8 +23,8 @@ struct qt_hash_s {
     uint64_t mask;
     size_t num_entries;
     size_t population, deletes;
-    size_t grow_size, shrink_size, tidy_up_size; // cache for speed
-    void *value[2]; //handle out-of-bound values
+    size_t grow_size, shrink_size, tidy_up_size;	// cache for speed
+    void *value[2];		//handle out-of-bound values
     short has_key[2];
 };
 
@@ -34,33 +34,39 @@ static size_t bucketmask;
 #define KEY_NULL ((qt_key_t)0)
 #define KEY_DELETED ((qt_key_t)1)
 
-static inline size_t encompassing_power_of_two(size_t k)
+static inline size_t encompassing_power_of_two(
+    size_t k)
 {
     size_t z = 1;
-    while (z < k) z<<=1;
+    while (z < k)
+	z <<= 1;
     assert((z >> 1) <= k && z >= k);
     return z;
 }
-static void qt_hash_internal_create(qt_hash ret, size_t entries)
+
+static inline void qt_hash_internal_create(
+    qt_hash ret,
+    size_t entries)
 {
     size_t min_entries = 2 * getpagesize() / sizeof(hash_entry);
     /* meta data */
     if (entries % min_entries != 0) {
 	entries += min_entries - (entries % min_entries);
     }
-    entries = encompassing_power_of_two(entries); // needs to be a power of two to make the masking work right (which is key to speed)
+    entries = encompassing_power_of_two(entries);	// needs to be a power of two to make the masking work right (which is key to speed)
     ret->num_entries = entries;
-    ret->mask = (entries-1) & ~bucketmask;
+    ret->mask = (entries - 1) & ~bucketmask;
     /* triggers */
-    ret->grow_size = (entries*0.65f) - 1;
-    ret->tidy_up_size = (entries*0.8f) - 1;
+    ret->grow_size = (entries * 0.65f) - 1;
+    ret->tidy_up_size = (entries * 0.8f) - 1;
     if (ret->num_entries == min_entries) {
 	ret->shrink_size = 0;
     } else {
 	ret->shrink_size = entries * 0.030f;
     }
     /* data storage */
-    posix_memalign((void**)&(ret->entries), linesize, sizeof(hash_entry) * entries);
+    posix_memalign((void **)&(ret->entries), linesize,
+		   sizeof(hash_entry) * entries);
     if (ret->entries) {
 	memset(ret->entries, 0, sizeof(hash_entry) * entries);
     } else {
@@ -68,50 +74,65 @@ static void qt_hash_internal_create(qt_hash ret, size_t entries)
 	ret = NULL;
     }
 }
+
 /* this function based on http://burtleburtle.net/bob/hash/evahash.html */
 #define rot(x,k)  (((x)<<(k)) | ((x)>>(32-(k))))
-static uint64_t qt_hashword(uint64_t key)
+static uint64_t qt_hashword(
+    uint64_t key)
 {
     uint32_t a, b, c;
     const union {
 	uint64_t key;
 	uint8_t b[sizeof(uint64_t)];
-    } k = { key };
+    }
+    k = {
+    key};
 
     a = b = c = 0xdeadbeef + sizeof(uint64_t);
     c += 47;
 
-    b+=k.b[7]<<24;
-    b+=k.b[6]<<16;
-    b+=k.b[5]<<8;
-    b+=k.b[4];
-    a+=k.b[3]<<24;
-    a+=k.b[2]<<16;
-    a+=k.b[1]<<8;
-    a+=k.b[0];
+    b += k.b[7] << 24;
+    b += k.b[6] << 16;
+    b += k.b[5] << 8;
+    b += k.b[4];
+    a += k.b[3] << 24;
+    a += k.b[2] << 16;
+    a += k.b[1] << 8;
+    a += k.b[0];
 
-    c^=b; c-=rot(b,14);
-    a^=c; a-=rot(c,11);
-    b^=a; b-=rot(a,25);
-    c^=b; c-=rot(b,16);
-    a^=c; a-=rot(c,4);
-    b^=a; b-=rot(a,14);
-    c^=b; c-=rot(b,24);
-    return ((uint64_t)c + ((uint64_t)b<<32));
+    c ^= b;
+    c -= rot(b, 14);
+    a ^= c;
+    a -= rot(c, 11);
+    b ^= a;
+    b -= rot(a, 25);
+    c ^= b;
+    c -= rot(b, 16);
+    a ^= c;
+    a -= rot(c, 4);
+    b ^= a;
+    b -= rot(a, 14);
+    c ^= b;
+    c -= rot(b, 24);
+    return ((uint64_t) c + ((uint64_t) b << 32));
 }
 
-static inline void** qt_hash_internal_find(qt_hash h, qt_key_t key)
+static inline void **qt_hash_internal_find(
+    qt_hash h,
+    qt_key_t key)
 {
     const hash_entry *z = h->entries;
     const uint64_t mask = h->mask;
-    const uint64_t hashed = qt_hashword((uint64_t)key);
+    const uint64_t hashed = qt_hashword((uint64_t) key);
 
     uint64_t bucket = hashed & mask;
     uint64_t step, quit;
 
-    for (unsigned i = 0; i<bucketsize; ++i) {
-	if (z[bucket+i].key == key) return (void*)&z[bucket+i].value;
-	if (z[bucket+i].key == KEY_NULL) return NULL;
+    for (unsigned i = 0; i < bucketsize; ++i) {
+	if (z[bucket + i].key == key)
+	    return (void *)&z[bucket + i].value;
+	if (z[bucket + i].key == KEY_NULL)
+	    return NULL;
     }
 
     step = (((hashed >> 16) | (hashed << 16)) & mask) | bucketsize;
@@ -119,15 +140,18 @@ static inline void** qt_hash_internal_find(qt_hash h, qt_key_t key)
     do {
 	unsigned i;
 	bucket = (bucket + step) & mask;
-	for (i = 0; i<bucketsize; ++i) {
-	    if (z[bucket+i].key == key) return (void*)&z[bucket+i].value;
-	    if (z[bucket+i].key == KEY_NULL) return NULL;
+	for (i = 0; i < bucketsize; ++i) {
+	    if (z[bucket + i].key == key)
+		return (void *)&z[bucket + i].value;
+	    if (z[bucket + i].key == KEY_NULL)
+		return NULL;
 	}
     } while (bucket != quit);
     return NULL;
 }
 
-qt_hash qt_hash_create(int needSync)
+qt_hash qt_hash_create(
+    int needSync)
 {
     qt_hash ret;
     if (linesize == 0) {
@@ -148,7 +172,8 @@ qt_hash qt_hash_create(int needSync)
     return ret;
 }
 
-void qt_hash_destroy(qt_hash h)
+void qt_hash_destroy(
+    qt_hash h)
 {
     assert(h);
     if (h->lock) {
@@ -163,7 +188,9 @@ void qt_hash_destroy(qt_hash h)
 
 /* This function destroys the hash and applies the given deallocator function
  * to each value stored in the hash */
-void qt_hash_destroy_deallocate(qt_hash h, qt_hash_deallocator_fn f)
+void qt_hash_destroy_deallocate(
+    qt_hash h,
+    qt_hash_deallocator_fn f)
 {
     size_t visited = 0;
     assert(h);
@@ -180,7 +207,7 @@ void qt_hash_destroy_deallocate(qt_hash h, qt_hash_deallocator_fn f)
     }
     if (visited < h->population) {
 	size_t i;
-	for (i=0; i<h->num_entries; ++i) {
+	for (i = 0; i < h->num_entries; ++i) {
 	    if (h->entries[i].key > KEY_DELETED) {
 		++visited;
 		f(h->entries[i].value);
@@ -197,7 +224,10 @@ void qt_hash_destroy_deallocate(qt_hash h, qt_hash_deallocator_fn f)
     qt_hash_destroy(h);
 }
 
-void *qt_hash_put(qt_hash h, const qt_key_t key, void *value)
+void *qt_hash_put(
+    qt_hash h,
+    const qt_key_t key,
+    void *value)
 {
     void *v;
     assert(h);
@@ -212,20 +242,23 @@ void *qt_hash_put(qt_hash h, const qt_key_t key, void *value)
     return v;
 }
 
-static void brehash(qt_hash h, size_t len)
+static void brehash(
+    qt_hash h,
+    size_t len)
 {
     qt_hash d = calloc(1, sizeof(struct qt_hash_s));
     size_t i, copied;
 
     assert(d);
     qt_hash_internal_create(d, len);
-    memcpy(d->has_key, h->has_key, sizeof(short)*2);
-    memcpy(d->value, h->value, sizeof(void*)*2);
+    memcpy(d->has_key, h->has_key, sizeof(short) * 2);
+    memcpy(d->value, h->value, sizeof(void *) * 2);
     copied = h->has_key[0] + h->has_key[1];
     if (copied < h->population) {
-	for (i=0; i<h->num_entries; ++i) {
+	for (i = 0; i < h->num_entries; ++i) {
 	    if (h->entries[i].key > KEY_DELETED) {
-		qassertnot(qt_hash_put_locked(d, h->entries[i].key, h->entries[i].value), NULL);
+		qassertnot(qt_hash_put_locked
+			   (d, h->entries[i].key, h->entries[i].value), NULL);
 		++copied;
 		if (copied == h->population) {
 		    break;
@@ -239,10 +272,13 @@ static void brehash(qt_hash h, size_t len)
     free(d);
 }
 
-void *qt_hash_put_locked(qt_hash h, const qt_key_t key, void *value)
+void *qt_hash_put_locked(
+    qt_hash h,
+    const qt_key_t key,
+    void *value)
 {
-    hash_entry *z; // for speed, to avoid extra ptr dereferences
-    uint64_t mask; // for speed, to avoid extra ptr dereferences
+    hash_entry *z;		// for speed, to avoid extra ptr dereferences
+    uint64_t mask;		// for speed, to avoid extra ptr dereferences
     int done;
     ssize_t f;
     size_t i;
@@ -251,27 +287,29 @@ void *qt_hash_put_locked(qt_hash h, const qt_key_t key, void *value)
     assert(h);
     qassert_ret(key != KEY_DELETED && key != KEY_NULL, NULL);
 
-restart:
+  restart:
     z = h->entries;
     mask = h->mask;
     done = 0;
-    f=-1;
+    f = -1;
 
-    hw = qt_hashword((uint64_t)(intptr_t)key);
+    hw = qt_hashword((uint64_t) (intptr_t) key);
     bucket = hw & mask;
 
     /* find the key in the bucket
      * - otherwise insert in the first DELETED bucket
      * - otherwise insert in the first NULL bucket
      * - otherwise the bucket is full (i.e. we're not done) */
-    for (i=0; i<bucketsize;++i) {
-	if (z[bucket+i].key == key) {
-	    z[bucket+i].value = value;
-	    return &z[bucket+i].value;
-	} else if (z[bucket+i].key == KEY_DELETED) {
-	    if (f == -1) f = bucket+i;
-	} else if (z[bucket+i].key == KEY_NULL) {
-	    if (f == -1) f = bucket+i;
+    for (i = 0; i < bucketsize; ++i) {
+	if (z[bucket + i].key == key) {
+	    z[bucket + i].value = value;
+	    return &z[bucket + i].value;
+	} else if (z[bucket + i].key == KEY_DELETED) {
+	    if (f == -1)
+		f = bucket + i;
+	} else if (z[bucket + i].key == KEY_NULL) {
+	    if (f == -1)
+		f = bucket + i;
 	    done = 1;
 	}
     }
@@ -280,24 +318,28 @@ restart:
 	step = (((hw >> 16) | (hw << 16)) & mask) | bucketsize;
 	do {
 	    bucket = (bucket + step) & mask;
-	    for (i=0; i<bucketsize; ++i) {
+	    for (i = 0; i < bucketsize; ++i) {
 		/* must search the entire cacheline (because otherwise we'd
 		 * have to do more movement when deleting things from the
 		 * cacheline)... should be cheap, though */
-		if (z[bucket+i].key == key) {
-		    z[bucket+i].value = value;
-		    return &z[bucket+i].value;
-		} else if (z[bucket+i].key == KEY_DELETED) {
-		    if (f == -1) { f = bucket+i; }
-		} else if (z[bucket+i].key == KEY_NULL) {
-		    if (f == -1) { f = bucket+i; }
+		if (z[bucket + i].key == key) {
+		    z[bucket + i].value = value;
+		    return &z[bucket + i].value;
+		} else if (z[bucket + i].key == KEY_DELETED) {
+		    if (f == -1) {
+			f = bucket + i;
+		    }
+		} else if (z[bucket + i].key == KEY_NULL) {
+		    if (f == -1) {
+			f = bucket + i;
+		    }
 		    done = 1;
 		}
 	    }
 	} while (!done && bucket != quit);
     }
-    assert(f != -1); // we MUST have found a place for it (otherwise the hash should have been resized bigger)
-    assert(z[f].key == KEY_NULL || z[f].key == KEY_DELETED); // sanity: the spot is empty
+    assert(f != -1);		       // we MUST have found a place for it (otherwise the hash should have been resized bigger)
+    assert(z[f].key == KEY_NULL || z[f].key == KEY_DELETED);	// sanity: the spot is empty
 
     if (z[f].key == KEY_NULL) {
 	if (h->population >= h->grow_size) {
@@ -316,7 +358,9 @@ restart:
     return &z[f].value;
 }
 
-void qt_hash_remove(qt_hash h, const qt_key_t key)
+void qt_hash_remove(
+    qt_hash h,
+    const qt_key_t key)
 {
     assert(h);
     if (h->lock) {
@@ -328,7 +372,9 @@ void qt_hash_remove(qt_hash h, const qt_key_t key)
     }
 }
 
-void qt_hash_remove_locked(qt_hash h, const qt_key_t key)
+void qt_hash_remove_locked(
+    qt_hash h,
+    const qt_key_t key)
 {
     hash_entry *p;
     void **value;
@@ -336,7 +382,7 @@ void qt_hash_remove_locked(qt_hash h, const qt_key_t key)
     value = qt_hash_internal_find(h, key);
     if (value == NULL)
 	return;
-    p = (hash_entry*) (value - 1); // sneaky way to recover the hash_entry ptr
+    p = (hash_entry *) (value - 1);    // sneaky way to recover the hash_entry ptr
     p->key = KEY_DELETED;
     ++h->deletes;
     --h->population;
@@ -347,7 +393,9 @@ void qt_hash_remove_locked(qt_hash h, const qt_key_t key)
     }
 }
 
-void *qt_hash_get(qt_hash h, const qt_key_t key)
+void *qt_hash_get(
+    qt_hash h,
+    const qt_key_t key)
 {
     void *ret;
     assert(h);
@@ -358,10 +406,12 @@ void *qt_hash_get(qt_hash h, const qt_key_t key)
     if (h->lock) {
 	QTHREAD_FASTLOCK_UNLOCK(h->lock);
     }
-    return (void*)ret;
+    return (void *)ret;
 }
 
-void *qt_hash_get_locked(qt_hash h, const qt_key_t key)
+void *qt_hash_get_locked(
+    qt_hash h,
+    const qt_key_t key)
 {
     void **value;
     assert(h);
@@ -373,7 +423,10 @@ void *qt_hash_get_locked(qt_hash h, const qt_key_t key)
     }
 }
 
-void qt_hash_callback(qt_hash h, qt_hash_callback_fn f, void *arg)
+void qt_hash_callback(
+    qt_hash h,
+    qt_hash_callback_fn f,
+    void *arg)
 {
     size_t visited = 0;
     assert(h);
@@ -390,7 +443,7 @@ void qt_hash_callback(qt_hash h, qt_hash_callback_fn f, void *arg)
     }
     if (visited < h->population) {
 	size_t i;
-	for (i=0; i<h->num_entries; ++i) {
+	for (i = 0; i < h->num_entries; ++i) {
 	    if (h->entries[i].key > KEY_DELETED) {
 		++visited;
 		f(h->entries[i].key, h->entries[i].value, arg);
@@ -405,7 +458,8 @@ void qt_hash_callback(qt_hash h, qt_hash_callback_fn f, void *arg)
     }
 }
 
-size_t qt_hash_count(qt_hash h)
+size_t qt_hash_count(
+    qt_hash h)
 {
     size_t ct;
     assert(h);
@@ -419,7 +473,8 @@ size_t qt_hash_count(qt_hash h)
     return ct;
 }
 
-void qt_hash_lock(qt_hash h)
+void qt_hash_lock(
+    qt_hash h)
 {
     assert(h);
     if (h->lock) {
@@ -427,7 +482,8 @@ void qt_hash_lock(qt_hash h)
     }
 }
 
-void qt_hash_unlock(qt_hash h)
+void qt_hash_unlock(
+    qt_hash h)
 {
     assert(h);
     if (h->lock) {
