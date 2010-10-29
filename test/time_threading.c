ea957e4ac333c4531e4c2bb2fb1776eb747f6580
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-10-29 14:25:04 +0000

    better benchmark results
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1672 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/time_threading.c
+++ test/time_threading.c
@@ -37,24 +37,36 @@ int main(
 			sizeof(void *) * 11)) / shepherds;
 	const size_t kbytes = bytes / 1024;
 	const size_t mbytes = kbytes / 1024;
-	printf
-	    ("With %u shepherd%s, qthread_fork() will need to be able to\n"
-	     "allocate %lu threads all at the same time. This will require\n"
-	     "at least %lu %s on this machine, and may affect spawn time.\n",
-	     shepherds, (shepherds > 1) ? "s" : "", THREADS / shepherds,
-	     (mbytes > 0) ? mbytes : ((kbytes > 0) ? kbytes : bytes),
-	     (mbytes > 0) ? "MB" : ((kbytes > 0) ? "kB" : "bytes"));
+	printf("With %u shepherd%s, qthread_fork() will need to be able to\n"
+	       "allocate %lu threads all at the same time. This will require\n"
+	       "at least %lu %s on this machine, and may affect spawn time.\n",
+	       shepherds, (shepherds > 1) ? "s" : "", THREADS / shepherds,
+	       (mbytes > 0) ? mbytes : ((kbytes > 0) ? kbytes : bytes),
+	       (mbytes > 0) ? "MB" : ((kbytes > 0) ? "kB" : "bytes"));
     }
 
     rets = malloc(sizeof(aligned_t) * THREADS);
 
+    for (unsigned long i = 0; i < THREADS; ++i) {
+	qthread_empty(NULL, rets + i);
+    }
+
     printf("Priming...\n");
+    qtimer_start(timer);
     for (unsigned long i = 0; i < THREADS; ++i) {
 	qthread_fork(null_thread, NULL, rets + i);
     }
+    qtimer_stop(timer);
+    spawn = qtimer_secs(timer);
     for (unsigned long i = 0; i < THREADS; ++i) {
-	qthread_readFF(NULL, NULL, rets + i);
+	qthread_readFE(NULL, NULL, rets + i);
     }
+    qtimer_stop(timer);
+    printf("\tCold spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS,
+	   THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tCold sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS,
+	   THREADS / spawn);
 
     printf("Timing forking...\n");
     qtimer_start(timer);
@@ -70,14 +82,20 @@ int main(
     }
     qtimer_stop(timer);
 
-    printf("Averaging over %lu threads...\n", THREADS);
-    printf("\tTime to spawn aligned_t thread: %g usecs\n",
-	   (spawn / THREADS) * 1000000);
-    printf("\tTime to sync aligned_t thread:  %g usecs\n",
-	   ((qtimer_secs(timer) - spawn) / THREADS) * 1000000);
+    printf("\tWarm spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS,
+	   THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tWarm sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS,
+	   THREADS / spawn);
     free(rets);
 
     srets = calloc(THREADS, sizeof(syncvar_t));
+    {
+	const syncvar_t empty = { .u.s={.data=0,.state=2,.lock=0} };
+	for (unsigned long i = 0; i < THREADS; ++i) {
+	    srets[i] = empty;
+	}
+    }
     qtimer_start(timer);
     for (unsigned long i = 0; i < THREADS; ++i) {
 	qthread_fork_syncvar(null_thread, NULL, srets + i);
@@ -91,10 +109,11 @@ int main(
     }
     qtimer_stop(timer);
 
-    printf("\tTime to spawn syncvar_t thread: %g usecs\n",
-	   (spawn / THREADS) * 1000000);
-    printf("\tTime to sync syncvar_t thread:  %g usecs\n",
-	   ((qtimer_secs(timer) - spawn) / THREADS) * 1000000);
+    printf("\tWarm syncvar_t spawn time: %f usecs, %f/sec\n",
+	   1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tWarm syncvar_t sync time : %f usecs, %f/sec\n",
+	   1000000 * spawn / THREADS, THREADS / spawn);
     free(srets);
 
     qtimer_start(timer);
@@ -102,8 +121,9 @@ int main(
 	qthread_fork(null_thread, NULL, NULL);
     }
     qtimer_stop(timer);
-    printf("\tTime to spawn no-ret thread:    %g usecs\n",
-	   (qtimer_secs(timer) / THREADS) * 1000000);
+    spawn = qtimer_secs(timer);
+    printf("\tWarm null spawn time: %f usecs, %f/sec\n",
+	   1000000 * spawn / THREADS, THREADS / spawn);
 
     qtimer_destroy(timer);
     return 0;
