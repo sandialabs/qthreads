0b53cdedd0238aecb500686cb57da36f5d6a2d7b
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2010-12-17 03:04:15 +0000

    fix bug in stress test
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1731 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_syncvar.c
+++ test/test_syncvar.c
@@ -6,12 +6,10 @@
 #include <qthread/qthread.h>
 #include "argparsing.h"
 
-#define ITERATIONS 10000
-#define PAIRS 3
-
 static syncvar_t x = SYNCVAR_STATIC_INITIALIZER;
 static syncvar_t id = SYNCVAR_STATIC_INITIALIZER;
 static uint64_t readout = 0;
+static uint64_t iterations = 1000000;
 
 static aligned_t consumer(qthread_t * t, void *arg)
 {
@@ -26,11 +24,12 @@ static aligned_t consumer(qthread_t * t, void *arg)
     me++;
     qthread_syncvar_writeEF(t, &id, &me);
 
-    for (uint64_t i = 0; i < ITERATIONS; ++i) {
-	iprintf("consumer(%p) readFF on x\n", t);
+    for (uint64_t i = 0; i < iterations; ++i) {
+	iprintf("consumer(%p) readFE on x\n", t);
 	qthread_syncvar_readFE(t, &readout, &x);
     }
-    qthread_syncvar_fill(t, &x);
+
+    iprintf("thread %i (%p) exiting\n", (int)(uintptr_t)arg, t);
 
     return 0;
 }
@@ -46,64 +45,80 @@ static aligned_t producer(qthread_t * t, void *arg)
     me++;
     qthread_syncvar_writeEF(t, &id, &me);
 
-    for (uint64_t i = 0; i < ITERATIONS; ++i) {
+    for (uint64_t i = 0; i < iterations; ++i) {
 	iprintf("producer(%p) x's status is: %s (expect empty)\n", t,
 		qthread_syncvar_status(&x)?"full":"empty");
 	iprintf("producer(%p) filling x(%p)\n", t, &x);
 	qthread_syncvar_writeEF_const(t, &x, i);
     }
+    iprintf("thread %i (%p) exiting\n", (int)(uintptr_t)arg, t);
 
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    aligned_t t[PAIRS][2];
+    aligned_t *t[2];
     uint64_t x_value;
+    qthread_t *me;
+
+    uint64_t pairs;
 
     assert(qthread_initialize() == 0);
+    me = qthread_self();
+    pairs = qthread_num_shepherds() * 6;
 
     CHECK_VERBOSE();
+    NUMARG(iterations, "ITERATIONS");
+    NUMARG(pairs, "PAIRS");
+
+    t[0] = calloc(pairs, sizeof(aligned_t));
+    t[1] = calloc(pairs, sizeof(aligned_t));
 
     iprintf("%i threads...\n", qthread_num_shepherds());
     iprintf("Initial value of x: %lu\n", (unsigned long)x.u.w);
 
-    qthread_syncvar_empty(qthread_self(), &id);
-    qthread_syncvar_writeF_const(qthread_self(), &id, 1);
+    qthread_syncvar_empty(me, &id);
+    qthread_syncvar_writeF_const(me, &id, 1);
     iprintf("id = 0x%lx\n", (unsigned long)id.u.w);
     {
 	uint64_t tmp = 0;
-	qthread_syncvar_readFF(qthread_self(), &tmp, &id);
+	qthread_syncvar_readFF(me, &tmp, &id);
 	assert(tmp == 1);
     }
     iprintf("x's status is: %s (want full (and nowait))\n",
 	    qthread_syncvar_status(&x)?"full":"empty");
     assert(qthread_syncvar_status(&x) == 1);
-    qthread_syncvar_readFE(qthread_self(), NULL, &x);
+    qthread_syncvar_readFE(me, NULL, &x);
     iprintf("x's status became: %s (want empty (and nowait))\n",
 	    qthread_syncvar_status(&x)?"full":"empty");
     assert(qthread_syncvar_status(&x) == 0);
-    for (unsigned int i = 0; i < PAIRS; ++i) {
-	qthread_fork(consumer, NULL, &(t[i][0]));
+    for (unsigned int i = 0; i < pairs; ++i) {
+	qthread_fork(consumer, (void*)(uintptr_t)i, &(t[0][i]));
     }
-    for (unsigned int i = 0; i < PAIRS; ++i) {
-	qthread_fork(producer, NULL, &(t[i][1]));
+    for (unsigned int i = 0; i < pairs; ++i) {
+	qthread_fork(producer, (void*)(uintptr_t)(i+pairs), &(t[1][i]));
     }
-    for (unsigned int i = 0; i < PAIRS; ++i) {
-	qthread_readFF(qthread_self(), NULL, &(t[i][0]));
-	qthread_readFF(qthread_self(), NULL, &(t[i][1]));
+    for (unsigned int i = 0; i < pairs; ++i) {
+	qthread_readFF(me, NULL, &(t[0][i]));
+	qthread_readFF(me, NULL, &(t[1][i]));
     }
     iprintf("shouldn't be blocking on x (current status: %s)\n",
 	    qthread_syncvar_status(&x)?"full":"empty");
-    qthread_syncvar_readFF(qthread_self(), &x_value, &x);
+    qthread_syncvar_fill(me, &x);
+    iprintf("shouldn't be blocking on x (current status: %s)\n",
+	    qthread_syncvar_status(&x)?"full":"empty");
+    qthread_syncvar_readFF(me, &x_value, &x);
     assert(qthread_syncvar_status(&x) == 1);
 
+    free(t[0]);
+    free(t[1]);
 
-    if (x_value == 55) {
-	iprintf("Success! x==55\n");
+    if (x_value == iterations-1) {
+	iprintf("Success! x==%lu\n", (unsigned long)x_value);
 	return 0;
     } else {
-	fprintf(stderr, "Final value of x=%lu\n", (unsigned long)x_value);
+	fprintf(stderr, "Final value of x=%lu, expected %lu\n", (unsigned long)x_value, (unsigned long)(iterations-1));
 	return -1;
     }
 }
