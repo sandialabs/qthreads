57d1ad6726476e4616efe4fc9311203145cdaaba
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-01-10 16:31:05 +0000

    match Chapel code more exactly
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1745 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_syncvar2.c
+++ test/test_syncvar2.c
@@ -6,35 +6,74 @@
 #include <qthread/qthread.h>
 #include "argparsing.h"
 
+typedef struct {
+    int64_t u:60;
+} int60_t;
+#define INT64TOINT60(x) ((uint64_t)((x)&0xfffffffffffffffUL))
+#define INT60TOINT64(x) ((int64_t)(((x)&0x800000000000000UL)?((x)|0xf800000000000000UL):(x)))
+
+/* More or less, this code matches the prodCons.chpl code from the open-source
+ * Chapel compiler. This is a demonstration generated in an attempt to figure
+ * out whether a given race condition is in qthreads on in Chapel */
+
+static uint64_t bufferSize = 1024;	/* size of the circular buffer */
+static uint64_t numItems;	/* number of items to write to buffer */
+
+/*
+ * Circular buffer of synchronization variables, which store
+ * full/empty state in addition to their numeric values. By default, reads to
+ * these variables will block until they are "full", leaving them "empty".
+ * Writes will block until "empty", leaving "full".
+ */
 static syncvar_t *buff = NULL;
 
-static uint64_t bufferSize = 1024;
-static uint64_t numItems;
-
-static aligned_t consumer(qthread_t * t, void *arg)
+/*
+ * the producer loops over the requested number of items, writing them to the
+ * buffer starting at location 0 and wrapping around when it hits the end of
+ * the buffer. It then writes the value -1 as a sentinel to the next position.
+ */
+static aligned_t producer(
+    qthread_t * t,
+    void *arg)
 {
-    for (unsigned int i=0; i<numItems; ++i) {
-	const unsigned int buffInd = i % bufferSize;
-	uint64_t buffVal;
-	qthread_syncvar_readFE(t, &buffVal, &buff[buffInd]);
-	iprintf("Consumer got: %u\n", (unsigned)buffVal);
-    }
-
-    return 0;
-}
-
-static aligned_t producer(qthread_t * t, void *arg)
-{
-    for (unsigned int i=0; i<numItems; ++i) {
+    for (unsigned int i = 0; i < numItems; ++i) {
 	const unsigned int buffInd = i % bufferSize;
 	qthread_syncvar_writeEF_const(t, &buff[buffInd], i);
 	iprintf("producer wrote value #%u\n", i);
     }
+    qthread_syncvar_writeEF_const(t, &buff[numItems % bufferSize], INT64TOINT60(-1));
 
     return 0;
 }
 
-int main(int argc, char *argv[])
+static int64_t readFromBuff(qthread_t *t) {
+    static unsigned int ind = 0;
+    uint64_t readVal;
+    int64_t nextVal;
+
+    qthread_syncvar_readFE(t, &readVal, &buff[ind]);
+    nextVal = INT60TOINT64(readVal);
+    if (nextVal != -1) {
+	ind = (ind + 1) % bufferSize;
+    }
+    return nextVal;
+}
+
+static aligned_t consumer(
+    qthread_t * t,
+    void *arg)
+{
+    int64_t buffVal;
+    while ((buffVal = readFromBuff(t)) != -1) {
+	iprintf("Consumer got: %li\n", (long)buffVal);
+    }
+
+    return 0;
+}
+
+int main(
+    int argc,
+    char *argv[])
 {
     aligned_t t[2];
 
@@ -42,13 +81,13 @@ int main(int argc, char *argv[])
 
     CHECK_VERBOSE();
     NUMARG(bufferSize, "BUFFERSIZE");
-    numItems = 8*bufferSize;
+    numItems = 8 * bufferSize;
     NUMARG(numItems, "NUMITEMS");
 
     iprintf("%i threads...\n", qthread_num_shepherds());
 
     buff = malloc(sizeof(syncvar_t) * bufferSize);
-    for (unsigned int i=0; i<bufferSize; ++i) {
+    for (unsigned int i = 0; i < bufferSize; ++i) {
 	buff[i] = SYNCVAR_INITIALIZER;
     }
 
