4ce1348e79a85409782fb0af9dd2fbcf51ecc8df
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-02-02 19:24:54 +0000

    re-arranging some stuff
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@1846 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/affinity/hwloc_affinity.c
+++ src/affinity/hwloc_affinity.c
@@ -19,7 +19,7 @@ static void qt_affinity_internal_hwloc_teardown(
 
 void qt_affinity_init(
     void)
-{
+{				       /*{{{ */
     qassert(hwloc_topology_init(&topology), 0);
     qassert(hwloc_topology_load(topology), 0);
     qthread_internal_cleanup(qt_affinity_internal_hwloc_teardown);
@@ -32,29 +32,30 @@ void qt_affinity_init(
     if (shep_depth == HWLOC_TYPE_DEPTH_UNKNOWN ||
 	shep_depth == HWLOC_TYPE_DEPTH_MULTIPLE) {
 	shep_depth = hwloc_get_type_depth(topology, HWLOC_OBJ_SOCKET);
-	assert(hwloc_get_nbobjs_inside_cpuset_by_depth(topology, hwloc_topology_get_allowed_cpuset(topology),
-						shep_depth) > 0);
+	assert(hwloc_get_nbobjs_inside_cpuset_by_depth
+	       (topology, hwloc_topology_get_allowed_cpuset(topology),
+		shep_depth) > 0);
 	qthread_debug(ALL_DETAILS, "depth of OBJ_SOCKET = %d\n", shep_depth);
     }
     if (shep_depth == HWLOC_TYPE_DEPTH_UNKNOWN ||
 	shep_depth == HWLOC_TYPE_DEPTH_MULTIPLE) {
 	qthread_debug(ALL_DETAILS,
 		      "topology too weird... nbobjs_by_type HWLOC_OBJ_PU is %u\n",
-		      (unsigned int)hwloc_get_nbobjs_by_depth(topology, HWLOC_OBJ_PU));
+		      (unsigned int)hwloc_get_nbobjs_by_depth(topology,
+							      HWLOC_OBJ_PU));
 	shep_depth = hwloc_get_type_depth(topology, HWLOC_OBJ_PU);
     }
 #endif
-}
+}				       /*}}} */
 
 qthread_shepherd_id_t guess_num_shepherds(
     void)
-{
+{				       /*{{{ */
     qthread_shepherd_id_t ret = 1;
 #ifdef QTHREAD_MULTITHREADED_SHEPHERDS
     ret = hwloc_get_nbobjs_by_depth(topology, shep_depth);
-    qthread_debug(ALL_DETAILS,
-		      "nbobjs_by_depth is %u, depth: %u\n",
-		      (unsigned int)ret, shep_depth);
+    qthread_debug(ALL_DETAILS, "nbobjs_by_depth is %u, depth: %u\n",
+		  (unsigned int)ret, shep_depth);
 #else
     /* the goal here is to pick the number of independent processing units
      * (i.e. cores) */
@@ -63,21 +64,61 @@ qthread_shepherd_id_t guess_num_shepherds(
 		  (unsigned int)ret);
 #endif
     return ret;
-}
+}				       /*}}} */
 
 #ifdef QTHREAD_MULTITHREADED_SHEPHERDS
+qthread_worker_id_t guess_num_workers_per_shep(
+    qthread_shepherd_id_t nshepherds)
+{				       /*{{{ */
+    qthread_worker_id_t ret = 0;
+    size_t total = 0;
+    const size_t max_socket = hwloc_get_nbobjs_by_depth(topology, shep_depth);
+    for (size_t socket = 0; socket < nshepherds && socket < max_socket;
+	 ++socket) {
+	hwloc_obj_t obj =
+	    hwloc_get_obj_by_depth(topology, shep_depth, socket);
+# if HWLOC_API_VERSION == 0x00010000
+	hwloc_cpuset_t cpuset = obj->allowed_cpuset;
+	unsigned int weight = hwloc_cpuset_weight(cpuset);
+# else
+	hwloc_bitmap_t cpuset = obj->allowed_cpuset;
+	unsigned int weight = hwloc_bitmap_weight(cpuset);
+# endif
+	qthread_debug(ALL_DETAILS, "socket %u has %u weight\n",
+		      (unsigned int)socket, weight);
+	total += weight;
+	if (socket == 0 || ret < weight) {
+	    ret = weight;
+	}
+    }
+    if (ret * nshepherds > total) {
+	ret = total / nshepherds;
+    }
+    qthread_debug(ALL_DETAILS, "guessed %i workers for %i sheps\n", (int)ret,
+		  (int)nshepherds);
+    return ret;
+}				       /*}}} */
+
 void qt_affinity_set(
-	qthread_worker_t *me)
-{
+    qthread_worker_t * me)
+{				       /*{{{ */
     hwloc_topology_t ltopology;
     qassert(hwloc_topology_init(&ltopology), 0);
     qassert(hwloc_topology_load(ltopology), 0);
     hwloc_const_cpuset_t allowed_cpuset = hwloc_topology_get_allowed_cpuset(ltopology);	// where am I allowed to run?
     qthread_shepherd_t *myshep = me->shepherd;
-    hwloc_obj_t obj = hwloc_get_obj_inside_cpuset_by_depth(ltopology, allowed_cpuset, shep_depth, myshep->node);
-    size_t nbobjs = hwloc_get_nbobjs_inside_cpuset_by_type(ltopology, obj->allowed_cpuset, HWLOC_OBJ_PU);
-    qthread_debug(ALL_DETAILS, "shep %i worker %i, there are %i PUs\n", me->shepherd->shepherd_id, me->worker_id, (int)nbobjs);
-    hwloc_obj_t sub_obj = hwloc_get_obj_inside_cpuset_by_type(ltopology, obj->allowed_cpuset, HWLOC_OBJ_PU, me->worker_id % nbobjs);
+    hwloc_obj_t obj =
+	hwloc_get_obj_inside_cpuset_by_depth(ltopology, allowed_cpuset,
+					     shep_depth, myshep->node);
+    size_t nbobjs =
+	hwloc_get_nbobjs_inside_cpuset_by_type(ltopology, obj->allowed_cpuset,
+					       HWLOC_OBJ_PU);
+    qthread_debug(ALL_DETAILS, "shep %i worker %i, there are %i PUs\n",
+		  me->shepherd->shepherd_id, me->worker_id, (int)nbobjs);
+    hwloc_obj_t sub_obj =
+	hwloc_get_obj_inside_cpuset_by_type(ltopology, obj->allowed_cpuset,
+					    HWLOC_OBJ_PU,
+					    me->worker_id % nbobjs);
 #ifdef QTHREAD_DEBUG
     {
 	char *str;
@@ -86,11 +127,15 @@ void qt_affinity_set(
 #else
 	hwloc_bitmap_asprintf(&str, sub_obj->allowed_cpuset);
 #endif
-	qthread_debug(ALL_DETAILS, "binding shep %i worker %i (%i) to mask %s\n", me->shepherd->shepherd_id, me->worker_id, me->packed_worker_id, str);
+	qthread_debug(ALL_DETAILS,
+		      "binding shep %i worker %i (%i) to mask %s\n",
+		      me->shepherd->shepherd_id, me->worker_id,
+		      me->packed_worker_id, str);
 	free(str);
     }
 #endif
-    if (hwloc_set_cpubind(ltopology, sub_obj->allowed_cpuset, HWLOC_CPUBIND_THREAD)) {
+    if (hwloc_set_cpubind
+	(ltopology, sub_obj->allowed_cpuset, HWLOC_CPUBIND_THREAD)) {
 	char *str;
 	int i = errno;
 #ifdef __APPLE__
@@ -107,14 +152,17 @@ void qt_affinity_set(
 		strerror(i), i);
 	free(str);
     }
-}
+}				       /*}}} */
 #else
 void qt_affinity_set(
-	qthread_shepherd_t *me)
+    qthread_shepherd_t * me)
 {				       /*{{{ */
     hwloc_const_cpuset_t allowed_cpuset = hwloc_topology_get_allowed_cpuset(topology);	// where am I allowed to run?
-    hwloc_obj_t obj = hwloc_get_obj_inside_cpuset_by_depth(topology, allowed_cpuset, shep_depth, me->node);
-    if (hwloc_set_cpubind(topology, obj->allowed_cpuset, HWLOC_CPUBIND_THREAD)) {
+    hwloc_obj_t obj =
+	hwloc_get_obj_inside_cpuset_by_depth(topology, allowed_cpuset,
+					     shep_depth, me->node);
+    if (hwloc_set_cpubind
+	(topology, obj->allowed_cpuset, HWLOC_CPUBIND_THREAD)) {
 	char *str;
 	int i = errno;
 #ifdef __APPLE__
@@ -134,49 +182,17 @@ void qt_affinity_set(
 }				       /*}}} */
 #endif
 
-#ifdef QTHREAD_MULTITHREADED_SHEPHERDS
-qthread_worker_id_t guess_num_workers_per_shep(
-    qthread_shepherd_id_t nshepherds)
-{
-    qthread_worker_id_t ret = 0;
-    size_t total = 0;
-    const size_t max_socket = hwloc_get_nbobjs_by_depth(topology, shep_depth);
-    for (size_t socket = 0; socket < nshepherds && socket < max_socket; ++socket) {
-	hwloc_obj_t obj =
-	    hwloc_get_obj_by_depth(topology, shep_depth, socket);
-# if HWLOC_API_VERSION == 0x00010000
-	hwloc_cpuset_t cpuset = obj->allowed_cpuset;
-	unsigned int weight = hwloc_cpuset_weight(cpuset);
-# else
-	hwloc_bitmap_t cpuset = obj->allowed_cpuset;
-	unsigned int weight = hwloc_bitmap_weight(cpuset);
-# endif
-	qthread_debug(ALL_DETAILS, "socket %u has %u weight\n",
-		(unsigned int)socket, weight);
-	total += weight;
-	if (socket == 0 || ret < weight) {
-	    ret = weight;
-	}
-    }
-    if (ret * nshepherds > total) {
-	ret = total / nshepherds;
-    }
-    qthread_debug(ALL_DETAILS, "guessed %i workers for %i sheps\n", (int)ret, (int)nshepherds);
-    return ret;
-}
-#endif
-
 int qt_affinity_gendists(
     qthread_shepherd_t * sheps,
     qthread_shepherd_id_t nshepherds)
-{
+{				       /*{{{ */
     hwloc_const_cpuset_t allowed_cpuset = hwloc_topology_get_allowed_cpuset(topology);	// where am I allowed to run?
     size_t num_extant_objs;
 
 #ifdef QTHREAD_MULTITHREADED_SHEPHERDS
     num_extant_objs =
 	hwloc_get_nbobjs_inside_cpuset_by_depth(topology, allowed_cpuset,
-		shep_depth);
+						shep_depth);
     for (size_t i = 0; i < nshepherds; ++i) {
 	sheps[i].node = i % num_extant_objs;
     }
@@ -251,4 +267,4 @@ int qt_affinity_gendists(
 #endif
     /* there does not seem to be a way to extract distances... <sigh> */
     return QTHREAD_SUCCESS;
-}
+}				       /*}}} */
