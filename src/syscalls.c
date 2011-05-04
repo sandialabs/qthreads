e3fb300d89f42c00c9b459c7e07ab0ae95273ada
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-05-04 21:10:53 +0000

    sleep should work outside of qthreads
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2016 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/syscalls.c
+++ src/syscalls.c
@@ -13,35 +13,75 @@
 
 unsigned int sleep(unsigned int seconds)
 {
-    qtimer_t t = qtimer_create();
-    qtimer_start(t);
-    do {
-	qthread_yield();
-	qtimer_stop(t);
-    } while (qtimer_secs(t) < seconds);
-    return 0;
+    if ((qlib != NULL) && (qthread_internal_self() != NULL)) {
+        qtimer_t t = qtimer_create();
+        qtimer_start(t);
+        do {
+            qthread_yield();
+            qtimer_stop(t);
+        } while (qtimer_secs(t) < seconds);
+        return 0;
+    } else {
+#if HAVE_DECL_SYS_SLEEP
+        return syscall(SYS_sleep, seconds);
+
+#elif HAVE_DECL_SYS_USLEEP
+        return syscall(SYS_usleep, seconds * 1e6);
+
+#elif HAVE_DECL_SYS_NANOSLEEP
+        return syscall(SYS_nanosleep, seconds * 1e9);
+
+#else
+        return 0;
+#endif
+    }
 }
 
 int usleep(useconds_t useconds)
 {
-    qtimer_t t = qtimer_create();
-    double seconds = useconds * 1e-6;
-    qtimer_start(t);
-    do {
-	qthread_yield();
-	qtimer_stop(t);
-    } while (qtimer_secs(t) < seconds);
-    return 0;
+    if ((qlib != NULL) && (qthread_internal_self() != NULL)) {
+        qtimer_t t       = qtimer_create();
+        double   seconds = useconds * 1e-6;
+        qtimer_start(t);
+        do {
+            qthread_yield();
+            qtimer_stop(t);
+        } while (qtimer_secs(t) < seconds);
+        return 0;
+    } else {
+#if HAVE_DECL_SYS_USLEEP
+        return syscall(SYS_usleep, useconds);
+
+#elif HAVE_DECL_SYS_NANOSLEEP
+        return syscall(SYS_nanosleep, useconds * 1e3);
+
+#else
+        return 0;
+#endif
+    }
 }
 
-int nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
+int nanosleep(const struct timespec *rqtp,
+              struct timespec       *rmtp)
 {
-    qtimer_t t = qtimer_create();
-    double seconds = rqtp->tv_sec + (rqtp->tv_nsec * 1e-9);
-    qtimer_start(t);
-    do {
-	qthread_yield();
-	qtimer_stop(t);
-    } while (qtimer_secs(t) < seconds);
-    return 0;
+    if ((qlib != NULL) && (qthread_internal_self() != NULL)) {
+        qtimer_t t       = qtimer_create();
+        double   seconds = rqtp->tv_sec + (rqtp->tv_nsec * 1e-9);
+
+        qtimer_start(t);
+        do {
+            qthread_yield();
+            qtimer_stop(t);
+        } while (qtimer_secs(t) < seconds);
+        return 0;
+    } else {
+#if HAVE_DECL_SYS_NANOSLEEP
+        return syscall(SYS_nanosleep, rqtp, rmtp);
+
+#else
+        return 0;
+#endif
+    }
 }
+
+/* vim:set expandtab: */
