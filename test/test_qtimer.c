f93035adfb591aa4608454e8b9a783cb25a81e60
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-05-23 11:53:58 +0000

    machines are fast enough now... zero-length qtimer results may just mean sub-micro-second execution
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2039 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_qtimer.c
+++ test/test_qtimer.c
@@ -20,16 +20,11 @@ int main(int argc,
     qtimer_stop(t);
 #ifndef SST                            /* SST can do this blazingly fast */
     if (qtimer_secs(t) == 0) {
-        struct timeval tv;
         fprintf(stderr, "qtimer_secs(t) reported zero length time.\n");
-        assert(gettimeofday(&tv, NULL) == 0);
-        printf("tv.tv_sec = %u, tv.tv_usec = %u\n", (unsigned int)tv.tv_sec,
-               (unsigned int)tv.tv_usec);
     } else if (qtimer_secs(t) < 0) {
         fprintf(stderr, "qtimer_secs(t) thinks time went backwards (%g).\n",
                 qtimer_secs(t));
     }
-    assert(qtimer_secs(t) > 0);
 #endif /* ifndef SST */
     iprintf("time to find self and assert it: %g secs\n", qtimer_secs(t));
 
