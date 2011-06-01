2d8d290e99f16c6ca74e5d73b10b79b78f2807c0
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-06-01 19:11:50 +0000

    adding some documentation comments
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2088 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qt_hash.h
+++ include/qt_hash.h
@@ -23,25 +23,115 @@ typedef void (*qt_hash_callback_fn)(const qt_key_t, void *, void *);
 typedef cp_hashlist *qt_hash;
 typedef int (*qt_hash_callback_fn)(void *, void *, void *);
 #endif
-typedef void (*qt_hash_deallocator_fn)(void*);
+typedef void (*qt_hash_deallocator_fn)(void *);
 
+/*!
+ * @fn qt_hash_create(int needSync)
+ * @brief Allocate and initialize a hash map
+ */
 qt_hash qt_hash_create(int needSync);
-void qt_hash_destroy(qt_hash h);
-void qt_hash_destroy_deallocate(qt_hash h, qt_hash_deallocator_fn f);
-void *qt_hash_put(qt_hash h, const qt_key_t key, void *value);
-void *qt_hash_put_locked(qt_hash h, const qt_key_t key, void *value);
-void qt_hash_remove(qt_hash h, const qt_key_t key);
-void qt_hash_remove_locked(qt_hash h, const qt_key_t key);
-void *qt_hash_get(qt_hash h, const qt_key_t key);
-void *qt_hash_get_locked(qt_hash h, const qt_key_t key);
-size_t qt_hash_count(qt_hash h);
-void qt_hash_callback(qt_hash h, qt_hash_callback_fn f, void *arg);
 
+/*!
+ * @fn qt_hash_destroy(qt_hash h)
+ * @brief Deallocates a hash map and any mappings that remain in it.
+ */
+void qt_hash_destroy(qt_hash h);
+
+/*!
+ * @fn qt_hash_destroy_deallocate(qt_hash                h,
+ *                                qt_hash_deallocator_fn f)
+ * @brief Deallocates a hash map and any mappings that remain in it. For each
+ *	mapping, it applies the deallocator function to deallocate any attached
+ *	memory.
+ */
+void qt_hash_destroy_deallocate(qt_hash                h,
+                                qt_hash_deallocator_fn f);
+/*!
+ * @fn qt_hash_put(qt_hash        h,
+ *                 const qt_key_t key,
+ *                 void          *value)
+ * @brief Insert a mapping from <key> to <value> into the hash map.
+ */
+void *qt_hash_put(qt_hash        h,
+                  const qt_key_t key,
+                  void          *value);
+
+/*!
+ * @fn qt_hash_put_locked(qt_hash        h,
+ *                        const qt_key_t key,
+ *                        void          *value)
+ * @brief Same as qt_hash_put(), but assumes that the hash map has been
+ *	locked already. */
+void *qt_hash_put_locked(qt_hash        h,
+                         const qt_key_t key,
+                         void          *value);
+
+/*!
+ * @fn qt_hash_remove(qt_hash        h,
+ *                    const qt_key_t key)
+ * @brief Remove the mapping for <key> from the hash map.
+ */
+void qt_hash_remove(qt_hash        h,
+                    const qt_key_t key);
+
+/*!
+ * @fn qt_hash_remove_locked(qt_hash        h,
+ *                           const qt_key_t key)
+ * @brief Same as qt_hash_remove(), but assumes that the hash map has been
+ *	locked already.
+ */
+void qt_hash_remove_locked(qt_hash        h,
+                           const qt_key_t key);
+
+/*!
+ * @fn qt_hash_get(qt_hash        h,
+ *                 const qt_key_t key)
+ * @brief Return the <value> associated with <key> in the hash map.
+ */
+void *qt_hash_get(qt_hash        h,
+                  const qt_key_t key);
+
+/*!
+ * @fn qt_hash_get_locked(qt_hash        h,
+ *                        const qt_key_t key)
+ * @brief Same as qt_hash_get(), but assumes that the hash map has been locked
+ *	already.
+ */
+void *qt_hash_get_locked(qt_hash        h,
+                         const qt_key_t key);
+
+/*!
+ * @fn qt_hash_count(qt_hash h)
+ * @brief Return the number of key/value pairs stored in the hash map.
+ */
+size_t qt_hash_count(qt_hash h);
+
+/*!
+ * @fn qt_hash_callback(qt_hash             h,
+ *                      qt_hash_callback_fn f,
+ *                      void               *arg)
+ * @brief Execute the function <f> on all of the key/value pairs stored in the
+ *	hash map.
+ */
+void qt_hash_callback(qt_hash             h,
+                      qt_hash_callback_fn f,
+                      void               *arg);
+
+/*!
+ * @fn qt_hash_lock(qt_hash h)
+ * @brief Lock the hash map.
+ */
 void qt_hash_lock(qt_hash h);
+
+/*!
+ * @fn qt_hash_unlock(qt_hash h)
+ * @brief Unock the hash map.
+ */
 void qt_hash_unlock(qt_hash h);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif // ifndef QT_HASH_H
+/* vim:set expandtab: */
