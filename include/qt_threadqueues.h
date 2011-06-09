3cffbcf07b804548763ba215caf443600557f506
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-06-09 14:51:49 +0000

    being explicit... fix compiler warning about qt_threadqueue_advisory_queuelen()
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2131 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qt_threadqueues.h
+++ include/qt_threadqueues.h
@@ -3,6 +3,8 @@
 
 #include <sys/types.h> /* for ssize_t (according to P90) */
 
+#include "qt_visibility.h"
+
 #include "qt_atomics.h"
 #include "qt_mpool.h"
 
@@ -24,30 +26,31 @@ typedef struct _qt_threadqueue_pools {
 # define QMS_ARG(x)
 #endif
 
-void qt_threadqueue_init_pools(qt_threadqueue_pools_t *p);
-void qt_threadqueue_destroy_pools(qt_threadqueue_pools_t *p);
+void INTERNAL qt_threadqueue_init_pools(qt_threadqueue_pools_t *p);
+void INTERNAL qt_threadqueue_destroy_pools(qt_threadqueue_pools_t *p);
 
-qt_threadqueue_t *qt_threadqueue_new(qthread_shepherd_t *shepherd);
-void              qt_threadqueue_free(qt_threadqueue_t *q);
-void              qt_threadqueue_enqueue(qt_threadqueue_t   *q,
-                                         qthread_t          *t,
-                                         qthread_shepherd_t *shep);
-void qt_threadqueue_enqueue_yielded(qt_threadqueue_t   *q,
-                                    qthread_t          *t,
-                                    qthread_shepherd_t *shep);
-qthread_t *qt_threadqueue_dequeue(qt_threadqueue_t *q);
+qt_threadqueue_t INTERNAL *qt_threadqueue_new(qthread_shepherd_t *shepherd);
+void INTERNAL              qt_threadqueue_free(qt_threadqueue_t *q);
+void INTERNAL              qt_threadqueue_enqueue(qt_threadqueue_t   *q,
+                                                  qthread_t          *t,
+                                                  qthread_shepherd_t *shep);
+void INTERNAL qt_threadqueue_enqueue_yielded(qt_threadqueue_t   *q,
+                                             qthread_t          *t,
+                                             qthread_shepherd_t *shep);
+ssize_t INTERNAL    qt_threadqueue_advisory_queuelen(qt_threadqueue_t *q);
+qthread_t INTERNAL *qt_threadqueue_dequeue(qt_threadqueue_t *q);
 
-qthread_t *qt_threadqueue_dequeue_blocking(qt_threadqueue_t * q QMS_ARG(size_t active));
+qthread_t INTERNAL *qt_threadqueue_dequeue_blocking(qt_threadqueue_t * q QMS_ARG(size_t active));
 
 #ifdef QTHREAD_MULTITHREADED_SHEPHERDS
 /* Functions for work stealing functionality */
-void qt_threadqueue_enqueue_multiple(qt_threadqueue_t      *q,
-                                     qt_threadqueue_node_t *first,
-                                     qthread_shepherd_t    *shep);
-qt_threadqueue_node_t *qt_threadqueue_dequeue_steal(qt_threadqueue_t *q);
-qt_threadqueue_node_t *qt_threadqueue_dequeue_specific(qt_threadqueue_t *q,
-                                                       void             *value);
-void qthread_steal_stat(void);
+void INTERNAL qt_threadqueue_enqueue_multiple(qt_threadqueue_t      *q,
+                                              qt_threadqueue_node_t *first,
+                                              qthread_shepherd_t    *shep);
+qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_steal(qt_threadqueue_t *q);
+qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_specific(qt_threadqueue_t *q,
+                                                                void             *value);
+void INTERNAL qthread_steal_stat(void);
 #endif // ifdef QTHREAD_MULTITHREADED_SHEPHERDS
 
 #endif // ifndef QT_THREADQUEUES_H
