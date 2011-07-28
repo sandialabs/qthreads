1bb6a859dcdc85a145c76eb511c95119b1dd0c9f
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-07-28 14:24:00 +0000

    white space
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2254 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/qloop.c
+++ src/qloop.c
@@ -198,8 +198,8 @@ static void qt_loop_step_inner(const size_t         start,
         qwa[threadct].id      = threadct;
         qwa[threadct].level   = 0;
         qwa[threadct].rets    = rets;
-	
-	qthread_syncvar_empty(rets+threadct);
+
+        qthread_syncvar_empty(rets + threadct);
         threadct++;
     }
     threadct = 0;
