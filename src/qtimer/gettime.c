8d6f0e85d67c06a41407ca673309df46af7c7594
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-08-04 16:43:24 +0000

    improve fastrand to be more rand
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2277 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/qtimer/gettime.c
+++ src/qtimer/gettime.c
@@ -18,6 +18,7 @@
 #ifdef HAVE_STDLIB_H
 # include <stdlib.h>
 #endif
+#include <qthread/qthread-int.h> /* for uint64_t */
 
 struct qtimer_s {
     struct timespec start, stop;
@@ -29,12 +30,54 @@ void qtimer_start(qtimer_t q)
     qassert(clock_gettime(CLOCK_MONOTONIC, &(q->start)), 0);
 }
 
+/* this function based on http://burtleburtle.net/bob/hash/evahash.html */
+#define rot(x, k) (((x) << (k)) | ((x) >> (32 - (k))))
+static uint64_t qt_hashword(uint64_t key)
+{   /*{{{*/
+    uint32_t a, b, c;
+
+    const union {
+        uint64_t key;
+        uint8_t  b[sizeof(uint64_t)];
+    } k = {
+        key
+    };
+
+    a  = b = c = 0xdeadbeef + sizeof(uint64_t);
+    c += 47;
+
+    b += k.b[7] << 24;
+    b += k.b[6] << 16;
+    b += k.b[5] << 8;
+    b += k.b[4];
+    a += k.b[3] << 24;
+    a += k.b[2] << 16;
+    a += k.b[1] << 8;
+    a += k.b[0];
+
+    c ^= b;
+    c -= rot(b, 14);
+    a ^= c;
+    a -= rot(c, 11);
+    b ^= a;
+    b -= rot(a, 25);
+    c ^= b;
+    c -= rot(b, 16);
+    a ^= c;
+    a -= rot(c, 4);
+    b ^= a;
+    b -= rot(a, 14);
+    c ^= b;
+    c -= rot(b, 24);
+    return ((uint64_t)c + ((uint64_t)b << 32));
+} /*}}}*/
+
 long qtimer_fastrand(void)
 {
     struct timespec s;
 
     qassert(clock_gettime(CLOCK_MONOTONIC, &(s)), 0);
-    return (long)(s.tv_nsec >> 5); // last five bits seem to always be the same
+    return qt_hashword((uint64_t)(s.tv_nsec));
 }
 
 void qtimer_stop(qtimer_t q)
