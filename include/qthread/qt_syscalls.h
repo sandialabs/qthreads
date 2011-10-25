6237b8c209c42ce08bf4c3664165df24e9bbbfd8
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-10-25 20:25:57 +0000

    add a flag to do header-based syscall interception via mangling
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2475 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread/qt_syscalls.h
+++ include/qthread/qt_syscalls.h
@@ -16,9 +16,6 @@ int qt_connect(int                    socket,
 int qt_poll(struct pollfd fds[],
             nfds_t        nfds,
             int           timeout);
-ssize_t qt_read(int    filedes,
-                void  *buf,
-                size_t nbyte);
 ssize_t qt_pread(int    filedes,
                  void  *buf,
                  size_t nbyte,
@@ -27,6 +24,9 @@ ssize_t qt_pwrite(int         filedes,
                   const void *buf,
                   size_t      nbyte,
                   off_t       offset);
+ssize_t qt_read(int    filedes,
+                void  *buf,
+                size_t nbyte);
 int qt_select(int                      nfds,
               fd_set *restrict         readfds,
               fd_set *restrict         writefds,
@@ -40,6 +40,19 @@ pid_t qt_wait4(pid_t          pid,
 ssize_t qt_write(int         filedes,
                  const void *buf,
                  size_t      nbyte);
+
+#ifdef USE_HEADER_SYSCALLS
+# define accept(s, a, l)       qt_accept((s), (a), (l))
+# define connect(s, a, l)      qt_connect((s), (a), (l))
+# define poll(f, n, t)         qt_poll((f), (n), (t))
+# define pread(f, b, n, o)     qt_pread((f), (b), (n), (o))
+# define pwrite(f, b, n, o)    qt_pread((f), (b), (n), (o))
+# define read(f, b, n)         qt_read((f), (b), (n))
+# define select(n, r, w, e, t) qt_select((n), (r), (w), (e), (t))
+# define system(c)             qt_system((c))
+# define wait4(p, s, o, r)     qt_wait4((p), (s), (o), (r))
+# define write(f, b, n)        qt_write((f), (b), (n))
+#endif // ifdef USE_HEADER_SYSCALLS
 
 #endif // ifndef QT_SYSCALLS_H
 /* vim:set expandtab: */
