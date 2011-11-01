e1dd589fd5a763c8352bbe003539b1624a63d95a
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-11-01 17:46:16 +0000

    use SPINLOCK_BODY instead of file-specific cpu_relax() macro; also use MACHINE_FENCE, which does what COMPILER_FENCE used to do
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2529 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/56reader-rwlock.h
+++ include/56reader-rwlock.h
@@ -12,18 +12,10 @@
  * behavior will happen.
  **/
 
-
 #include <stdint.h>
 
 #ifndef NOINLINE
-#define NOINLINE __attribute__((noinline))
-#endif
-
-#if (QTHREAD_ASSEMBLY_ARCH == QTHREAD_IA32) || \
-    (QTHREAD_ASSEMBLY_ARCH == QTHREAD_AMD64)
-# define cpu_relax() do { __asm__ __volatile__ ("pause" ::: "memory"); } while (0)
-#else
-# define cpu_relax() do { } while (0)
+# define NOINLINE __attribute__((noinline))
 #endif
 
 struct tlrw_lock {
@@ -33,58 +25,55 @@ struct tlrw_lock {
 
 typedef struct tlrw_lock rwlock_t;
 
-static QINLINE void rwlock_init(rwlock_t *l) {
+static QINLINE void rwlock_init(rwlock_t *l)
+{
     unsigned int i;
 
     l->owner = 0;
-    for (i = 0; i < sizeof l->readers; i++)
-        l->readers[i] = 0;
-
-    return;
+    for (i = 0; i < sizeof l->readers; i++) l->readers[i] = 0;
 }
 
-static QINLINE void rwlock_rdlock(rwlock_t *l, int id) {
+static QINLINE void rwlock_rdlock(rwlock_t *l,
+                                  int       id)
+{
     for (;;) {
         l->readers[id] = 1;
-        COMPILER_FENCE; 
+        MACHINE_FENCE;
 
         if (l->owner == 0) {
             break;
         } else {
             l->readers[id] = 0;
 
-            while (l->owner != 0)
-                cpu_relax();
+            while (l->owner != 0) SPINLOCK_BODY();
         }
     }
-
-    return;
 }
 
-static QINLINE void rwlock_rdunlock(rwlock_t *l, int id) {
+static QINLINE void rwlock_rdunlock(rwlock_t *l,
+                                    int       id)
+{
     l->readers[id] = 0;
 }
 
-static QINLINE void rwlock_wrlock(rwlock_t *l, int id) {
-
+static QINLINE void rwlock_wrlock(rwlock_t *l,
+                                  int       id)
+{
     id = id + 1;
 
-    uint64_t *readers = (void *) l->readers;
-    while(qthread_cas32(&(l->owner), 0, id) != id)
-        cpu_relax();
+    uint64_t *readers = (void *)l->readers;
+    while(qthread_cas32(&(l->owner), 0, id) != id) SPINLOCK_BODY();
 
-    COMPILER_FENCE; 
+    MACHINE_FENCE;
     for (int i = 0; i < sizeof(l->readers) / sizeof(uint64_t); i++) {
-        while (readers[i] != 0) 
-            cpu_relax();
+        while (readers[i] != 0) SPINLOCK_BODY();
     }
-
-    return;
 }
 
-static QINLINE void rwlock_wrunlock(rwlock_t *l) {
+static QINLINE void rwlock_wrunlock(rwlock_t *l)
+{
     l->owner = 0;
-    COMPILER_FENCE; 
+    MACHINE_FENCE;
 }
 
 #endif // ifndef FIFTY_SIX_RWLOCK_H
