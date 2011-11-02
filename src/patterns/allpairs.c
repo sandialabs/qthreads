8cac621152ca2995a9e0378f0ec86f550acb0f68
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-11-02 19:34:34 +0000

    remove volatile in favor of memory fences
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2565 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/patterns/allpairs.c
+++ src/patterns/allpairs.c
@@ -26,7 +26,7 @@ struct cacheline_s *distances = NULL;
 #ifdef QTHREAD_USE_HALFWAYARRAY
 qthread_shepherd_id_t **halfway = NULL;
 // unsigned int ** halfway_dist = NULL;
-// volatile aligned_t mindistances = 0;
+// aligned_t mindistances = 0;
 // aligned_t stolen_work = 0;
 // aligned_t stealing_penalty = 0;
 #endif
@@ -36,26 +36,20 @@ union distfuncunion {
     dist_out_f od;
 };
 struct qt_ap_wargs {
-    qdqueue_t *restrict                work_queue;
-    const union distfuncunion          f;
-    const int                          outfunc_style;
-    volatile aligned_t *restrict const no_more_work;
-    volatile aligned_t *restrict const donecount;
-    const qarray                      *a1;
-    const qarray                      *a2;
-    void *restrict *restrict           output;
-    const size_t                       outsize;
+    qdqueue_t *restrict       work_queue;
+    const union distfuncunion f;
+    const int                 outfunc_style;
+    aligned_t *restrict const no_more_work;
+    aligned_t *restrict const donecount;
+    const qarray             *a1;
+    const qarray             *a2;
+    void *restrict *restrict  output;
+    const size_t              outsize;
 };
 
 struct qt_ap_workunit {
     size_t a1_start, a1_stop, a2_start, a2_stop;
 };
-
-/* avoid compiler bugs with volatile... */
-static Q_NOINLINE aligned_t vol_read_a(volatile aligned_t *ptr)
-{
-    return *ptr;
-}
 
 static aligned_t qt_ap_worker(struct qt_ap_wargs *restrict args)
 {
@@ -63,30 +57,26 @@ static aligned_t qt_ap_worker(struct qt_ap_wargs *restrict args)
         struct qt_ap_workunit *restrict const wu =
             qdqueue_dequeue(args->work_queue);
         if (wu == NULL) {
-            if (vol_read_a(args->no_more_work)) {
+            if (args->no_more_work) {
+                MACHINE_FENCE;
                 qthread_incr(args->donecount, 1);
                 break;
             }
             qthread_yield();
         } else {
-            const char *const a1_base =
-                qarray_elem_nomigrate(args->a1, wu->a1_start);
-            const char *const a2_base =
-                qarray_elem_nomigrate(args->a2, wu->a2_start);
-            const size_t a1_usize = args->a1->unit_size;
-            const size_t a2_usize = args->a2->unit_size;
-            size_t       a1_i;
+            const char *const a1_base  = qarray_elem_nomigrate(args->a1, wu->a1_start);
+            const char *const a2_base  = qarray_elem_nomigrate(args->a2, wu->a2_start);
+            const size_t      a1_usize = args->a1->unit_size;
+            const size_t      a2_usize = args->a2->unit_size;
+            size_t            a1_i;
 
 #ifdef QTHREAD_TRACK_DISTANCES
             {
-                const qthread_shepherd_id_t shep    = qthread_shep();
-                const qthread_shepherd_id_t a1_shep =
-                    qarray_shepof(args->a1, wu->a1_start);
-                const qthread_shepherd_id_t a2_shep =
-                    qarray_shepof(args->a2, wu->a2_start);
-                const unsigned int cur_dist =
-                    qthread_distance(shep, a1_shep) + qthread_distance(shep,
-                                                                       a2_shep);
+                const qthread_shepherd_id_t shep     = qthread_shep();
+                const qthread_shepherd_id_t a1_shep  = qarray_shepof(args->a1, wu->a1_start);
+                const qthread_shepherd_id_t a2_shep  = qarray_shepof(args->a2, wu->a2_start);
+                const unsigned int          cur_dist = qthread_distance(shep, a1_shep) +
+                                                       qthread_distance(shep, a2_shep);
                 /*if (shep != halfway[a1_shep][a2_shep]) {
                  * qthread_incr(&stolen_work, 1);
                  * qthread_incr(&stealing_penalty, cur_dist - halfway_dist[a1_shep][a2_shep]);
@@ -100,30 +90,23 @@ static aligned_t qt_ap_worker(struct qt_ap_wargs *restrict args)
                 if (args->output == NULL) {
                     for (a1_i = 0; a1_i < (wu->a1_stop - wu->a1_start);
                          a1_i++) {
-                        const char *restrict const this_a1_base =
-                            a1_base + (a1_i * a1_usize);
-                        size_t a2_i;
+                        const char *restrict const this_a1_base = a1_base + (a1_i * a1_usize);
+                        size_t                     a2_i;
 
-                        for (a2_i = 0; a2_i < (wu->a2_stop - wu->a2_start);
-                             a2_i++) {
-                            f(this_a1_base, a2_base + (a2_i * a2_usize),
-                              NULL);
+                        for (a2_i = 0; a2_i < (wu->a2_stop - wu->a2_start); a2_i++) {
+                            f(this_a1_base, a2_base + (a2_i * a2_usize), NULL);
                         }
                     }
                 } else {
-                    for (a1_i = 0; a1_i < (wu->a1_stop - wu->a1_start);
-                         a1_i++) {
-                        const char *restrict const this_a1_base =
-                            a1_base + (a1_i * a1_usize);
-                        char *restrict const this_outbase =
-                            args->output[a1_i + wu->a1_start];
-                        size_t a2_i;
+                    for (a1_i = 0; a1_i < (wu->a1_stop - wu->a1_start); a1_i++) {
+                        const char *restrict const this_a1_base = a1_base + (a1_i * a1_usize);
+                        char *restrict const       this_outbase = args->output[a1_i + wu->a1_start];
+                        size_t                     a2_i;
 
-                        for (a2_i = 0; a2_i < (wu->a2_stop - wu->a2_start);
-                             a2_i++) {
-                            f(this_a1_base, a2_base + (a2_i * a2_usize),
-                              this_outbase +
-                              ((a2_i + wu->a2_start) * args->outsize));
+                        for (a2_i = 0; a2_i < (wu->a2_stop - wu->a2_start); a2_i++) {
+                            f(this_a1_base,
+                              a2_base + (a2_i * a2_usize),
+                              this_outbase + ((a2_i + wu->a2_start) * args->outsize));
                         }
                     }
                 }
@@ -131,12 +114,10 @@ static aligned_t qt_ap_worker(struct qt_ap_wargs *restrict args)
                 const dist_f f = args->f.d;
 
                 for (a1_i = 0; a1_i < (wu->a1_stop - wu->a1_start); a1_i++) {
-                    const char *restrict const this_a1_base =
-                        a1_base + (a1_i * a1_usize);
-                    size_t a2_i;
+                    const char *restrict const this_a1_base = a1_base + (a1_i * a1_usize);
+                    size_t                     a2_i;
 
-                    for (a2_i = 0; a2_i < (wu->a2_stop - wu->a2_start);
-                         a2_i++) {
+                    for (a2_i = 0; a2_i < (wu->a2_stop - wu->a2_start); a2_i++) {
                         f(this_a1_base, a2_base + (a2_i * a2_usize));
                     }
                 }
@@ -220,8 +201,7 @@ static void qt_ap_genwork(const size_t                    startat,
                           const qarray *restrict Q_UNUSED a,
                           struct qt_ap_gargs *restrict    gargs)
 {
-    struct qt_ap_gargs2 garg2 =
-    { gargs->wq, startat, stopat, qthread_shep() };
+    struct qt_ap_gargs2 garg2 = { gargs->wq, startat, stopat, qthread_shep() };
 
     qarray_iter_constloop(gargs->array2, 0, gargs->array2->count,
                           (qa_cloop_f)qt_ap_genwork2, &garg2);
@@ -245,8 +225,8 @@ static void qt_allpairs_internal(const qarray             *array1,
                                  const size_t              outsize)
 {
     const qthread_shepherd_id_t max_i        = qthread_num_shepherds();
-    volatile aligned_t          no_more_work = 0;
-    volatile aligned_t          donecount    = 0;
+    aligned_t                   no_more_work = 0;
+    aligned_t                   donecount    = 0;
     struct qt_ap_wargs          wargs        = {
         qdqueue_create(), distfunc, funcstyle, &no_more_work, &donecount,
         array1,
@@ -269,8 +249,7 @@ static void qt_allpairs_internal(const qarray             *array1,
     // stolen_work = 0;
     // stealing_penalty = 0;
     if (halfway == NULL) {
-        qthread_shepherd_id_t *equivs =
-            calloc(max_i, sizeof(qthread_shepherd_id_t));
+        qthread_shepherd_id_t *equivs = calloc(max_i, sizeof(qthread_shepherd_id_t));
         halfway = calloc(max_i, sizeof(qthread_shepherd_id_t *));
         // halfway_dist = calloc(max_i, sizeof(unsigned int*));
         for (int s = 0; s < max_i; s++) {
@@ -279,13 +258,11 @@ static void qt_allpairs_internal(const qarray             *array1,
             for (int d = 0; d < max_i; d++) {
                 /* halfway[s][d] is the shep id with the lowest total distance to both */
                 unsigned int equiv_cnt = 0;
-                unsigned int dist      =
-                    qthread_distance(0, s) + qthread_distance(0, d);
+                unsigned int dist      = qthread_distance(0, s) + qthread_distance(0, d);
                 equiv_cnt = 1;
                 equivs[0] = 0;
                 for (int h = 1; h < max_i; h++) {
-                    unsigned int tmp =
-                        qthread_distance(h, s) + qthread_distance(h, d);
+                    unsigned int tmp = qthread_distance(h, s) + qthread_distance(h, d);
                     if (tmp < dist) {
                         dist      = tmp;
                         equiv_cnt = 1;
@@ -313,8 +290,10 @@ static void qt_allpairs_internal(const qarray             *array1,
     qarray_iter_constloop(array1, 0, array1->count,
                           (qa_cloop_f)qt_ap_genwork, &gargs);
     /* step 4: wait for the workers to get done */
+    MACHINE_FENCE;
     no_more_work = 1;
-    while (vol_read_a(&donecount) < max_i) {
+    MACHINE_FENCE;
+    while (donecount < max_i) {
         qthread_yield();
     }
     qdqueue_destroy(wargs.work_queue);
