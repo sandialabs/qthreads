d52fd50f919cef8ede028df471f63471ab4e2b4a
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-11-21 20:32:51 +0000

    more complex behavior
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2656 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/benchmarks/generic/time_qt_loopaccums.c
+++ test/benchmarks/generic/time_qt_loopaccums.c
@@ -5,42 +5,86 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
+#include <float.h> // for DBL_MAX, from C89
 #include <qthread/qloop.h>
 #include <qthread/qtimer.h>
 #include "argparsing.h"
 
-static aligned_t threads  = 0;
-static aligned_t numincrs = 1024;
-static size_t   *sleeplen;
-static size_t    numiters = 10;
+typedef struct {
+    double max;
+    double min;
+    double sum;
+    double prod;
+    double avg;
+    size_t cnt;
+} multiple_op_t;
+
+static aligned_t numincrs       = 1024;
+static double   *random_numbers = NULL;
+static size_t   *sleeplen       = NULL;
+static size_t    numiters       = 10;
 static qtimer_t  timer;
+
+static multiple_op_t reset = { 0, DBL_MAX, 0, 1, 0, 0 };
+static multiple_op_t known_correct = { 0, DBL_MAX, 0, 1, 0, 0 };
 
 static void count(const size_t startat,
                   const size_t stopat,
                   void        *arg_,
                   void        *ret_)
 {
-    for (size_t i = startat; i < stopat; ++i) {
+    multiple_op_t *ret;
+
+    ret.max  = random_numbers[startat];
+    ret.min  = random_numbers[startat];
+    ret.sum  = random_numbers[startat];
+    ret.prod = random_numbers[startat];
+    ret.avg  = random_numbers[startat];
+    ret.cnt  = 1;
+    usleep(sleeplen[startat]);
+    for (size_t i = startat + 1; i < stopat; ++i) {
+        ret.max   = (random_numbers[i] > ret.max) ? random_numbers[i] : ret.max;
+        ret.min   = (random_numbers[i] > ret.min) ? ret.min : random_numbers[i];
+        ret.sum  += random_numbers[i];
+        ret.prod *= random_numbers[i];
+        ret.avg   = ((ret.avg * ret.cnt) / (ret.cnt + 1)) + (random_numbers[i] / (ret.cnt + 1));
+        ret.cnt++;
         usleep(sleeplen[i]);
     }
-    *(size_t *)ret_ = stopat - startat;
+    *(multiple_op_t *)ret_ = ret;
 }
 
-static void sum(void *a_,
-                void *b_)
+static void multi_op(void *restrict a_,
+                     void *restrict b_)
 {
-    *(size_t*)a_ += *(size_t*)b_;
-    //qthread_incr((size_t *)a_, *(size_t *)b_);
-}
+    multiple_op_t *restrict a = (multiple_op_t *)a_;
+    multiple_op_t *restrict b = (multiple_op_t *)b_;
 
-#if 0
-static void sum_sinc(void *a_,
-                     void *b_)
-{
-    *(size_t *)a_ += *(size_t *)b_;
+    //printf("a->max = %f, b->max = %f\n", a->max, b->max);
+    a->max = (a->max > b->max) ? a->max : b->max;
+    //printf("a->max = %f\n", a->max);
+    //printf("a->min = %f, b->min = %f\n", a->min, b->min);
+    a->min = (a->min > b->min) ? b->min : a->min;
+    //printf("a->min = %f\n", a->min);
+    //printf("a->sum = %f, b->sum = %f\n", a->sum, b->sum);
+    a->sum += b->sum;
+    //printf("a->sum = %f\n", a->sum);
+    //printf("a->prod = %f, b->prod = %f\n", a->prod, b->prod);
+    a->prod *= b->prod;
+    //printf("a->prod = %f\n", a->prod);
+    //printf("a->avg = %f, b->avg = %f\n", a->avg, b->avg);
+    if (b->cnt > 0) {
+	if (a->cnt > 0) {
+	    a->avg = ((a->avg * a->cnt) / (a->cnt + b->cnt)) + ((b->avg * b->cnt) / (a->cnt + b->cnt));
+	} else {
+	    a->avg = b->avg;
+	}
+    }
+    //printf("a->avg = %f\n", a->avg);
+    //printf("a->cnt = %zu, b->cnt = %zu\n", a->cnt, b->cnt);
+    a->cnt += b->cnt;
+    //printf("a->cnt = %zu\n", a->cnt);
 }
-
-#endif
 
 static void run_iterations(void                                                   (*loop)(const size_t a,
                                                                  const size_t     b,
@@ -55,20 +99,31 @@ static void run_iterations(void                                                 
                            const char                                            *type,
                            const char                                            *name)
 {
-    double total  = 0;
-    int    passed = 0;
-    int    i;
+    double        total  = 0;
+    int           passed = 0;
+    int           i;
+    multiple_op_t tmp = reset;
 
     // to warm the caches
-    threads=0;
-    loop(0, numincrs, sizeof(aligned_t), &threads, func, NULL, acc);
+    loop(0, numincrs, sizeof(tmp), &tmp, func, NULL, acc);
     // now, to time it
     for (i = 0; i < numiters; ++i) {
-        threads = 0;
+        tmp = reset;
         qtimer_start(timer);
-        loop(0, numincrs, sizeof(aligned_t), &threads, func, NULL, acc);
+        loop(0, numincrs, sizeof(tmp), &tmp, func, NULL, acc);
         qtimer_stop(timer);
-        passed = threads == numincrs;
+        //printf("max = %f (%f)\n", tmp.max, known_correct.max);
+	passed = (tmp.max == known_correct.max);
+	if (!passed) break;
+        //printf("min = %f (%f)\n", tmp.min, known_correct.min);
+	passed = (tmp.min == known_correct.min);
+	if (!passed) break;
+        //printf("sum = %f (%f)\n", tmp.sum, known_correct.sum);
+        //printf("prd = %f (%f)\n", tmp.prod, known_correct.prod);
+        //printf("avg = %f (%f)\n", tmp.avg, known_correct.avg);
+        //printf("cnt = %zu (%zu)\n", tmp.cnt, known_correct.cnt);
+	passed = (tmp.cnt == known_correct.cnt);
+	if (!passed) break;
         total += qtimer_secs(timer);
         if (!passed) {
             break;
@@ -78,9 +133,6 @@ static void run_iterations(void                                                 
            (unsigned long)numincrs,
            (total / i) - overhead,
            passed ? "PASSED" : "FAILED");
-    if (!passed) {
-	printf("threads = %lu, numincrs = %lu\n", (unsigned long)threads, (unsigned long)numincrs);
-    }
 }
 
 int main(int   argc,
@@ -95,19 +147,27 @@ int main(int   argc,
     iprintf("%i shepherds\n", qthread_num_shepherds());
     iprintf("%i threads\n", qthread_num_workers());
 
-    sleeplen = malloc(sizeof(size_t) * numincrs);
+    sleeplen       = malloc(sizeof(size_t) * numincrs);
+    random_numbers = malloc(sizeof(double) * numincrs);
     for (int i = 0; i < numincrs; ++i) {
-        sleeplen[i] = qtimer_fastrand() % 1000;
-        overhead   += sleeplen[i] / 1.0e6;
+        sleeplen[i]         = random() % 1000;
+        random_numbers[i]   = ((double)random() / (double)RAND_MAX);
+        known_correct.max   = (random_numbers[i] > known_correct.max) ? random_numbers[i] : known_correct.max;
+        known_correct.min   = (random_numbers[i] > known_correct.min) ? known_correct.min : random_numbers[i];
+        known_correct.sum  += random_numbers[i];
+        known_correct.prod *= random_numbers[i];
+        overhead           += sleeplen[i] / 1.0e6;
     }
-    overhead /= qthread_num_workers();
+    overhead         /= qthread_num_workers();
+    known_correct.cnt = numincrs;
+    known_correct.avg = known_correct.sum / known_correct.cnt;
 
     timer = qtimer_create();
 
     printf("%-21s %-9s %5s %8s %s\n", "version", "sync", "iters", "incrs", "time");
-    run_iterations(qt_loopaccum_balance_syncvar,      count, sum, overhead, "balanced", "syncvar");
-    run_iterations(qt_loopaccum_balance_sinc, count, sum, overhead, "balanced", "sinc");
-    run_iterations(qt_loopaccum_balance_dc, count, sum, overhead, "balanced", "donecount");
+    run_iterations(qt_loopaccum_balance_syncvar, count, multi_op, overhead, "balanced", "syncvar");
+    run_iterations(qt_loopaccum_balance_sinc, count, multi_op, overhead, "balanced", "sinc");
+    run_iterations(qt_loopaccum_balance_dc, count, multi_op, overhead, "balanced", "donecount");
 
     return 0;
 }
