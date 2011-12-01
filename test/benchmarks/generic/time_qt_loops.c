08f636213bbbcf793a5e6b32aa86a43555ee6fbc
dylan.stark@gmail.com <dylan.stark@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-12-01 23:26:45 +0000

    Updates loop timing code to test with teams and to use qtimer_fastrand() instead of sleep to generate the artificial workload.
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2699 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/benchmarks/generic/time_qt_loops.c
+++ test/benchmarks/generic/time_qt_loops.c
@@ -11,20 +11,27 @@
 
 static aligned_t threads  = 0;
 static aligned_t numincrs = 1024;
-static size_t   *sleeplen;
+static size_t   *randlen;
 static qtimer_t  timer;
 static size_t    numiters = 10;
 
-static void sumsleep(const size_t startat,
-                     const size_t stopat,
-                     void        *arg_)
+static int num_sheps = 0;
+static int num_workers = 0;
+
+static void sumrand(const size_t startat,
+                    const size_t stopat,
+                    void        *arg_)
 {
-    size_t totalsleep = 0;
+    size_t tmp, tmp2;
     qthread_incr(&threads, stopat - startat);
     for (size_t i = startat; i < stopat; ++i) {
-        totalsleep += sleeplen[i];
+        tmp = randlen[i];
+        tmp2 = tmp;
+        while (tmp2 > 0) {
+            tmp += qtimer_fastrand();
+            tmp2--;
+        }
     }
-    usleep(totalsleep);
 }
 
 static void sum(const size_t startat,
@@ -34,15 +41,26 @@ static void sum(const size_t startat,
     qthread_incr(&threads, stopat - startat);
 }
 
-static void run_iterations(void                                                 (*loop)(const size_t a,
-                                                                const size_t    b,
-                                                                const qt_loop_f f,
-                                                                void           *c),
-                           qt_loop_f                                            func,
-                           double                                               overhead,
-                           const char                                          *type,
-                           const char                                          *name)
+typedef void (*loop_f)(const size_t a,
+                       const size_t    b,
+                       const qt_loop_f f,
+                       void           *c);
+
+typedef struct run_args_s {
+    loop_f      loop;
+    qt_loop_f   func;
+    const char *type;
+    const char *name;
+} run_args_t;
+
+static aligned_t run_iterations(void *arg_)
 {
+    run_args_t *arg = (run_args_t *)arg_;
+    loop_f      loop = arg->loop;
+    qt_loop_f   func = arg->func;
+    const char *type = arg->type;
+    const char *name = arg->name;
+
     double total = 0;
 
     for (int i = 0; i < numiters; ++i) {
@@ -53,57 +71,127 @@ static void run_iterations(void                                                 
         qtimer_stop(timer);
         total += qtimer_secs(timer);
     }
-    printf("%-21s %-9s %8lu %f\n", type, name, (unsigned long)numincrs,
-           (total / numiters) - overhead);
+    printf("%5i %7i %-23s %-9s %8lu %f\n", num_sheps, num_workers, type, name, (unsigned long)numincrs,
+           (total / numiters));
+
+    return 0;
 }
 
 int main(int   argc,
          char *argv[])
 {
-    double overhead = 0;
+    int    print_headers = 1;
 
     assert(qthread_initialize() == QTHREAD_SUCCESS);
+    num_sheps   = qthread_num_shepherds();
+    num_workers = qthread_num_workers();
+
     CHECK_VERBOSE();
     NUMARG(numincrs, "NUM_INCRS");
     NUMARG(numiters, "NUM_ITERS");
-    printf("%i shepherds\n", qthread_num_shepherds());
-    printf("%i threads\n", qthread_num_workers());
+    NUMARG(print_headers, "PRINT_HEADERS");
+    if (print_headers) {
+        printf("%i shepherds\n", num_sheps);
+        printf("%i threads\n", num_workers);
+    }
     timer = qtimer_create();
 
-    sleeplen = malloc(sizeof(size_t) * numincrs);
-    for (int i = 0; i < numincrs; ++i) {
-        sleeplen[i] = qtimer_fastrand() % 1000;
-        overhead   += sleeplen[i] / 1.0e6;
-    }
-    overhead /= qthread_num_workers();
+    aligned_t ret;
 
-    printf("Pure Increment\n");
-    printf("%-21s %-9s %8s time\n", "version", "sync", "iters");
+    randlen = malloc(sizeof(size_t) * numincrs);
+    for (int i = 0; i < numincrs; ++i) {
+        randlen[i] = qtimer_fastrand() % 10;
+    }
+
+    if (print_headers) {
+        printf("Pure Increment\n");
+        printf("%-4s %-4s %-23s %-9s %8s time\n", "sheps", "workers", "grouping work looptype", "sync", "iters");
+    }
 
     qt_loop(0, numincrs, sum, NULL);
-    run_iterations(qt_loop_dc,              sum, 0, "thread per iteration", "donecount");
-    run_iterations(qt_loop_aligned,         sum, 0, "thread per iteration", "aligned");
-    run_iterations(qt_loop_sv,              sum, 0, "thread per iteration", "syncvar");
-    run_iterations(qt_loop_sinc,            sum, 0, "thread per iteration", "sinc");
-    run_iterations(qt_loop_balance_dc,      sum, 0, "balanced",             "donecount");
-    run_iterations(qt_loop_balance_aligned, sum, 0, "balanced",             "aligned");
-    run_iterations(qt_loop_balance_sv,      sum, 0, "balanced",             "syncvar");
-    run_iterations(qt_loop_balance_sinc,    sum, 0, "balanced",             "sinc");
 
-    printf("\n");
-    printf("Increment with Sleep (%f secs overhead, %f secs per worker)\n",
-	    overhead * qthread_num_workers(),
-	    overhead);
-    printf("%-21s %-9s %8s time\n", "version", "sync", "iters");
+    run_args_t pure_args[8] = {
+        {qt_loop_dc,              sum, "solo pure TPI",      "donecount"},
+        {qt_loop_aligned,         sum, "solo pure TPI",      "aligned"},
+        {qt_loop_sv,              sum, "solo pure TPI",      "syncvar"},
+        {qt_loop_sinc,            sum, "solo pure TPI",      "sinc"},
+        {qt_loop_balance_dc,      sum, "solo pure balanced", "donecount"},
+        {qt_loop_balance_aligned, sum, "solo pure balanced", "aligned"},
+        {qt_loop_balance_sv,      sum, "solo pure balanced", "syncvar"},
+        {qt_loop_balance_sinc,    sum, "solo pure balanced", "sinc"},
+    };
 
-    run_iterations(qt_loop_dc,              sumsleep, overhead, "thread per iteration", "donecount");
-    run_iterations(qt_loop_aligned,         sumsleep, overhead, "thread per iteration", "aligned");
-    run_iterations(qt_loop_sv,              sumsleep, overhead, "thread per iteration", "syncvar");
-    run_iterations(qt_loop_sinc,            sumsleep, overhead, "thread per iteration", "sinc");
-    run_iterations(qt_loop_balance_dc,      sumsleep, overhead, "balanced",             "donecount");
-    run_iterations(qt_loop_balance_aligned, sumsleep, overhead, "balanced",             "aligned");
-    run_iterations(qt_loop_balance_sv,      sumsleep, overhead, "balanced",             "syncvar");
-    run_iterations(qt_loop_balance_sinc,    sumsleep, overhead, "balanced",             "sinc");
+    for (int i = 0; i < 8; i++) {
+        qthread_fork(run_iterations, &pure_args[i], &ret);
+        qthread_readFE(NULL, &ret);
+    }
+
+    if (print_headers) {
+        printf("Teamed Pure Increment\n");
+        printf("%-4s %-4s %-23s %-9s %8s time\n", "sheps", "workers", "grouping work looptype", "sync", "iters");
+    }
+
+    qt_loop(0, numincrs, sum, NULL);
+
+    run_args_t team_pure_args[8] = {
+        {qt_loop_dc,              sum, "team pure TPI",      "donecount"},
+        {qt_loop_aligned,         sum, "team pure TPI",      "aligned"},
+        {qt_loop_sv,              sum, "team pure TPI",      "syncvar"},
+        {qt_loop_sinc,            sum, "team pure TPI",      "sinc"},
+        {qt_loop_balance_dc,      sum, "team pure balanced", "donecount"},
+        {qt_loop_balance_aligned, sum, "team pure balanced", "aligned"},
+        {qt_loop_balance_sv,      sum, "team pure balanced", "syncvar"},
+        {qt_loop_balance_sinc,    sum, "team pure balanced", "sinc"},
+    };
+
+    for (int i = 0; i < 8; i++) {
+        qthread_fork_new_team(run_iterations, &team_pure_args[i], &ret);
+        qthread_readFE(NULL, &ret);
+    }
+
+    if (print_headers) {
+        printf("\n");
+        printf("Increment with Rand\n");
+        printf("%-4s %-4s %-23s %-9s %8s time\n", "sheps", "workers", "grouping work looptype", "sync", "iters");
+    }
+
+    run_args_t rand_args[8] = {
+        {qt_loop_dc,              sumrand, "solo rand TPI",      "donecount"},
+        {qt_loop_aligned,         sumrand, "solo rand TPI",      "aligned"},
+        {qt_loop_sv,              sumrand, "solo rand TPI",      "syncvar"},
+        {qt_loop_sinc,            sumrand, "solo rand TPI",      "sinc"},
+        {qt_loop_balance_dc,      sumrand, "solo rand balanced", "donecount"},
+        {qt_loop_balance_aligned, sumrand, "solo rand balanced", "aligned"},
+        {qt_loop_balance_sv,      sumrand, "solo rand balanced", "syncvar"},
+        {qt_loop_balance_sinc,    sumrand, "solo rand balanced", "sinc"},
+    };
+    
+    for (int i = 0; i < 8; i++) {
+        qthread_fork(run_iterations, &rand_args[i], &ret);
+        qthread_readFE(NULL, &ret);
+    }
+
+    if (print_headers) {
+        printf("\n");
+        printf("Team Increment with Rand\n");
+        printf("%-4s %-4s %-23s %-9s %8s time\n", "sheps", "workers", "grouping work looptype", "sync", "iters");
+    }
+
+    run_args_t team_rand_args[8] = {
+        {qt_loop_dc,              sumrand, "team rand TPI",      "donecount"},
+        {qt_loop_aligned,         sumrand, "team rand TPI",      "aligned"},
+        {qt_loop_sv,              sumrand, "team rand TPI",      "syncvar"},
+        {qt_loop_sinc,            sumrand, "team rand TPI",      "sinc"},
+        {qt_loop_balance_dc,      sumrand, "team rand balanced", "donecount"},
+        {qt_loop_balance_aligned, sumrand, "team rand balanced", "aligned"},
+        {qt_loop_balance_sv,      sumrand, "team rand balanced", "syncvar"},
+        {qt_loop_balance_sinc,    sumrand, "team rand balanced", "sinc"},
+    };
+    
+    for (int i = 0; i < 8; i++) {
+        qthread_fork_new_team(run_iterations, &team_rand_args[i], &ret);
+        qthread_readFE(NULL, &ret);
+    }
 
     qtimer_destroy(timer);
     return 0;
