a234c19332f811c2dbbf5fe4184ad2d25fc73721
dylan.stark@gmail.com <dylan.stark@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-12-07 20:40:03 +0000

    Adds team fork use cases to thread timing code.
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2745 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/benchmarks/generic/time_threading.c
+++ test/benchmarks/generic/time_threading.c
@@ -12,6 +12,82 @@ static aligned_t null_thread(void *arg)
     return 0;
 }
 
+static aligned_t team_thread(void *arg)
+{
+    qtimer_t     timer = qtimer_create();
+    double       spawn;
+    aligned_t   *rets;
+    syncvar_t   *srets;
+
+    rets = malloc(sizeof(aligned_t) * THREADS);
+
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_empty(rets + i);
+    }
+
+    printf("Priming in team...\n");
+    qtimer_start(timer);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_fork(null_thread, NULL, rets + i);
+    }
+    qtimer_stop(timer);
+    spawn = qtimer_secs(timer);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_readFE(NULL, rets + i);
+    }
+    qtimer_stop(timer);
+    printf("\tCold in team spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tCold in team sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+
+    printf("Timing in team forking...\n");
+    qtimer_start(timer);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_fork(null_thread, NULL, rets + i);
+    }
+    qtimer_stop(timer);
+
+    spawn = qtimer_secs(timer);
+
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_readFF(NULL, rets + i);
+    }
+    qtimer_stop(timer);
+
+    printf("\tWarm in team spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tWarm in team sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    free(rets);
+
+    srets = calloc(THREADS, sizeof(syncvar_t));
+    {
+        const syncvar_t empty = { .u.s = { .data = 0, .state = 2, .lock = 0 } };
+        for (unsigned long i = 0; i < THREADS; ++i) {
+            srets[i] = empty;
+        }
+    }
+    qtimer_start(timer);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_fork_syncvar(null_thread, NULL, srets + i);
+    }
+    qtimer_stop(timer);
+
+    spawn = qtimer_secs(timer);
+
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_syncvar_readFF(NULL, srets + i);
+    }
+    qtimer_stop(timer);
+
+    printf("\tWarm in team syncvar_t spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tWarm in team syncvar_t sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    free(srets);
+
+    qtimer_destroy(timer);
+    return 0;
+}
+
 int main(void)
 {
     qtimer_t     timer = qtimer_create();
@@ -107,15 +183,77 @@ int main(void)
     printf("\tWarm syncvar_t sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
     free(srets);
 
+    rets = malloc(sizeof(aligned_t) * THREADS);
+
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_empty(rets + i);
+    }
+
+    printf("Priming new team...\n");
     qtimer_start(timer);
     for (unsigned long i = 0; i < THREADS; ++i) {
-        qthread_fork(null_thread, NULL, NULL);
+        qthread_fork_new_team(null_thread, NULL, rets + i);
     }
     qtimer_stop(timer);
     spawn = qtimer_secs(timer);
-    printf("\tWarm null spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_readFE(NULL, rets + i);
+    }
+    qtimer_stop(timer);
+    printf("\tCold new team spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tCold new team sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+
+    printf("Timing new team forking...\n");
+    qtimer_start(timer);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_fork_new_team(null_thread, NULL, rets + i);
+    }
+    qtimer_stop(timer);
+
+    spawn = qtimer_secs(timer);
+
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_readFF(NULL, rets + i);
+    }
+    qtimer_stop(timer);
+
+    printf("\tWarm new team spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tWarm new team sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    free(rets);
+
+    srets = calloc(THREADS, sizeof(syncvar_t));
+    {
+        const syncvar_t empty = { .u.s = { .data = 0, .state = 2, .lock = 0 } };
+        for (unsigned long i = 0; i < THREADS; ++i) {
+            srets[i] = empty;
+        }
+    }
+    qtimer_start(timer);
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_fork_syncvar_new_team(null_thread, NULL, srets + i);
+    }
+    qtimer_stop(timer);
+
+    spawn = qtimer_secs(timer);
+
+    for (unsigned long i = 0; i < THREADS; ++i) {
+        qthread_syncvar_readFF(NULL, srets + i);
+    }
+    qtimer_stop(timer);
+
+    printf("\tWarm new team syncvar_t spawn time: %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    spawn = qtimer_secs(timer) - spawn;
+    printf("\tWarm new team syncvar_t sync time : %f usecs, %f/sec\n", 1000000 * spawn / THREADS, THREADS / spawn);
+    free(srets);
 
     qtimer_destroy(timer);
+
+    aligned_t team_thread_ret;
+    qthread_fork_new_team(team_thread, NULL, &team_thread_ret);
+    qthread_readFF(NULL, &team_thread_ret);
+
     return 0;
 }
 
