8f697f29564a116a124a44f9582cd6f37866ba83
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2011-12-08 15:49:42 +0000

    rename cxx tests for more accuracy
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2768 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/test_cxx_2.cpp
+++ test/test_cxx_2.cpp
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <qthread/qthread.h>
 #include <qthread/qloop.hpp>
-#include <qthread/qloop.h>
 
 #include "argparsing.h"
 
 struct test_struct {
-    test_struct(int _x, int* _answer) : x(_x), answer(_answer) {}
+    test_struct(int  _x,
+                int *_answer) : x(_x), answer(_answer) {}
 
-    void operator() (int start, int stop)
+    void operator() (int start,
+                     int stop)
     {
-      iprintf("start: %4d    stop: %4d\n", start, stop);
-
-      for (int i = start; i < stop; ++i) answer[i] = x * i;
+        iprintf("start: %4d    stop: %4d\n", start, stop);
+        for (int i = start; i < stop; ++i) {
+            answer[i] = x * i;
+        }
     }
 
-    int x;
-    int* answer;
+    int  x;
+    int *answer;
 };
 
 void test_func()
 {
-    int* answer = new int[100];
+    int *answer = new int[100];
+
     for (int i = 0; i < 100; ++i) answer[i] = 0;
 
 //    qt_loop(0, 100, test_struct(3, answer));
@@ -30,17 +33,17 @@ void test_func()
 //    qt_loop_balance_future(0, 100, test_struct(3, answer));
 
     iprintf("\nanswer:");
-    for (int i = 0; i < 100; ++i)
-    {
-      iprintf("%6d", answer[i]);
-      if (i % 10 == 9) iprintf("\n       ");
+    for (int i = 0; i < 100; ++i) {
+        iprintf("%6d", answer[i]);
+        if (i % 10 == 9) { iprintf("\n       "); }
     }
     iprintf("\n");
 
-    delete [] answer;
+    delete[] answer;
 }
 
-int main(int argc, char **argv)
+int main(int    argc,
+         char **argv)
 {
     assert(qthread_initialize() == 0);
 
@@ -50,3 +53,5 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+/* vim:set expandtab: */
