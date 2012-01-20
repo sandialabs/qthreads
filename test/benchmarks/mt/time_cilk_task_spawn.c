602eb79069d0b2009bf48f9a783157c5d4a156f7
kbwheel@gmail.com <kbwheel@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-01-20 19:46:17 +0000

    i lied
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2828 9dbd018f-d1da-d342-4c11-58c73dfe904b

---
+++ test/benchmarks/mt/time_cilk_task_spawn.c
@@ -0,0 +1,74 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h" /* for _GNU_SOURCE */
+#endif
+
+#include <assert.h>
+#include <stdio.h>
+#include <cilk/cilk.h>
+#include <qthread/qthread.h>
+#include <qthread/qtimer.h>
+#include "argparsing.h"
+
+static aligned_t donecount = 0;
+
+static double   global_scratch = 0;
+static unsigned long num_iterations = 0;
+
+static double delay(void)
+{
+    double d = 0;
+    unsigned long i;
+
+    for (i = 0; i < num_iterations; i++) d += 1 / (2.0 * i + 1);
+    return d;
+}
+
+static aligned_t null_task(void *args_)
+{
+    global_scratch = delay();
+
+    return qthread_incr(&donecount, 1);
+}
+
+int main(int   argc,
+         char *argv[])
+{
+    unsigned long count = 0;
+    unsigned long i;
+
+    qtimer_t timer;
+    double   total_time = 0.0;
+
+    CHECK_VERBOSE();
+
+    NUMARG(num_iterations, "MT_NUM_ITERATIONS");
+    NUMARG(count, "MT_COUNT");
+    assert(0 != count);
+
+    timer = qtimer_create();
+    qtimer_start(timer);
+
+    for (i = 0; i < count; i++) {
+	_Cilk_spawn null_task(NULL);
+    }
+    _Cilk_sync;
+    /*do {
+        qthread_yield();
+    } while (donecount != count);*/
+
+    qtimer_stop(timer);
+
+    total_time = qtimer_secs(timer);
+
+    qtimer_destroy(timer);
+
+    printf("%lu %lu %lu %f\n",
+           (unsigned long)qthread_num_workers(),
+           (unsigned long)count,
+           (unsigned long)num_iterations,
+           total_time);
+
+    return 0;
+}
+
+/* vim:set expandtab */
