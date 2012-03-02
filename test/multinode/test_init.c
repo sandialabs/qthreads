a1a6224f23872964b3ca38279d7d7366a5d543dc
dylan.stark@gmail.com <dylan.stark@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-03-02 21:17:41 +0000

    Updates `multinode/test_init` to run on one node.
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@2984 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/multinode/test_init.c
+++ test/multinode/test_init.c
@@ -40,7 +40,8 @@ int main(int argc,
     qthread_multinode_run();
     if (my_id != 0) return 2;
 
-    retval = qthread_fork_remote(returner, &foobar, &ret, 1, sizeof(long));
+    int target = (world_size > 1) ? 1 : 0;
+    retval = qthread_fork_remote(returner, &foobar, &ret, target, sizeof(long));
     if (retval != 0) {
         fprintf(stderr, "(%03d) fork_remote returned %d\n", my_id, retval);
         return 3;
