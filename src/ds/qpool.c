0c43bc7b95fc19489735af858c64e1040d2690b0
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-03-29 14:44:42 +0000

    add forcibly unpooled version
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3062 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/ds/qpool.c
+++ src/ds/qpool.c
@@ -2,35 +2,70 @@
 # include "config.h"
 #endif
 
+/* System Headers */
+#ifdef UNPOOLED
+#include <stdlib.h> /* for malloc() */
+#endif
+
 /* Internal Headers */
 #include "qthread/qpool.h"
 #include "qt_mpool.h"
+#include "qthread_asserts.h"
+
+#undef UNPOOLED
+
+#ifdef UNPOOLED
+struct qt_mpool_s {
+    size_t size;
+    size_t alignment;
+};
+#endif
 
 qpool *qpool_create_aligned(const size_t isize,
                             size_t       alignment)
 {                                      /*{{{ */
+#ifdef UNPOOLED
+    qpool *ret = malloc(sizeof(struct qt_mpool_s));
+    assert(ret);
+    ret->size = isize;
+    ret->alignment = alignment;
+    return ret;
+#else
     return qt_mpool_create_aligned(isize, alignment);
+#endif
 }                                      /*}}} */
 
 qpool *qpool_create(const size_t item_size)
 {                                      /*{{{ */
-    return qt_mpool_create_aligned(item_size, 0);
+    return qpool_create_aligned(item_size, 0);
 }                                      /*}}} */
 
 void *qpool_alloc(qpool *pool)
 {
+#ifdef UNPOOLED
+    return malloc(pool->size);
+#else
     return qt_mpool_alloc(pool);
+#endif
 }
 
 void qpool_free(qpool *restrict pool,
                 void *restrict  mem)
 {
+#ifdef UNPOOLED
+    free(mem);
+#else
     qt_mpool_free(pool, mem);
+#endif
 }
 
 void qpool_destroy(qpool *pool)
 {
+#ifdef UNPOOLED
+    free(pool);
+#else
     qt_mpool_destroy(pool);
+#endif
 }
 
 /* vim:set expandtab: */
