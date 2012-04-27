721d9d87c061959a8d40d367130f4ee0a5e0e9e1
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-04-27 17:12:59 +0000

    alternative spawns
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3156 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/benchmarks/sc12/spawn_parallel_qthreads.c
+++ test/benchmarks/sc12/spawn_parallel_qthreads.c
@@ -14,16 +14,26 @@ static void par_null_task(size_t start,
                           void  *args_)
 {}
 
+static void par_null_task2(size_t start,
+                          size_t stop,
+                          void  *args_)
+{
+    for (size_t i=start; i<stop; ++i) {
+    }
+}
+
 int main(int   argc,
          char *argv[])
 {
-    uint64_t count    = 1048576;
+    uint64_t count      = 1048576;
+    uint64_t loop_style = 1;
 
     qtimer_t timer;
     double   total_time = 0.0;
 
     CHECK_VERBOSE();
 
+    NUMARG(loop_style, "LOOP_STYLE");
     NUMARG(count, "MT_COUNT");
     assert(0 != count);
 
@@ -31,9 +41,42 @@ int main(int   argc,
 
     timer = qtimer_create();
 
-    qtimer_start(timer);
-    qt_loop(0, count, par_null_task, NULL);
-    qtimer_stop(timer);
+    switch (loop_style) {
+	case 1:
+	    qtimer_start(timer);
+	    qt_loop(0, count, par_null_task, NULL);
+	    qtimer_stop(timer);
+	    break;
+	case 2:
+	    qtimer_start(timer);
+	    qt_loop_parallel(0, count, par_null_task2, NULL);
+	    qtimer_stop(timer);
+	    break;
+	case 3:
+	    qtimer_start(timer);
+	    qqloop_handle_t *l = qt_loop_queue_create(CHUNK, 0, count, 1, par_null_task2, NULL);
+	    qt_loop_queue_run(l);
+	    qtimer_stop(timer);
+	    break;
+	case 4:
+	    qtimer_start(timer);
+	    qqloop_handle_t *l = qt_loop_queue_create(GUIDED, 0, count, 1, par_null_task2, NULL);
+	    qt_loop_queue_run(l);
+	    qtimer_stop(timer);
+	    break;
+	case 5:
+	    qtimer_start(timer);
+	    qqloop_handle_t *l = qt_loop_queue_create(FACTORED, 0, count, 1, par_null_task2, NULL);
+	    qt_loop_queue_run(l);
+	    qtimer_stop(timer);
+	    break;
+	case 6:
+	    qtimer_start(timer);
+	    qqloop_handle_t *l = qt_loop_queue_create(TIMED, 0, count, 1, par_null_task2, NULL);
+	    qt_loop_queue_run(l);
+	    qtimer_stop(timer);
+	    break;
+    }
 
     total_time = qtimer_secs(timer);
 
