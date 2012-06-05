81051d13be83a6192cda3713485b0aa88b83b70a
sbarlea@gmail.com <sbarlea@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-06-05 20:47:22 +0000

    Added in depth comments to dicitonary header file
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3204 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread/dictionary.h
+++ include/qthread/dictionary.h
@@ -40,11 +40,30 @@ typedef struct {
 	int bkt;
 } qt_dictionary_iterator;
 
+
+/*
+ * 	Creates a dictionary, with the key comparison function parameter
+ * and a hashcode function
+ *
+ * the signature of the key comparsion function is: int my_key_equals(void* key1, void* key2)
+ * 		and it should return 1 ("true") if the keys are equal and 0 otherwise
+ *
+ * the signature of the hashcode function is: int my_hashcode(void* string)
+ * 		and it can return any integer value.
+ *
+ * if my_key_equals (A, B) = 1, then my_hashcode(A) == my_hashcode(B)
+ */ 
 qt_dictionary* qt_dictionary_create(key_equals eq, hashcode hash);
+
+/*
+	Destroys the dictionary d
+	d must be empty (keys and values must have been cleaned up already (or else, leaks)
+	
+*/
 void qt_dictionary_destroy(qt_dictionary* d);
 
 /*
-	inserts a key, value pair in the dictionary
+	Inserts a key, value pair in the dictionary
 	returns void*:
 			ADDR - address of item found in the hashmap after the current put
 			NULL - if the insert failed because of an error
@@ -53,7 +72,7 @@ void qt_dictionary_destroy(qt_dictionary* d);
 void* qt_dictionary_put(qt_dictionary* dict, void* key, void* value);
 
 /*
-	inserts a key, value pair in the dictionary
+	Inserts a key, value pair in the dictionary
 	returns void*:
 			ADDR - address of item found in the hashmap after the current put
 			(if "key" was present in the dictionary, the address of the old
@@ -64,7 +83,7 @@ void* qt_dictionary_put(qt_dictionary* dict, void* key, void* value);
 void* qt_dictionary_put_if_absent(qt_dictionary* dict, void* key, void* value);
 
 /*
-	gets a value from the dictionary for a given key
+	Gets a value from the dictionary for a given key
 	returns:
 			item - if get was successful and 	
 				item was present in the dictionary
@@ -74,7 +93,7 @@ void* qt_dictionary_put_if_absent(qt_dictionary* dict, void* key, void* value);
 void* qt_dictionary_get(qt_dictionary* dict, void* key);
 
 /*
-	removes a key,value pair from the dictionary
+	Removes a key,value pair from the dictionary
 	returns:
 			item - if the item was present and successfully removed
 			NULL - if the item identified by key was not in the hash
@@ -84,7 +103,7 @@ void* qt_dictionary_delete(qt_dictionary* dict, void* key);
 
 
 /*
-	creates a new iterator on the dictionary dict
+	Creates a new iterator on the dictionary dict
 	returns:
 			addr - address of new iterator if creation was successful
 			ERROR - if an error occurred
@@ -93,13 +112,13 @@ void* qt_dictionary_delete(qt_dictionary* dict, void* key);
 qt_dictionary_iterator* qt_dictionary_iterator_create(qt_dictionary* dict);
 
 /*
-	destroys the iterator it
+	Destroys the iterator it
 	
 */
 void qt_dictionary_iterator_destroy(qt_dictionary_iterator* it);
 
 /*
-	advances the iterator and retrieves the next entry
+	Advances the iterator and retrieves the next entry
 	returns:
 			addr - address of the next entry in the hash, if one was found
 			NULL -  if there are no more elements in hash
@@ -111,7 +130,7 @@ void qt_dictionary_iterator_destroy(qt_dictionary_iterator* it);
 list_entry* qt_dictionary_iterator_next(qt_dictionary_iterator* it);
 
 /*
-	retrieves the curent entry in iterator it
+	Retrieves the curent entry in iterator it
 	returns:
 			addr - address of the next entry in the hash, if one was found
 			NULL -  if there are no more elements in hash
@@ -123,7 +142,7 @@ list_entry* qt_dictionary_iterator_next(qt_dictionary_iterator* it);
 list_entry* qt_dictionary_iterator_get(const qt_dictionary_iterator* it);
 
 /*
-	creates a new iterator, pointing to the item past the end of the list (which is NULL)
+	Creates a new iterator, pointing to the item past the end of the list (which is NULL)
 	returns:
 		addr - address of the newly created iterator
 		NULL - if dictionary is null or not allocated properly
@@ -131,14 +150,14 @@ list_entry* qt_dictionary_iterator_get(const qt_dictionary_iterator* it);
 qt_dictionary_iterator* qt_dictionary_end(qt_dictionary* dict);
 
 /*
-	tests equality for two iterators
+	Tests equality for two iterators
 	requires that they are both walking the same dictionaries and are located on the same positions
 */
 int qt_dictionary_iterator_equals(qt_dictionary_iterator*a, qt_dictionary_iterator* b);
 
 
 /*
- * displays debugging info for dictionary buckets 
+ * Displays debugging info for dictionary buckets 
  */ 
 void qt_dictionary_printbuckets(qt_dictionary* dict);
 
