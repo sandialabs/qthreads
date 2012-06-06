4680f2ede05c188c86cb8b040bad98c5cd694491
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-06-06 14:21:45 +0000

    apparently #include-ing assert.h after qthread_asserts.h will re-override my #def of assert(), but then re-including qthread_asserts.h will not re-re-override the #def. Well, it will NOW.
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3208 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qthread_asserts.h
+++ include/qthread_asserts.h
@@ -1,32 +1,51 @@
+/* This does not do the whole file, because assert.h doesn't avoid re-defining itself either */
 #ifndef QTHREAD_ASSERTS_H
 #define QTHREAD_ASSERTS_H
+#endif
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
 
-#ifndef QTHREAD_NO_ASSERTS
-# include <assert.h> /* for assert() */
-#endif
+#include <assert.h>  /* for assert() */
 
-#ifdef QTHREAD_NO_ASSERTS
-# define qassert(op, val) op
-# define qassertnot(op, val) op
+#ifdef qassert
+# undef qassert
+#endif
+#ifdef qassertnot
+# undef qassertnot
+#endif
 # ifdef assert
 #  undef assert
 # endif
-# define assert(foo)
-# define qassert_ret(assertion, retval) do { if (!(assertion)) { return retval; } } while (0)
-# define qassert_retvoid(assertion) do { if (!(assertion)) { return; } } while (0)
-# define qassert_goto(assertion, tag) do { if (!(assertion)) { goto tag; } } while (0)
-#define qgoto(tag) tag:
-#else
-# define qassert(op, val) assert(op == val)
-# define qassertnot(op, val) assert(op != val)
-# define qassert_ret(assertion, retval) assert(assertion)
-# define qassert_retvoid(assertion) assert(assertion)
-# define qassert_goto(assertion, tag) assert(assertion)
-# define qgoto(tag)
+#ifdef qassert_ret
+# undef qassert_ret
+#endif
+#ifdef qassert_retvoid
+# undef qassert_retvoid
+#endif
+#ifdef qassert_goto
+# undef qassert_goto
+#endif
+#ifdef qgoto
+# undef qgoto
 #endif
 
-#endif
+#ifdef QTHREAD_NO_ASSERTS
+# define qassert(op, val)    op
+# define qassertnot(op, val) op
+# define assert(foo)
+# define qassert_ret(assertion, retval) do { if (!(assertion)) { return retval; } } while (0)
+# define qassert_retvoid(assertion)     do { if (!(assertion)) { return; } } while (0)
+# define qassert_goto(assertion, tag)   do { if (!(assertion)) { goto tag; } } while (0)
+# define qgoto(tag)                     tag :
+#else // ifdef QTHREAD_NO_ASSERTS
+# define qassert(op, val)               assert(op == val)
+# define qassertnot(op, val)            assert(op != val)
+# define qassert_ret(assertion, retval) assert(assertion)
+# define qassert_retvoid(assertion)     assert(assertion)
+# define qassert_goto(assertion, tag)   assert(assertion)
+# define qgoto(tag)
+#endif // ifdef QTHREAD_NO_ASSERTS
+
+/* vim:set expandtab: */
