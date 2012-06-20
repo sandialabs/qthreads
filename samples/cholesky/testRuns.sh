07bb9fd39e6d1e6b80bfcbf85fb20dba01ef82c9
sbarlea@gmail.com <sbarlea@gmail.com@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-06-20 20:50:25 +0000

    Updated testing script for time as function of input size (constant tile size)
    git-svn-id: https://qthreads.googlecode.com/svn/qtCnC@3284 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- samples/cholesky/testRuns.sh
+++ samples/cholesky/testRuns.sh
@@ -1,21 +1,24 @@
 INPUT_DIR=input
 OUTPUT_DIR=output
 REF_DIR=reference_output
-
-#for i in 4 2 1
-for SIZE in 1000 2000 #3000 4000 5000 6000 7000 9000
-        do
-        for j in $((${SIZE}/8)) $((${SIZE}/10)) $((${SIZE}/20)) $((${SIZE}/50))
-        do
-                echo SIZE = ${SIZE} TILE = ${j}
-                ./bin/cholesky ${SIZE} ${j}-i ${INPUT_DIR}/${SIZE}.in -o ${OUTPUT_DIR}/${SIZE}.out
-                #-th $i
-                #echo Threads ${i}  Size ${SIZE} Tile ${j}
-                echo
-                if [ $? -ne '0' ]; then
-                        echo "Received error...interrupt run!"
-                        exit 2
-                fi
-                diff -q  ${OUTPUT_DIR}/${SIZE}.out ${REF_DIR}/${SIZE}_ref.out
-        done
+svn info
+uname -a
+for SIZE in 1000 2000 3000 4000 5000 6000 7000 9000
+do
+	for i in 1 2 3 4 5 6 7 8 9
+	do
+			for j in 125 #$((${SIZE}/8)) $((${SIZE}/10)) $((${SIZE}/20)) $((${SIZE}/50))
+			do
+					echo SIZE = ${SIZE} TILE = ${j}
+					./bin/cholesky ${SIZE} ${j} -i ${INPUT_DIR}/${SIZE}.in -o ${OUTPUT_DIR}/${SIZE}.out
+					#-th $i
+					#echo Threads ${i}  Size ${SIZE} Tile ${j}
+					echo
+					if [ $? -ne '0' ]; then
+							echo "Received error...interrupt run!"
+							exit 2
+					fi
+					diff -q  --ignore-space-change  ${OUTPUT_DIR}/${SIZE}.out ${REF_DIR}/${SIZE}_ref.out
+			done
+	done
 done
