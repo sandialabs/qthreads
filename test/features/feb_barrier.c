5de09517026bb331a70f88a277d989594fa2bd8f
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-07-06 20:11:43 +0000

    test barrier a bit more repeatedly
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3346 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- test/features/feb_barrier.c
+++ test/features/feb_barrier.c
@@ -8,9 +8,9 @@
 #include <qthread/qtimer.h>
 #include "argparsing.h"
 
-aligned_t           initme_idx = 0;
-volatile aligned_t *initme     = NULL;
-qt_feb_barrier_t   *wait_on_me;
+aligned_t         initme_idx = 0;
+aligned_t        *initme     = NULL;
+qt_feb_barrier_t *wait_on_me;
 
 static aligned_t barrier_thread(void *arg)
 {
@@ -25,62 +25,59 @@ static aligned_t barrier_thread(void *arg)
 int main(int   argc,
          char *argv[])
 {
-    size_t     threads = 1000, i;
-    aligned_t *rets;
-    qtimer_t   t;
+    size_t       threads = 1000, i;
+    aligned_t   *rets;
+    qtimer_t     t;
+    unsigned int iter, iterations = 10;
 
     assert(qthread_initialize() == 0);
     t = qtimer_create();
 
     CHECK_VERBOSE();
     NUMARG(threads, "THREADS");
-
-    iprintf("%i shepherds...\n", qthread_num_shepherds());
-    iprintf("%i threads...\n", (int)threads);
+    NUMARG(iterations, "ITERATIONS");
 
     initme = (aligned_t *)calloc(threads, sizeof(aligned_t));
     assert(initme);
 
-    rets = (aligned_t *)malloc(threads * sizeof(aligned_t));
+    rets = (aligned_t *)malloc(iterations * threads * sizeof(aligned_t));
     assert(rets);
 
     iprintf("creating the barrier for %zu threads\n", threads + 1);
     wait_on_me = qt_feb_barrier_create(threads + 1);    // all my spawnees plus me
     assert(wait_on_me);
 
-    iprintf("forking the threads\n");
-    for (i = 0; i < threads; i++) {
-        qthread_fork(barrier_thread, wait_on_me, rets + i);
-    }
-    iprintf("done forking the threads, entering the barrier\n");
-    qtimer_start(t);
-    qt_feb_barrier_enter(wait_on_me);
-    qtimer_stop(t);
-    iprintf("main thread exited barrier 1 in %f seconds\n", qtimer_secs(t));
-    initme_idx = 0;
-
-    for (i = 0; i < threads; i++) {
-        qthread_fork(barrier_thread, wait_on_me, rets + i);
-    }
-    qtimer_start(t);
-    qt_feb_barrier_enter(wait_on_me);
-    qtimer_stop(t);
-    iprintf("main thread exited barrier 2 in %f seconds\n", qtimer_secs(t));
-
-    for (i = 0; i < threads; i++) {
-        if (initme[i] != 2) {
-            iprintf("initme[%i] = %i (should be 2)\n", (int)i,
-                    (int)initme[i]);
+    for (iter = 0; iter < iterations; iter++) {
+        iprintf("%i: forking the threads\n", iter);
+        for (i = 0; i < threads; i++) {
+            qthread_fork(barrier_thread, wait_on_me, rets + (iter * threads) + i);
         }
-        assert(initme[i] == 2);
+        iprintf("%i: done forking the threads, entering the barrier\n", iter);
+        qtimer_start(t);
+        qt_feb_barrier_enter(wait_on_me);
+        qtimer_stop(t);
+        iprintf("%i: main thread exited barrier in %f seconds\n", iter, qtimer_secs(t));
+
+        initme_idx = 0;
+
+        for (i = 0; i < threads; i++) {
+            if (initme[i] != iter + 1) {
+                iprintf("initme[%i] = %i (should be %i)\n", (int)i,
+                        (int)initme[i], iter + 1);
+            }
+            assert(initme[i] == iter + 1);
+        }
     }
+
     iprintf("Success!\n");
 
     /* this loop shouldn't be necessary... but seems to avoid crashes in rare
      * cases (in other words there must a race condition in qthread_finalize()
      * if there are outstanding threads out there) */
-    for (i = 0; i < threads; i++) {
-        qthread_readFF(NULL, rets + i);
+    for (i = 0; i < threads * 2; i++) {
+        aligned_t tmp = 1;
+        qthread_readFF(&tmp, rets + i);
+        assert(tmp == 0);
     }
     return 0;
 }
