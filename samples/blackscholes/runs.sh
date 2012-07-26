ee8d0ce2e762295d1bc53393941d34f94eef0589
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-07-26 18:10:14 +0000

    runs for workshop
    git-svn-id: https://qthreads.googlecode.com/svn/qtCnC@3501 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- samples/blackscholes/runs.sh
+++ samples/blackscholes/runs.sh
@@ -1,8 +1,13 @@
 svn info
 uname -a
+export QT_STACK_SIZE=$((1024*1024*2))
 
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-	/usr/bin/time -v ./blackscholes 5000000 100
-	echo					
+for OPT in {1..3} ; do
+	for SCALE in 1 2 4 8 16 24 32 ; do
+		for i in {1..10} ; do 
+			export QT_HWPAR=$SCALE
+			/usr/bin/time -v ./blackscholes_opt$OPT 5000000 100
+			echo					
+		done 2>&1 | tee "log-opt$OPT-t$SCALE.txt"
+	done
 done
