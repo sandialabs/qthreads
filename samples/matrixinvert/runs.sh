d7ab93482340aa5b2ed66c197a8c86d3f418a31f
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-07-26 20:51:04 +0000

    ignores and runs
    git-svn-id: https://qthreads.googlecode.com/svn/qtCnC@3505 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- samples/matrixinvert/runs.sh
+++ samples/matrixinvert/runs.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
 svn info
 uname -a
-export QT_STACK_SIZE=1048576
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-	/usr/bin/time -v ./matrixinvert 2048
-	echo					
+export QT_STACK_SIZE=$((1024*1024*2))
+for OPT in {1..3} ; do
+	for SCALE in 1 2 4 8 16 24 32 ; do
+		for SIZE in 2048 ; do
+			for i in {1..10} ; do
+				export QT_HWPAR=$SCALE
+				echo OPT = $OPT SCALE = $SCALE i = $i
+				/usr/bin/time -v ./matrixinvert_opt$OPT $SIZE
+				echo
+				if [ $? -ne '0' ]; then
+					echo "Received error...interrupt run!"
+					exit 2
+				fi
+			done
+		done 2>&1 | tee "log-opt$OPT-t$SCALE.txt"
+	done
 done
