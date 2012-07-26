add17bbe49d9c1ba783cb77075c68c15ab1994b1
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-07-26 17:51:33 +0000

    benchmarks for paper
    git-svn-id: https://qthreads.googlecode.com/svn/qtCnC@3498 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- samples/cholesky/runs.sh
+++ samples/cholesky/runs.sh
@@ -1,22 +1,26 @@
 INPUT_DIR=input
 OUTPUT_DIR=output
 REF_DIR=reference_output
+export QT_STACK_SIZE=$((1024*1024*2))
 svn info
 uname -a
-for SIZE in 5000
-do
-	for i in 1 2 3 4 5 6 7 8 9 10
-	do
-			for j in 125 
-			do
-					echo SIZE = ${SIZE} TILE = ${j}
-					/usr/bin/time -v ./cholesky ${SIZE} ${j} -i ${INPUT_DIR}/${SIZE}.in -o ${OUTPUT_DIR}/${SIZE}.out
+for OPT in {1..3} ; do 
+	for SCALE in 1 2 4 8 16 24 32 ; do
+		for SIZE in 4000 ; do 
+			for i in {1..10} ; do 
+				for j in 125 ; do
+					echo SIZE = ${SIZE} TILE = ${j} SCALE = $SCALE i = $i
+					echo /usr/bin/time -v ./cholesky_opt${OPT} ${SIZE} ${j} -i ${INPUT_DIR}/${SIZE}.in
+					export QT_HWPAR=$SCALE
+					/usr/bin/time -v ./cholesky_opt${OPT} ${SIZE} ${j} -i ${INPUT_DIR}/${SIZE}.in
 					echo
 					if [ $? -ne '0' ]; then
-							echo "Received error...interrupt run!"
-							exit 2
+						echo "Received error...interrupt run!"
+						exit 2
 					fi
-					diff -q  --ignore-space-change  ${OUTPUT_DIR}/${SIZE}.out ${REF_DIR}/${SIZE}_ref.out
+					#diff -q  --ignore-space-change  ${OUTPUT_DIR}/${SIZE}.out ${REF_DIR}/${SIZE}_ref.out
+				done
 			done
+		done 2>&1 | tee "log-opt$OPT-t$SCALE.txt"
 	done
 done
