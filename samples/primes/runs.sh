505f0bf7eb3787f101c3dcf2d5a02664b4b5c69c
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-07-26 20:53:46 +0000

    git-svn-id: https://qthreads.googlecode.com/svn/qtCnC@3508 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- samples/primes/runs.sh
+++ samples/primes/runs.sh
@@ -1,8 +1,14 @@
 svn info
 uname -a
+export QT_STACK_SIZE=$((1024*1024*2))
 
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-	/usr/bin/time -v ./primes_opt$OPT 1500000
-	echo					
+for OPT in {1..3} ; do
+	for SCALE in 1 2 4 8 16 24 32 ; do
+		for i in {1..10} ; do
+			export QT_HWPAR=$SCALE
+			echo OPT = $OPT SCALE = $SCALE i = $i
+			/usr/bin/time -v ./primes_opt$OPT 500000
+			echo					
+		done 2>&1 | tee "log-opt$OPT-t$SCALE.txt"
+	done
 done
