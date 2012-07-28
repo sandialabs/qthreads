690bf780dbac068870b5f5dc674137e6bd5b1c1d
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-07-28 21:07:05 +0000

    how the results were generated
    git-svn-id: https://qthreads.googlecode.com/svn/qtCnC@3511 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- samples/blackscholes/runs.sh
+++ samples/blackscholes/runs.sh
@@ -6,7 +6,8 @@ for OPT in {1..3} ; do
 	for SCALE in 1 2 4 8 16 24 32 ; do
 		for i in {1..10} ; do 
 			export QT_HWPAR=$SCALE
-			/usr/bin/time -v ./blackscholes_opt$OPT 5000000 100
+			echo OPT = $OPT SCALE = $SCALE i = $i
+			/usr/bin/time -v ./blackscholes_opt$OPT 1500000 100
 			echo					
 		done 2>&1 | tee "log-opt$OPT-t$SCALE.txt"
 	done
