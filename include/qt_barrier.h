4a7a11275ec356023b220fedcf6731a25d22e563
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-08-02 17:35:03 +0000

    fix datatype on prototype
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3531 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- include/qt_barrier.h
+++ include/qt_barrier.h
@@ -26,7 +26,7 @@ typedef unsigned int qthread_shepherd_id_t; // potentially included in qthread.c
                                             // qthread.h but too early to include whole file
 */
 qt_barrier_t *qt_barrier_create(int size, enum barrierType type, int debug);
-void qt_barrier_enter(qt_barrier_t *b, unsigned int shep);
+void qt_barrier_enter(qt_barrier_t *b, qthread_shepherd_id_t shep);
 void qt_barrier_dump(qt_barrier_t *b, enum dumpType dt);
 void qt_barrier_resize(size_t size);
 void qt_barrier_destroy(qt_barrier_t *b);
