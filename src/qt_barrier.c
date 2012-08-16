91d233688720ef71d715dfe56397cc3a2e7f9647
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-08-16 18:13:42 +0000

    fix compile error
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3565 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/qt_barrier.c
+++ src/qt_barrier.c
@@ -17,6 +17,7 @@
 #include "qthread/qthread.h"
 #include "qt_debug.h"
 #include "qt_visibility.h"
+#include "qthread_innards.h" /* for qthread_library_initialized */
 
 struct qt_barrier_s {
     int        count;           // size of barrier
