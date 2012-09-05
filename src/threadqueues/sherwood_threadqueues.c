f6a8d08a5c8e562931ce099b219320177a535f4a
kbwheel <kbwheel@9dbd018f-d1da-d342-4c11-58c73dfe904b>
2012-09-05 20:54:44 +0000

    paranoia
    git-svn-id: https://qthreads.googlecode.com/svn/trunk@3635 9dbd018f-d1da-d342-4c11-58c73dfe904b

--- src/threadqueues/sherwood_threadqueues.c
+++ src/threadqueues/sherwood_threadqueues.c
@@ -88,6 +88,30 @@ void qt_spin_exclusive_unlock(qt_spin_exclusive_t *l)
 /* end of added functions - AKP */
 #endif /* if AKP_DEBUG */
 
+#ifdef QTHREAD_PARANOIA
+static inline void sanity_check_queue(qt_threadqueue_t *q)
+{
+    qt_threadqueue_node_t *cursor          = q->head;
+    size_t                 count_stealable = 0, count_total = 0;
+
+    assert((q->head == NULL) || q->qlength);
+    assert(q->qlength_stealable <= q->qlength);
+    assert((q->head && q->tail) || (!q->head && !q->tail));
+
+    while (cursor) {
+        count_total++;
+        count_stealable += cursor->stealable;
+        cursor           = cursor->next;
+    }
+    assert(count_total == q->qlength);
+    assert(count_stealable == q->qlength_stealable);
+}
+
+# define PARANOIA_ONLY(x) x
+#else /* ifndef QTHREAD_NO_ASSERTS */
+# define PARANOIA_ONLY(x)
+#endif /* ifndef QTHREAD_NO_ASSERTS */
+
 /* Memory Management */
 #if defined(UNPOOLED_QUEUES) || defined(UNPOOLED)
 # define ALLOC_THREADQUEUE() (qt_threadqueue_t *)MALLOC(sizeof(qt_threadqueue_t))
@@ -141,6 +165,7 @@ ssize_t INTERNAL qt_threadqueue_advisory_queuelen(qt_threadqueue_t *q)
 #else
     ssize_t tmp;
     QTHREAD_TRYLOCK_LOCK(&q->qlock);
+    PARANOIA_ONLY(sanity_check_queue(q));
     tmp = q->qlength;
     QTHREAD_TRYLOCK_UNLOCK(&q->qlock);
     return tmp;
@@ -182,6 +207,7 @@ void INTERNAL qt_threadqueue_free(qt_threadqueue_t *q)
     if (q->head != q->tail) {
         qthread_t *t;
         QTHREAD_TRYLOCK_LOCK(&q->qlock);
+        PARANOIA_ONLY(sanity_check_queue(q));
         while (q->head != q->tail) {
             qt_threadqueue_node_t *node = q->tail;
             if (node != NULL) {
@@ -228,6 +254,7 @@ void INTERNAL qt_threadqueue_enqueue(qt_threadqueue_t *restrict q,
     assert(t != NULL);
 
     QTHREAD_TRYLOCK_LOCK(&q->qlock);
+    PARANOIA_ONLY(sanity_check_queue(q));
     node->next = NULL;
     node->prev = q->tail;
     q->tail    = node;
@@ -364,6 +391,7 @@ void INTERNAL qt_threadqueue_enqueue_yielded(qt_threadqueue_t *restrict q,
     assert(t != NULL);
 
     QTHREAD_TRYLOCK_LOCK(&q->qlock);
+    PARANOIA_ONLY(sanity_check_queue(q));
     node->prev = NULL;
     node->next = q->head;
     q->head    = node;
@@ -441,7 +469,7 @@ qthread_t INTERNAL *qt_scheduler_get_thread(qt_threadqueue_t         *q,
                 }
 #else           /* if 0 */
                 QTHREAD_TRYLOCK_LOCK(&q->qlock);
-                assert((q->head && q->tail) || (!q->head && !q->tail));
+                PARANOIA_ONLY(sanity_check_queue(q));
                 assert(q->head != first);
                 assert(q->tail != last);
                 first->prev = q->tail;
@@ -463,6 +491,7 @@ qthread_t INTERNAL *qt_scheduler_get_thread(qt_threadqueue_t         *q,
             }
         } else if (q->head) {
             QTHREAD_TRYLOCK_LOCK(&q->qlock);
+            PARANOIA_ONLY(sanity_check_queue(q));
             node = q->tail;
             if (node != NULL) {
                 q->tail = node->prev;
@@ -548,6 +577,7 @@ void INTERNAL qt_threadqueue_enqueue_multiple(qt_threadqueue_t      *q,
     }
 
     QTHREAD_TRYLOCK_LOCK(&q->qlock);
+    PARANOIA_ONLY(sanity_check_queue(q));
     last->next  = NULL;
     first->prev = q->tail;
     q->tail     = last;
@@ -585,6 +615,7 @@ qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_steal(qt_threadqueue_t *h
     if (!QTHREAD_TRYLOCK_TRY(&v->qlock)) {
         return NULL;
     }
+    PARANOIA_ONLY(sanity_check_queue(v));
     while (v->qlength_stealable > 0 && amtStolen < desired_stolen) {
         node = (qt_threadqueue_node_t *)v->head;
         do {
@@ -768,6 +799,7 @@ qthread_t INTERNAL *qt_threadqueue_dequeue_specific(qt_threadqueue_t *q,
     assert(q != NULL);
 
     QTHREAD_TRYLOCK_LOCK(&q->qlock);
+    PARANOIA_ONLY(sanity_check_queue(q));
     if (q->qlength > 0) {
         node = (qt_threadqueue_node_t *)q->tail;
         t    = (node) ? (qthread_t *)node->value : NULL;
