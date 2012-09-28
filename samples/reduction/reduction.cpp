f0c40b66f831de3cfbd7fb30f7e3759e5a7a82e5
sbarlea <sbarlea@gmail.com>
2012-09-28 11:34:35 -0500

    Added the reduction mini-app test

---
+++ samples/reduction/reduction.cpp
@@ -0,0 +1,146 @@
+#include "reduction.h"
+
+
+int Update::execute(const pair & t, reduction_context & c ) const
+{
+	Tile* tile = new Tile();
+    //printf("Starting update %d-%d...\n", t.first, t.second);
+    c.tile_data.get(t, *tile);
+    //printf("Producing tile %d of iteration %d\n", t.first, t.second+1);
+    my_usleep(1);
+    c.tile_data.put(*new pair(t.first, t.second+1), *new Tile());
+    return CnC::CNC_Success;
+}
+
+aligned_t** Update::get_dependences(const pair & t, reduction_context & c, int & no ) const
+{
+	no = 1;
+	aligned_t** read = (aligned_t**)malloc(no*sizeof(int)); 
+    c.tile_data.wait_on(t, &(read[0]));
+    return (aligned_t**)read;
+}
+
+int Reduce::execute(const int & t, reduction_context & c ) const
+{
+    int max = c.NO_TILES/2;
+    #ifdef CNC_PRECOND_ONLY
+    	max = c.NO_TILES;
+    #endif
+    
+    for(int k = 0; k < max; k++) {
+    	Tile crt;
+        c.tile_data.get(pair(k, t), crt);
+        my_usleep(1);
+    }
+    //printf("Reduced data %d produced.\n", t);
+    c.reduced_data.put(t, 1);
+    return CnC::CNC_Success;
+}
+
+aligned_t** Reduce::get_dependences(const int & t, reduction_context & c, int & no ) const
+{
+	no = c.NO_TILES;
+	aligned_t** read; 
+	#ifdef CNC_PRECOND_ONLY
+		// strict runtime needs to wait on everything	
+		read = (aligned_t**)malloc(no*sizeof(int));
+	
+		for(int k = 0; k < c.NO_TILES; k++) {
+    		Tile crt;
+        	c.tile_data.wait_on(pair(k, t), &(read[k]));
+    	}
+    #else
+    	// flexible runtime can pick and choose
+    	no = 1;
+    	read = (aligned_t**)malloc(no*sizeof(int));
+    	c.tile_data.wait_on(pair(0, t), &(read[0]));
+    #endif
+    
+    return read;
+}
+
+int NextIter::execute(const int & t, reduction_context & c ) const
+{
+	//printf("NexIIter %d runinngin\n", t);
+	int reduced_value;
+    if (t>0) 
+    {	
+    	//printf("NextIter waiting for reduced data %d\n", t-1);
+    	c.reduced_data.get(t-1, reduced_value);
+    	
+    }
+    	
+    if (t<c.NO_ITER) {
+    	c.reduce_tags.put(t);
+    	c.next_iter_tags.put(t+1);
+    	//printf("Prescribed next iteration start (%d)...\n", t+1);
+    	
+    	for(int i=0; i<c.NO_TILES; i++)
+			c.update_tags.put(pair(i, t));
+    }
+    
+    return CnC::CNC_Success;
+}
+
+aligned_t** NextIter::get_dependences(const int & t, reduction_context & c, int & no ) const
+{
+	no = 0;
+    if (t>0) 
+    {	
+    	no = 1;
+    	int* read = (int*)malloc(1 * sizeof(int));
+    	c.reduced_data.wait_on(t-1, (aligned_t**)&(read[0]));
+    	return (aligned_t**)read;	
+    }
+    return NULL;
+}
+
+
+int main (int argc, char **argv)
+{
+
+	if (argc != 4)
+	{
+		printf("Usage:\n\t%s <number of tiles> <tile size> <number of iterations\n", argv[0]);
+		return 0;
+	}
+	int tiles = atoi(argv[1]);
+	int tile_size = atoi(argv[2]);
+	int no_iter = atoi(argv[3]);
+	if (tiles < 2){
+		    fprintf(stderr, "Number of tiles cannot be less than 2.\n");
+			return -1;
+	}
+	TILE_SIZE = tile_size;
+    // Create an instance of the context class which defines the graph
+    reduction_context c( tiles, tile_size , no_iter);
+	
+	
+	
+	srand(time(NULL));
+	qtimer_t timer;
+    double   total_time = 0.0;
+    timer = qtimer_create();
+    qtimer_start(timer);
+
+	for( int i = 0; i < tiles; i++ ) {
+		c.tile_data.put(pair(i, 0), Tile());   
+	}
+	c.next_iter_tags.put(0);
+    // Wait for all steps to finish
+    c.wait();
+
+	qtimer_stop(timer);
+    total_time = qtimer_secs(timer);
+    printf("Time(s): %.3f\n", total_time);
+    qtimer_destroy(timer);
+
+    
+	/*
+	for( int i = 1; i < jobs + jobs; i++ )
+		printFile(&c, i);
+	*/
+	printf("Jobs finished.\n");
+	//printFile(&c, 1);
+	return 0;
+}
---
+++ samples/reduction/reduction.h
@@ -0,0 +1,123 @@
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <cnc/cnc.h>
+#include <iostream>
+#include <cassert>
+#include <memory>
+
+#include <qthread/qtimer.h>
+#define usecs (4000)
+
+int TILE_SIZE = 100;
+
+int t;
+
+int my_usleep(int i) {
+	
+	for(int i=0; i<2000000; i++)
+		t += 1.12*t;
+	//printf("t%d", t);
+	return t;
+}
+
+typedef std::pair<int,int> pair;
+template <>
+class cnc_tag_hash_compare< pair >
+{
+public:
+    size_t hash( const pair & t ) const
+    {
+        return t.first + t.second;
+    }
+    bool equal( const pair & a, const pair & b) const { 
+	return a.first == b.first && a.second == b.second; 
+    }
+};
+
+
+
+class Tile
+{
+public:
+	double* m_array;
+	Tile()/*, m_full( true )*/
+    {
+		
+    		m_array = new double[TILE_SIZE*TILE_SIZE];
+    		for(int i=0; i<TILE_SIZE*TILE_SIZE; i++) {
+    			m_array[i] = 0.0;
+    		}
+    }
+    
+    ~Tile() {
+		delete m_array;
+    }
+};
+
+// Forward declaration of the context class (also known as graph)
+struct reduction_context;
+
+// The step classes
+struct Update
+{
+    int execute( const pair &, reduction_context & ) const;
+    aligned_t** get_dependences(const pair & t, reduction_context & c, int & no ) const;
+};
+
+struct Reduce
+{
+    int execute( const int &, reduction_context & ) const;
+    aligned_t** get_dependences(const int & t, reduction_context & c, int & no ) const;
+};
+
+struct NextIter
+{
+    int execute( const int &, reduction_context & ) const;
+    aligned_t** get_dependences(const int & t, reduction_context & c, int & no ) const;
+};
+
+
+
+// The context class
+struct reduction_context : public CnC::context< reduction_context >
+{
+    // Step collections
+    CnC::step_collection< Update > update;
+	CnC::step_collection< Reduce > reduce;
+	CnC::step_collection< NextIter > next;
+    // Item collections
+    CnC::item_collection< pair, Tile > tile_data;
+    CnC::item_collection< int, int >  reduced_data;
+
+    // Tag collections
+    CnC::tag_collection< int > reduce_tags;
+    CnC::tag_collection< pair > update_tags;
+	CnC::tag_collection< int > next_iter_tags;
+    int NO_TILES;
+    int TILE_SIZE;
+    int NO_ITER;
+    // The context class constructor
+    reduction_context( int tiles = 2, int tile_size = 2, int no_iter = 20)
+        : CnC::context< reduction_context >(),
+	  // Initialize each step collection
+          update( *this ),
+          reduce( *this ),
+          next( *this ),
+          // Initialize each item collection
+          tile_data( *this ),
+          reduced_data( *this ),
+          // Initialize each tag collection
+          reduce_tags( *this ),
+          update_tags( *this ),
+          next_iter_tags( *this ),
+          NO_TILES(tiles),
+          TILE_SIZE(tile_size),
+          NO_ITER(no_iter)
+    {
+        // Prescriptive relations
+    	update_tags.prescribes( update, *this );
+    	reduce_tags.prescribes( reduce, *this );
+    	next_iter_tags.prescribes( next, *this );
+    }
+};
