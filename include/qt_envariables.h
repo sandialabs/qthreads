02856e825251a5840c5c7384e432a4c6a91d6fde
Dylan Stark <dylan.stark@gmail.com>
2012-10-02 14:30:15 -0600

    Adds qt_internal_unset_envstr().

--- include/qt_envariables.h
+++ include/qt_envariables.h
@@ -8,6 +8,7 @@ const char INTERNAL *qt_internal_get_env_str(const char *envariable,
 unsigned long INTERNAL qt_internal_get_env_num(const char   *envariable,
                                                unsigned long dflt,
                                                unsigned long zerodflt);
+int INTERNAL qt_internal_unset_envstr(const char *envariable);
 
 #endif // ifndef QT_ENVARIABLES_H
 /* vim:set expandtab: */
