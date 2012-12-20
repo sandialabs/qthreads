87aa3932153da706457acc9c4222df30338bca3f
Kyle Wheeler <kbwheel@sandia.gov>
2012-12-20 10:45:56 -0700

    fix minor compiler warning

--- test/features/eureka.c
+++ test/features/eureka.c
@@ -171,7 +171,7 @@ int main(int   argc,
     int ret = 0;
     char stack_size[20];
 
-    snprintf(stack_size, 20, "%lu", 4096*2); // XXX: The root cause of this is hard to track down
+    snprintf(stack_size, 20, "%u", 4096*2); // XXX: The root cause of this is hard to track down
     setenv("QT_STACK_SIZE", stack_size, 1);
     ret = qthread_init(3);
     if (ret != QTHREAD_SUCCESS) {
