ac375219f3285b5ec5544542b3b647f801107022
Kyle Wheeler <kbwheel@sandia.gov>
2013-01-04 10:44:36 -0700

    fix how I use log_barrier

--- test/features/log_barrier.c
+++ test/features/log_barrier.c
@@ -8,37 +8,42 @@
 #include <qthread/qtimer.h>
 #include "argparsing.h"
 
-aligned_t           initme_idx = 0;
+aligned_t           initme_idx = 1;
 volatile aligned_t *initme     = NULL;
 qt_barrier_t       *wait_on_me;
 
 static aligned_t barrier_thread(void *arg)
 {
-    qt_barrier_t *b   = (qt_barrier_t *)arg;
+    void **args = (void**)arg;
+    qt_barrier_t *b   = (qt_barrier_t *)args[0];
+    uintptr_t     id = (uintptr_t)args[1];
     aligned_t     idx = qthread_incr(&initme_idx, 1);
+    qthread_shepherd_id_t shep;
+    qthread_worker_id_t wkr;
 
+    wkr = qthread_worker(&shep);
+    //iprintf("I woke up on shep %u worker %u, barrier %p, using ID number %u, idx %u\n", shep, wkr, b, id, idx);
     qthread_incr(&(initme[idx]), 1);
-    qt_barrier_enter(b, qthread_shep());
+    qt_barrier_enter(b, id);
     return 0;
 }
 
 int main(int   argc,
          char *argv[])
 {
-    size_t     threads = 1000, i;
+    size_t     threads, i;
     aligned_t *rets;
     qtimer_t   t;
+    unsigned int iter, iterations = 10;
+    double tot = 0.0;
 
     assert(qthread_initialize() == 0);
     t = qtimer_create();
 
     CHECK_VERBOSE();
-    NUMARG(threads, "THREADS");
-    printf("run \"export VERBOSE=1\" to make test_log_barrier run (it's currently broken)\n");
-    if (!verbose) {
-        exit(0);
-    }                                  // XXX don't break the trunk
+    NUMARG(iterations, "ITERATIONS");
 
+    threads = qthread_num_workers();
     iprintf("%i shepherds...\n", qthread_num_shepherds());
     iprintf("%i threads...\n", (int)threads);
 
@@ -48,43 +53,44 @@ int main(int   argc,
     rets = malloc(threads * sizeof(aligned_t));
     assert(rets);
 
-    iprintf("Creating a barrier to block %i threads\n", threads + 1);
-
-    wait_on_me = qt_barrier_create(threads + 1, REGION_BARRIER, 1);     // all my spawnees plus me
+    iprintf("Creating a barrier to block %i threads\n", threads);
+    wait_on_me = qt_barrier_create(threads, REGION_BARRIER, 0);     // all my spawnees plus me
     assert(wait_on_me);
 
-    for (i = 0; i < threads; i++) {
-        qthread_fork(barrier_thread, wait_on_me, rets + i);
-    }
-    qtimer_start(t);
-    qt_barrier_enter(wait_on_me, qthread_shep());
-    qtimer_stop(t);
-    iprintf("main thread exited barrier 1 in %f seconds\n", qtimer_secs(t));
-    initme_idx = 0;
+    for (iter = 0; iter < iterations; iter++) {
+	iprintf("%i: forking the threads\n", iter);
+	for (i = 1; i < threads; i++) {
+	    void *arg[2] = {wait_on_me, (void*)(intptr_t)i};
+	    qthread_spawn(barrier_thread, arg, sizeof(void*)*2, rets + i, 0, NULL, i, 0);
+	}
+	iprintf("%i: done forking the threads, entering the barrier\n", iter);
+	qtimer_start(t);
+	qt_barrier_enter(wait_on_me, 0);
+	qtimer_stop(t);
+	iprintf("%i: main thread exited barrier in %f seconds\n", iter, qtimer_secs(t));
+	tot += qtimer_secs(t);
 
-    for (i = 0; i < threads; i++) {
-        qthread_fork(barrier_thread, wait_on_me, rets + i);
-    }
-    qtimer_start(t);
-    qt_barrier_enter(wait_on_me, qthread_shep());
-    qtimer_stop(t);
-    iprintf("main thread exited barrier 2 in %f seconds\n", qtimer_secs(t));
+	// reset
+	initme_idx = 1;
 
-    for (i = 0; i < threads; i++) {
-        if (initme[i] != 2) {
-            iprintf("initme[%i] = %i (should be 2)\n", (int)i,
-                    (int)initme[i]);
-        }
-        assert(initme[i] == 2);
+	// check retvals
+	for (i = 1; i < threads; i++) {
+	    qthread_readFF(NULL, rets + i);
+	    if (initme[i] != iter + 1) {
+                iprintf("initme[%i] = %i (should be %i)\n", (int)i,
+                        (int)initme[i], iter + 1);
+            }
+            assert(initme[i] == iter + 1);
+	}
     }
+
+    iprintf("Average barrier time = %f\n", tot / iterations);
+
+    iprintf("Destroying the barrier...\n");
+    qt_barrier_destroy(wait_on_me);
+
     iprintf("Success!\n");
 
-    /* this loop shouldn't be necessary... but seems to avoid crashes in rare
-     * cases (in other words there must a race condition in qthread_finalize()
-     * if there are outstanding threads out there) */
-    for (i = 0; i < threads; i++) {
-        qthread_readFF(NULL, rets + i);
-    }
     return 0;
 }
 
