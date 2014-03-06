e30774332b4a4f44a1dd1f04a52745dcd73caa16
Dylan Stark <dstark@sandia.gov>
2014-03-06 16:45:46 -0700

    Adds qthreads initialization to MPIQ_Init().

--- src/interfaces/mpiq/mpiq.c
+++ src/interfaces/mpiq/mpiq.c
@@ -1,10 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <mpi.h>
+
+#include "qthread/qthread.h"
 
 int MPIQ_Init(int * argc, char *** argv)
 {
     int rc = 0;
 
+    rc = qthread_initialize();
+    if (QTHREAD_SUCCESS != rc) {
+        fprintf(stderr, "Error: failed to initialize Qthreads (rc=%d)\n", rc);
+        abort();
+    }
+
     rc = MPI_Init(argc, argv);
+    if (MPI_SUCCESS != rc) {
+        fprintf(stderr, "Error: failed to initialize MPI (rc=%d)\n", rc);
+        abort();
+    }
 
     return rc;
 }
