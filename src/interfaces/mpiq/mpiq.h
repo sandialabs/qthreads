b5e8c96861e9442e976e5d01717f1ebd93e0338c
Dylan Stark <dstark@sandia.gov>
2014-03-10 15:52:50 -0600

    Adds support for all more MPIQ calls.

--- src/interfaces/mpiq/mpiq.h
+++ src/interfaces/mpiq/mpiq.h
@@ -1,8 +1,17 @@
 #ifndef MPIQ_H
 #define MPIQ_H
 
+#include <mpi.h>
+
 int MPIQ_Init(int * argc, char *** argv);
 int MPIQ_Init_thread(int * argc, char *** argv, int required, int * provided);
 int MPIQ_Finalize(void);
+int MPIQ_Comm_dup(MPI_Comm comm, MPI_Comm *newcomm);
+int MPIQ_Errhandler_set(MPI_Comm comm, MPI_Errhandler errhandler);
+int MPIQ_Comm_rank(MPI_Comm comm, int *rank);
+int MPIQ_Comm_size(MPI_Comm comm, int *size);
+int MPIQ_Barrier(MPI_Comm comm);
+int MPIQ_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm);
+int MPIQ_Abort(MPI_Comm comm, int errorcode);
 
 #endif
