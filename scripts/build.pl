57ec91a038261371e3e72a412bf2f11489dbf4cb
Dylan Stark <dstark@sandia.gov>
2015-02-11 14:14:45 -0700

    Adds Chapel-specific configurations.

--- scripts/build.pl
+++ scripts/build.pl
@@ -33,6 +33,9 @@ my %config = (
     debug         => 'CFLAGS="-g -O0" CXXFLAGS="-g -O0" --enable-debug --enable-static --disable-shared',
     hwloc         => '--with-topology=hwloc',
     sinc_stats    => '--enable-profiling=sincs',
+    chapel_default => '--enable-static --disable-shared --enable-condwait-queue --disable-spawn-cache --with-scheduler=nemesis',
+    chapel_oversubscription => '--enable-oversubscription',
+    chapel_guard_pages => '--enable-guard-pages',
 );
 
 my @summaries;
