59e4f8518d1945a4e84fdd2530fc712f25f9bfd1
Erik Lee <ejlee@sandia.gov>
2016-02-17 12:50:50 -0600

    first tests, not passing.

--- include/qthread/logging.h
+++ include/qthread/logging.h
@@ -16,13 +16,13 @@
 // time. To use this, make preprocessor definitions of the different
 // error groups you want to have available and set them to either 0 or
 // 1 according to what you want to see.
-#define logargs(group, s, args...) do {                                 \
+#define qtlogargs(group, s, args...) do {                                 \
     if(group) {                                                         \
       printf("(" #group ") " __FILE__ ":" STRING(__LINE__) ": " s "\n", args); \
     }                                                                   \
   } while(0)
 
-#define log(group, s) do {                                            \
+#define qtlog(group, s) do {                                            \
     if(group) {                                                       \
       printf("(" #group ") " __FILE__ ":" STRING(__LINE__) ": " s "\n");   \
     }                                                                 \
