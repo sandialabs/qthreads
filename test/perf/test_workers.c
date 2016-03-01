e4e78e24531653045bf125b5ec4fc6ade386d82d
Erik Lee <ejlee@sandia.gov>
2016-03-01 10:46:36 -0600

    initialize worker data collection successfully

--- test/perf/test_workers.c
+++ test/perf/test_workers.c
@@ -7,20 +7,21 @@
 #include<qthread/performance.h>
 #include<qthread/logging.h>
 
-int setup(void** state){
-  return 0;
-}
-
-int teardown(void**state){
-  return 0;
-}
-
 void test_setup(void** state){
+  qtperf_set_instrument_workers(1);
+  qthread_initialize();
+  assert_true(qtperf_check_invariants());
+}
+
+void test_teardown(void** state){
+  qtperf_free_data();
+  assert_true(qtperf_check_invariants());
 }
 
 int main(int argc, char** argv){
   const struct CMUnitTest test[] ={
-    cmocka_unit_test_setup_teardown(test_setup, setup, teardown)
+    cmocka_unit_test(test_setup),
+    cmocka_unit_test(test_teardown)
   };
   return cmocka_run_group_tests(test,NULL,NULL);
 }
