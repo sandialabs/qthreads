6be3ccd89947985e24fb2f6b8d522e71e775890d
Erik Lee <ejlee@sandia.gov>
2016-11-08 08:52:12 -0600

    updating build script

--- config-instrumentation.sh
+++ config-instrumentation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+qthread_root=`pwd`
+
+cmocka_lib="${qthread_root}/dependencies/install/lib"
+cmocka_inc="${qthread_root}/dependencies/install/include"
+
+CFLAGS="-DQTHREAD_PERFORMANCE -DQTPERF_TESTING -I${cmocka_inc}" LDFLAGS="-L${cmocka_lib}" LIBS=-lcmocka ./configure --enable-debugging --disable-lazy-threadids  --enable-picky
+
 ## enable testing, disable perf debug output 
-CFLAGS='-DQTHREAD_PERFORMANCE -DQTPERF_TESTING' LDFLAGS=-L/home/erik/packages/lib LIBS=-lcmocka ./configure --enable-debugging --disable-lazy-threadids  --enable-picky
+#CFLAGS='-DQTHREAD_PERFORMANCE -DQTPERF_TESTING' LDFLAGS=-L/home/erik/packages/lib LIBS=-lcmocka ./configure --enable-debugging --disable-lazy-threadids  --enable-picky
 
 ## Enable testing AND perfdbg output
 #CFLAGS='-DQTHREAD_PERFORMANCE -DQTPERF_TESTING -DPERFDBG=1' ./configure --enable-debugging --disable-lazy-threadids  --enable-picky
