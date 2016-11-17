f698e86e509fc19cd76e8bf1bdb2b7630868fb41
Noah Evans <noah.evans@gmail.com>
2016-11-16 17:58:41 -0700

    Guard page test API and signal handling changes
    Change the guard page feature test to use the new stack introspection
    API and to catch bus errors instead of segfaults (mprotect PROC_NONE bus
    errors instead of segfaulting).

--- test/features/guard_pages.c
+++ test/features/guard_pages.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <float.h> /* for DBL_MAX, per C89 */
+#include <signal.h>
 #include <qthread/qthread.h>
 #include <qthread/barrier.h>
 #include <qthread/qtimer.h>
@@ -12,29 +13,32 @@
 aligned_t     initme_idx = 1;
 aligned_t    *initme     = NULL;
 qt_barrier_t *wait_on_me;
+int stack_size;
+size_t       threads = 2;
+aligned_t    *rets;
 
-void segfault_sigaction(int signal, siginfo_t *si, void *arg)
+void bus_sigaction(int signal, siginfo_t *si, ucontext_t *arg)
 {
     exit(0);
 }
 
-static aligned_t segfault_thread(void *arg)
+static aligned_t buserr_thread(void *arg)
 {
     volatile char *eos; // end of stack
-    eos = &eos + qt_stack_size;
+
+    eos = (volatile char*)((unsigned long long int)(((qthread_id() % 2) ? qthread_bos() + 256 : qthread_tos() - 256)));
+    iprintf("%s: tos %p bos %p eos %p\n", __func__, qthread_tos(), qthread_bos(), eos);
     *eos = 1;
-    // get the stack size
-    // this might be slightly more complicated
+
     return 0;
 }
 
 int main(int   argc,
          char *argv[])
 {
-    size_t       threads = 1000, i;
-    aligned_t   *rets;
+    int i;
     qtimer_t     t;
-    unsigned int iter, iterations = 10;
+    unsigned int iter, iterations = 1;
     double       total_time = 0.0;
     double       max_time   = 0.0;
     double       min_time   = DBL_MAX;
@@ -42,38 +46,50 @@ int main(int   argc,
 
     memset(&sa, 0, sizeof(sigaction));
     sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = segfault_sigaction;
+    sa.sa_sigaction = bus_sigaction;
     sa.sa_flags   = SA_SIGINFO;
 
     // TODO(npe) how does qthreads handle signals?
-    sigaction(SIGSEGV, &sa, NULL);
+    sigaction(SIGBUS, &sa, NULL);
 
     assert(qthread_initialize() == 0);
-    t = qtimer_create();
 
     CHECK_VERBOSE();
     NUMARG(threads, "THREADS");
     NUMARG(iterations, "ITERATIONS");
+    if (!getenv("QT_STACK_SIZE") && !getenv("QTHREAD_STACK_SIZE"))
+      setenv("QT_STACK_SIZE", "4096", 0);
+    if (!getenv("GUARD_PAGES"))
+      setenv("GUARD_PAGES", "1", 0);
 
+    stack_size = atoi(getenv("QT_STACK_SIZE"));
     rets = (aligned_t *)malloc(threads * sizeof(aligned_t));
     assert(rets);
 
-    iprintf("creating the barrier for %zu threads\n", threads);
-
     for (iter = 0; iter <= iterations; ++iter) {
-        // iprintf("%i: forking the threads\n", iter);
         for (i = 1; i < threads; ++i) {
-            qthread_spawn(segfaults_thread, NULL, NULL,
+            qthread_spawn(buserr_thread, NULL, 0,
                           rets + i,
                           0, NULL, i, 0);
         }
     }
 
+    // FIXME(npe) We should really catch the bus errors for the top and
+    // bottom of the stack and then return via a synchronizing for
+    // every exiting thread.
+    for (i = 1; i < threads; ++i) {
+        qthread_readFF(NULL, rets + i);
+        if (initme[i] != iter + 1) {
+            iprintf("initme[%i] = %i (should be %i)\n", (int)i,
+                    (int)initme[i], iter + 1);
+        }
+        assert(initme[i] == iter + 1);
+    }
+    
     free(rets);
 
-    iprintf("Success!\n");
-
-    return 0;
+    iprintf("Failure!\n");
+    exit(1);
 }
 
 /* vim:set expandtab */
