302429c9dd519b8b12e57e37ee656cc6874a2f2b
Noah Evans <noah.evans@gmail.com>
2018-06-25 12:45:00 -0600

    first stab at network aware queues

--- src/threadqueues/distrib_threadqueues.c
+++ src/threadqueues/distrib_threadqueues.c
@@ -32,7 +32,7 @@
 typedef uint8_t cacheline[CACHELINE_WIDTH];
 
 /* Cutoff variables */
-int max_backoff; 
+int max_backoff;
 int spinloop_backoff;
 int condwait_backoff;
 int steal_ratio;
@@ -44,11 +44,12 @@ struct _qt_threadqueue_node {
   qthread_t                   *value;
 };
 
-typedef struct {
+typedef struct q_int {
   qt_threadqueue_node_t *head;
   qt_threadqueue_node_t *tail;
   long                 qlength;
   QTHREAD_TRYLOCK_TYPE qlock;
+  struct q_int *nq;
   cacheline buf; // ensure internal nodes are a cacheline apart
 } qt_threadqueue_internal;
 
@@ -63,7 +64,7 @@ struct _qt_threadqueue {
   w_ind* w_inds;
   QTHREAD_COND_DECL(cond);
   long                 numwaiters;
-}; 
+};
 
 // global cond pool
 int finalizing;
@@ -77,11 +78,14 @@ qt_threadqueue_pools_t generic_threadqueue_pools;
 #define myqueue(q) (q->t + mycounter(q))
 
 static qt_threadqueue_t* alloc_threadqueue(){
+  int i;
   qt_threadqueue_t* t = (qt_threadqueue_t *)qt_mpool_alloc(generic_threadqueue_pools.queues);
   t->num_queues = qlib->nworkerspershep; // Assumption built into api of constant number of workers per shepherd
   t->t = qt_malloc(sizeof(qt_threadqueue_internal) * t->num_queues);
   t->w_inds = qt_calloc(qlib->nshepherds * qlib->nworkerspershep,
-                        sizeof(w_ind));  
+                        sizeof(w_ind));
+  for(i = 0; i < t->num_queues; i++)
+    t->t[i].nq = qt_malloc(sizeof(qt_threadqueue_internal));
   return t;
 }
 
@@ -91,13 +95,13 @@ static void free_threadqueue(qt_threadqueue_t* t){
   qt_mpool_free(generic_threadqueue_pools.queues, t);
 }
 
-static QINLINE qt_threadqueue_node_t *alloc_tqnode(void){                                     
+static QINLINE qt_threadqueue_node_t *alloc_tqnode(void){
   return (qt_threadqueue_node_t *)qt_mpool_alloc(generic_threadqueue_pools.nodes);
-} 
+}
 
-static QINLINE void free_tqnode(qt_threadqueue_node_t* t){                                     
+static QINLINE void free_tqnode(qt_threadqueue_node_t* t){
   qt_mpool_free(generic_threadqueue_pools.nodes, t);
-} 
+}
 
 extern qt_mpool generic_qthread_pool;
 
@@ -108,10 +112,11 @@ static QINLINE void free_qthread(qthread_t* t){
   return qt_mpool_free(generic_qthread_pool, t);
 }
 
-qt_threadqueue_t INTERNAL *qt_threadqueue_new(void){   
+qt_threadqueue_t INTERNAL *qt_threadqueue_new(void){
   qt_threadqueue_t *qe = alloc_threadqueue();
+
   for(int i=0; i<qe->num_queues; i++){
-    qt_threadqueue_internal* q = qe->t + i; 
+    qt_threadqueue_internal* q = qe->t + i;
     if (q != NULL) {
       q->head              = NULL;
       q->tail              = NULL;
@@ -124,9 +129,9 @@ qt_threadqueue_t INTERNAL *qt_threadqueue_new(void){
   }
   QTHREAD_COND_INIT(qe->cond);
   return qe;
-} 
+}
 
-void INTERNAL qt_threadqueue_free(qt_threadqueue_t *qe){   
+void INTERNAL qt_threadqueue_free(qt_threadqueue_t *qe){
   for(int i=0; i<qe->num_queues; i++){
     qt_threadqueue_internal* q = qe->t + i;
     if (q->head != q->tail) {
@@ -137,9 +142,9 @@ void INTERNAL qt_threadqueue_free(qt_threadqueue_t *qe){
         if (node != NULL) {
           q->tail = node->prev;
           if (q->tail == NULL) {
-              q->head = NULL;
+            q->head = NULL;
           } else {
-              q->tail->next = NULL;
+            q->tail->next = NULL;
           }
           t = node->value;
           free_tqnode(node);
@@ -156,14 +161,14 @@ void INTERNAL qt_threadqueue_free(qt_threadqueue_t *qe){
   }
   QTHREAD_COND_DESTROY(qe->cond);
   free_threadqueue(qe);
-} 
+}
 
-static void qt_threadqueue_subsystem_shutdown(){   
+static void qt_threadqueue_subsystem_shutdown(){
   qt_mpool_destroy(generic_threadqueue_pools.nodes);
   qt_mpool_destroy(generic_threadqueue_pools.queues);
-} 
+}
 
-void INTERNAL qt_threadqueue_subsystem_init(){   
+void INTERNAL qt_threadqueue_subsystem_init(){
   steal_ratio = qt_internal_get_env_num("STEAL_RATIO", 8, 0);
   condwait_backoff = qt_internal_get_env_num("CONDWAIT_BACKOFF", 2048, 0);
   finalizing = 0;
@@ -174,14 +179,15 @@ void INTERNAL qt_threadqueue_subsystem_init(){
   qthread_internal_cleanup(qt_threadqueue_subsystem_shutdown);
 }
 
-ssize_t INTERNAL qt_threadqueue_advisory_queuelen(qt_threadqueue_t *q){   
+ssize_t INTERNAL qt_threadqueue_advisory_queuelen(qt_threadqueue_t *q){
   return myqueue(q)->qlength;
-} 
+}
 
-/* Threadqueue operations 
+/* Threadqueue operations
  * We have 4 basic queue operations, enqueue and dequeue for head and tail */
 void INTERNAL qt_threadqueue_enqueue_tail(qt_threadqueue_t *restrict qe,
-                                          qthread_t *restrict        t){ 
+                                          qthread_t *restrict        t){
+  qt_threadqueue_internal* q;
   if (t->thread_state == QTHREAD_STATE_TERM_SHEP) {
     finalizing = 1;
   }
@@ -192,11 +198,17 @@ void INTERNAL qt_threadqueue_enqueue_tail(qt_threadqueue_t *restrict qe,
     }
     mccoy = t;
   } else {
-    qt_threadqueue_internal* q = myqueue(qe);
+
+    if (t->flags & QTHREAD_NETWORK) {
+      q = myqueue(qe)->nq;
+    } else {
+      q = myqueue(qe);
+    }
     mycounter(qe) = (mycounter(qe) + 1) % qe->num_queues;
     qt_threadqueue_node_t *node = alloc_tqnode();
     node->value = t;
     node->next = NULL;
+
 
     QTHREAD_TRYLOCK_LOCK(&q->qlock);
     node->prev = q->tail;
@@ -220,10 +232,10 @@ void INTERNAL qt_threadqueue_enqueue_tail(qt_threadqueue_t *restrict qe,
     if(qe->numwaiters) QTHREAD_COND_SIGNAL(qe->cond);
     QTHREAD_COND_UNLOCK(qe->cond);
   }
-} 
+}
 
 void INTERNAL qt_threadqueue_enqueue_head(qt_threadqueue_t *restrict qe,
-                                          qthread_t *restrict        t){   
+                                          qthread_t *restrict        t){
   if (t->flags & QTHREAD_REAL_MCCOY) { // only needs to be on worker 0 for termination
     if(mccoy) {
       printf("mccoy thread non-null and trying to set!\n");
@@ -233,7 +245,11 @@ void INTERNAL qt_threadqueue_enqueue_head(qt_threadqueue_t *restrict qe,
     return;
   }
 
-  qt_threadqueue_internal* q = myqueue(qe);
+  qt_threadqueue_internal* q;
+  if (t->flags & QTHREAD_NETWORK)
+    q = myqueue(qe)->nq;
+  else
+    q = myqueue(qe);
   mycounter(qe) = (mycounter(qe) + 1) % qe->num_queues;
   qt_threadqueue_node_t *node = alloc_tqnode();
   node->value     = t;
@@ -243,9 +259,9 @@ void INTERNAL qt_threadqueue_enqueue_head(qt_threadqueue_t *restrict qe,
   node->next = q->head;
   q->head    = node;
   if (q->tail == NULL) {
-      q->tail = node;
+    q->tail = node;
   } else {
-      node->next->prev = node;
+    node->next->prev = node;
   }
   q->qlength++;
   QTHREAD_TRYLOCK_UNLOCK(&q->qlock);
@@ -256,13 +272,25 @@ void INTERNAL qt_threadqueue_enqueue_head(qt_threadqueue_t *restrict qe,
     }
     QTHREAD_COND_UNLOCK(qe->cond);
   }
-} 
+}
 
-qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_tail(qt_threadqueue_t *qe){                                     
-  qt_threadqueue_internal* q = myqueue(qe);
+// add a way of setting this.
+int take_from_network()
+{
+  return 1;
+}
+
+qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_tail(qt_threadqueue_t *qe){
+  qt_threadqueue_internal* q;
+  // Add logic to take from network tasks here.
+  if (take_from_network())
+    q = myqueue(qe)->nq;
+  else
+    q = myqueue(qe);
+
   mycounter(qe) = (mycounter(qe) + 1) % qe->num_queues;
   qt_threadqueue_node_t *node;
-  
+
   // If there is no work or we can't get the lock, fail
   if (q->qlength == 0) return NULL;
   if (!QTHREAD_TRYLOCK_TRY(&q->qlock)) return NULL;
@@ -270,7 +298,7 @@ qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_tail(qt_threadqueue_t *qe
     QTHREAD_TRYLOCK_UNLOCK(&q->qlock);
     return NULL;
   }
-  
+
   node = (qt_threadqueue_node_t *)q->tail;
   q->tail = node->prev;
   if(q->tail) q->tail->next = NULL;
@@ -279,13 +307,18 @@ qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_tail(qt_threadqueue_t *qe
   QTHREAD_TRYLOCK_UNLOCK(&q->qlock);
 
   return node;
-}                                   
+}
 
-qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_head(qt_threadqueue_t *qe){                                     
-  qt_threadqueue_internal* q = myqueue(qe);
+qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_head(qt_threadqueue_t *qe){
+  qt_threadqueue_internal* q;
+  if (take_from_network())
+    q = myqueue(qe)->nq;
+  else
+    q = myqueue(qe);
+
   mycounter(qe) = (mycounter(qe) + 1) % qe->num_queues;
   qt_threadqueue_node_t *node;
-  
+
   // If there is no work or we can't get the lock, fail
   if (q->qlength == 0) return NULL;
   if (!QTHREAD_TRYLOCK_TRY(&q->qlock)) return NULL;
@@ -293,7 +326,7 @@ qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_head(qt_threadqueue_t *qe
     QTHREAD_TRYLOCK_UNLOCK(&q->qlock);
     return NULL;
   }
-  
+
   node = (qt_threadqueue_node_t *)q->head;
   q->head = node->next;
   if(q->head) q->head->prev = NULL;
@@ -302,7 +335,7 @@ qt_threadqueue_node_t INTERNAL *qt_threadqueue_dequeue_head(qt_threadqueue_t *qe
   QTHREAD_TRYLOCK_UNLOCK(&q->qlock);
 
   return node;
-}                                   
+}
 
 void INTERNAL qt_threadqueue_enqueue(qt_threadqueue_t *restrict q,
                                      qthread_t *restrict        t){
@@ -317,12 +350,12 @@ void INTERNAL qt_threadqueue_enqueue_yielded(qt_threadqueue_t *restrict q,
 /* Unsupported operations */
 qthread_t INTERNAL * qt_threadqueue_dequeue_specific(qt_threadqueue_t * q,
                                                      void             * value){
-    return NULL;
+  return NULL;
 }
 
 qthread_t INTERNAL *qt_threadqueue_private_dequeue(qt_threadqueue_private_t *c){
-    return NULL;
-} 
+  return NULL;
+}
 
 void INTERNAL qt_threadqueue_enqueue_cache(qt_threadqueue_t         *q,
                                            qt_threadqueue_private_t *cache)
@@ -341,13 +374,17 @@ inline int square (int x) { return x * x; }
 
 int INTERNAL qt_threadqueue_private_enqueue_yielded(qt_threadqueue_private_t *restrict q,
                                                     qthread_t *restrict                t)
-{ return 0; } 
+{ return 0; }
 
 // We try and dequeue locally, if that fails we should do some stealing
 qthread_t INTERNAL *qt_scheduler_get_thread(qt_threadqueue_t         *qe,
                                             qt_threadqueue_private_t *qc,
-                                            uint_fast8_t              active){   
-  qt_threadqueue_internal * q= myqueue(qe);
+                                            uint_fast8_t              active){
+  qt_threadqueue_internal* q;
+  if (take_from_network())
+    q = myqueue(qe)->nq;
+  else
+    q = myqueue(qe);
   qt_threadqueue_node_t *node = NULL;
   qthread_t* t;
   qthread_shepherd_t *my_shepherd = qthread_internal_getshep();
@@ -355,7 +392,7 @@ qthread_t INTERNAL *qt_scheduler_get_thread(qt_threadqueue_t         *qe,
   for(int numwaits = 0; !node; numwaits ++){
     node = qt_threadqueue_dequeue_tail(qe);
 
-    // If we've done QT_STEAL_RATIO waits on local queue, try to steal 
+    // If we've done QT_STEAL_RATIO waits on local queue, try to steal
     if(!node && steal_ratio > 0 && numwaits % steal_ratio == 0) {
       for(int i=0; i < qlib->nshepherds; i++){
         qt_threadqueue_t *victim_queue = qlib->shepherds[i].ready;
@@ -371,7 +408,7 @@ qthread_t INTERNAL *qt_scheduler_get_thread(qt_threadqueue_t         *qe,
     if(!node && qthread_worker(NULL) == 0 && mccoy){
       qthread_t *t = mccoy;
       mccoy = NULL;
-      return t; 
+      return t;
     } else if(!node){
       if(numwaits > condwait_backoff && !finalizing){
         QTHREAD_COND_LOCK(qe->cond);
@@ -389,13 +426,13 @@ qthread_t INTERNAL *qt_scheduler_get_thread(qt_threadqueue_t         *qe,
   t = node->value;
   free_tqnode(node);
   return t;
-} 
+}
 
-void INTERNAL qthread_steal_enable(){     
-}   
+void INTERNAL qthread_steal_enable(){
+}
 
-void INTERNAL qthread_steal_disable(){     
-}     
+void INTERNAL qthread_steal_disable(){
+}
 
 qthread_shepherd_id_t INTERNAL qt_threadqueue_choose_dest(qthread_shepherd_t * curr_shep){
   qthread_shepherd_id_t dest_shep_id = 0;
@@ -403,11 +440,11 @@ qthread_shepherd_id_t INTERNAL qt_threadqueue_choose_dest(qthread_shepherd_t * c
     dest_shep_id               = curr_shep->sched_shepherd++ % qlib->nshepherds;
     curr_shep->sched_shepherd *= (qlib->nshepherds > (dest_shep_id + 1));
   } else {
-    dest_shep_id = 
-        (qthread_shepherd_id_t)qthread_internal_incr_mod(
-            &qlib->sched_shepherd,
-            qlib->nshepherds,
-            &qlib->sched_shepherd_lock);
+    dest_shep_id =
+      (qthread_shepherd_id_t)qthread_internal_incr_mod(
+                                                       &qlib->sched_shepherd,
+                                                       qlib->nshepherds,
+                                                       &qlib->sched_shepherd_lock);
   }
 
   return dest_shep_id;
@@ -415,8 +452,8 @@ qthread_shepherd_id_t INTERNAL qt_threadqueue_choose_dest(qthread_shepherd_t * c
 
 size_t INTERNAL qt_threadqueue_policy(const enum threadqueue_policy policy){
   switch (policy) {
-    default:
-      return THREADQUEUE_POLICY_UNSUPPORTED;
+  default:
+    return THREADQUEUE_POLICY_UNSUPPORTED;
   }
 }
 
