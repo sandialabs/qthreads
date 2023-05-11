80dd8660f5c00750c8470404f04f28b4af1518d0
Chris Jenkins <cdjenk@sandia.gov>
2023-05-10 20:24:31 -0600

    use cond variable directly inside struct. modularize code a little

--- test/basics/qthread_fp.c
+++ test/basics/qthread_fp.c
@@ -7,7 +7,6 @@
 #include <assert.h>
 #include <qthread/qthread.h>
 
-
 // https://www.geeksforgeeks.org/comparison-float-value-c/
 // https://dotnettutorials.net/lesson/taylor-series-using-recursion-in-c/
 // https://www.studytonight.com/c/programs/important-concepts/sum-of-taylor-series
@@ -17,7 +16,7 @@ struct parts
   int length;
   float exp;
   float ans;
-  aligned_t* cond;
+  aligned_t cond;
 };
 
 // https://www.w3resource.com/c-programming-exercises/math/c-math-exercise-24.php
@@ -33,38 +32,41 @@ static float taylor_exponential_core(int n, float x)
 
 static aligned_t taylor_exponential(void *arg)
 {
-  struct parts* te = (struct parts*) arg;
+  struct parts *te = (struct parts *)arg;
   float exp = te->exp;
   float length = te->length;
   te->ans = taylor_exponential_core(te->length, te->exp);
   return 0;
 }
 
+static void checkFloatAsQthread()
+{
+  int ret = -1;
+  struct parts teParts = {250, 9.0f, 0.0f};
+  qthread_empty(&teParts.cond);
+
+  ret = qthread_fork(taylor_exponential, &teParts, &teParts.cond);
+  assert(ret == QTHREAD_SUCCESS);
+
+  ret = qthread_readFF(NULL, &teParts.cond);
+  assert(ret == QTHREAD_SUCCESS);
+
+  assert(teParts.ans == 8103.083984f);
+}
+
 static void checkFloat()
 {
-  void* cond = malloc(QTHREAD_SIZEOF_ALIGNED_T);
-  struct parts teParts = {250,9.0f,0.0f, cond};
-  int ret = -1;
-  qthread_empty(teParts.cond);
-
-  ret = qthread_fork(taylor_exponential, &teParts, teParts.cond);
-  assert(ret == QTHREAD_SUCCESS);
-
-  ret = qthread_readFF(NULL, teParts.cond);
-  assert(ret == QTHREAD_SUCCESS);
-
-  printf("value of ans = %f\n", teParts.ans);
+  float ans = taylor_exponential_core(250, 9.0f);
+  assert(ans == 8103.083984f);
 }
 
 int main(void)
 {
-  float ans = taylor_exponential_core(250, 9.0);
-  printf("value of ans = %f\n", ans);
-  assert(ans == 8103.083984f);
+  checkFloat();
 
   int status = qthread_initialize();
   assert(status == QTHREAD_SUCCESS);
-
-  checkFloat();
+  checkFloatAsQthread();
+  
   return EXIT_SUCCESS;
 }
