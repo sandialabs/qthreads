111df0042a33a64ccbc34eb0352216c48f6e939c
Ian Henriksen <iandhenriksen@gmail.com>
2023-11-21 12:44:16 -0800

    Fix garbage values and overflows in sinc_workers test due to wrong synchronization order.

--- test/basics/sinc_workers.c
+++ test/basics/sinc_workers.c
@@ -2,6 +2,8 @@
 #include <qthread/sinc.h>
 #include <assert.h>
 
+int expected[31] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040};
+
 typedef struct {
   int i;
   qt_sinc_t* s;
@@ -10,8 +12,9 @@ typedef struct {
 
 static aligned_t fib(void *args_){
   args_t* args = (args_t*)args_;
-  if(args->i < 2){
+  if(args->i <= 2){
     *args->ret = 1;
+    assert(*args->ret == expected[args->i]);
     qt_sinc_submit(args->s, NULL);
     return 0;
   }
@@ -26,8 +29,9 @@ static aligned_t fib(void *args_){
   qthread_fork_copyargs(fib, &argsy, sizeof(args_t), NULL);
 
   qt_sinc_wait(&sinc, NULL);
-  qt_sinc_submit(args->s, NULL);
   *args->ret  = x + y;
+  assert(*args->ret == expected[args->i]);
+  qt_sinc_submit(args->s, NULL);
   return 0;
 }
 
@@ -39,6 +43,6 @@ int main(void){
   args_t args = { 30, &sinc, &ret };
   qthread_fork_copyargs(fib, &args, sizeof(args_t), NULL);
   qt_sinc_wait(&sinc, NULL);
-  printf("%d\n", ret);
+  assert(ret == expected[30]);
 }
 
