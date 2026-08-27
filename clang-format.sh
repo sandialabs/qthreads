9ff8866ea4a717787436cff968d8861f9b1dc536
Ian Henriksen <iandhenriksen@gmail.com>
2026-08-27 14:46:12 -0600

    Update canonical clang-format version used in testing.

--- clang-format.sh
+++ clang-format.sh
@@ -1,1 +1,1 @@
-find -iname *.h -or -iname *.c -or -iname *.hpp -or -iname *.cpp | xargs clang-format-22 -i
+find -iname *.h -or -iname *.c -or -iname *.hpp -or -iname *.cpp | xargs clang-format-23 -i
